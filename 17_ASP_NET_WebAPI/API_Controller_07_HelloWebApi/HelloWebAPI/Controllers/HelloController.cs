38b997650b56f98b5ce2fb7127d964d7ea4be0e3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-07-26 10:50:55 +0200

    New trial web api;

---
+++ 17_ASP_NET_WebAPI/API_Controller_07_HelloWebApi/HelloWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWebAPI.Controllers
+{
+    //https://www.tutorialsteacher.com/webapi/web-api-tutorials
+    public class HelloController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
