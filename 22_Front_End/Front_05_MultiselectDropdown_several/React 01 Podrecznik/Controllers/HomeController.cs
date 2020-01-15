45e060a9d4a80093756d8e700e5a8f42b5c838fd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-01-15 08:56:41 +0100

    Removed bug;

--- 22_Front_End/Front_05_MultiselectDropdown_several/React 01 Podrecznik/Controllers/HomeController.cs
+++ 22_Front_End/Front_05_MultiselectDropdown_several/React 01 Podrecznik/Controllers/HomeController.cs
@@ -11,10 +11,11 @@ namespace React_01_Podrecznik.Controllers
         {
             List<string> vesselTypes = new List<string>() { "bulk", "tug", "yacht", "tanker", "supply" };
             List<string> vesselFlags = new List<string>() { "belize", "china", "norway", "cyprus", "poland" };
+            List<string> dupa = new List<string>() { "belize", "nikaragua", "norway", "cyprus", "poland" };
 
             ViewBag.VesselTypes = vesselTypes;
             ViewBag.VesselFlags = vesselFlags;
-            ViewBag.Dupa = vesselFlags;
+            ViewBag.Dupa = dupa;
 
             return View(model);
         }
