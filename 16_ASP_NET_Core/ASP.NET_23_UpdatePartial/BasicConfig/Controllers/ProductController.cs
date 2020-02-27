6dc7b3376f721dc564ec9c2c30515a7f86fdf8eb
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-02-27 23:10:55 +0100

    Added 3 sandbox trials: 2x updating partial in ASP.NET Core, 1x Cefsharp.Offscreen console app with two parallel browsers performing some tasks.

---
+++ 16_ASP_NET_Core/ASP.NET_23_UpdatePartial/BasicConfig/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BasicConfig.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<PartialViewResult> LoadSomeShit()
+        {
+            await Task.Delay(5000);
+            ViewBag.Dupa = "viewbag dupa displayed, together with partial";
+
+            return new PartialViewResult
+            {
+                ViewName = "_LoadSomeShit",
+                ViewData = ViewData,
+            };
+        }
+    }
+
+
+}
