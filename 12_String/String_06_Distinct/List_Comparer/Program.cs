3e64b0d54d7da31e0d836e6875993df39c5a7b78
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-02-29 07:57:39 +0100

    Simple use of Distinct() method;

---
+++ 12_String/String_06_Distinct/List_Comparer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = File.ReadAllLines("1.txt");
+            List<string> pierwotnaLista = new List<string>(lista);
+
+            List<string> robocza = pierwotnaLista.Distinct().ToList();
+            List<string> output = new List<string>();
+            foreach (var item in robocza)
+            {
+                Console.WriteLine(item);
+
+                output.Add(item);
+
+
+            }
+            System.IO.File.WriteAllLines("output.txt", output);
+            Console.WriteLine("saved");
+            Console.ReadKey();
+
+        }
+    }
+}
