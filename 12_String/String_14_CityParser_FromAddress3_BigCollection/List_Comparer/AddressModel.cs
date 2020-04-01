451d5228a5edc8714a1b89f182051d0574a6d1d4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-04-01 09:35:57 +0200

    Added six small project operating on string, and code snipped for user verify action filter;

---
+++ 12_String/String_14_CityParser_FromAddress3_BigCollection/List_Comparer/AddressModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Comparer
+{
+    public class AddressModel
+    {
+        public string Address { get; set; }
+        public char CountryChar { get; set; }
+    }
+}
