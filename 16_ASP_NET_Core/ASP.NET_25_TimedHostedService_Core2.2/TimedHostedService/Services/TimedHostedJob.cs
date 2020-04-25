17be64ca1258b85e0c8bcbaeffff3774a593353f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-04-25 21:27:06 +0200

    Try-out of simplified Background task that runs on a timer in ASP.NET Core;

---
+++ 16_ASP_NET_Core/ASP.NET_25_TimedHostedService_Core2.2/TimedHostedService/Services/TimedHostedJob.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimedHostedService.Services
+{
+    //creds: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-2.2&tabs=visual-studio
+    public class TimedHostedJob : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private Timer _timer;
+
+        public TimedHostedJob(ILogger<TimedHostedJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Timed Background Service is working.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
