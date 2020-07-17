12f4131cfdd6da9417feb0386b5c3f888ef720ec
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-07-17 11:22:20 +0200

    Html to Pdf with TuesPechkin examples;

---
+++ 02_APIs/HtmlToPdf_07_TuesPechkin/HtmlToPdf/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.IO;
+using TuesPechkin;
+
+namespace HtmlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratePdf();
+        }
+
+        private static void GeneratePdf()
+        {
+            IConverter converter =
+    new ThreadSafeConverter(
+        new PdfToolset(
+            new Win32EmbeddedDeployment(
+                new TempFolderDeployment())));
+
+            HtmlToPdfDocument doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings =
+                {
+                    ProduceOutline = true,
+                    DocumentTitle = "Pretty stuff",
+                    PaperSize = PaperKind.A4, // Implicit conversion to PechkinPaperSize
+                    Margins =
+                    {
+                        All = 1.375,
+                        Unit = Unit.Centimeters
+                    }
+                },
+                Objects = {
+                    new ObjectSettings { HtmlText = @"<html style='background-color:gray;'><dir style='color:orange;'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur mauris eget ultrices  iaculis. Ut odio viverra, molestie lectus nec, venenatis turpis.</div></html>" }
+                }
+            };
+
+            byte[] pdf = converter.Convert(doc);
+
+            ByteArrayToFile("dupa.pdf", pdf);
+        }
+
+        public static void ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(byteArray, 0, byteArray.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+            }
+        }
+    }
+}
---
+++ 02_APIs/HtmlToPdf_08_TuesPechkin_CV_Spike/HtmlToPdf/Models/Literature.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HtmlToPdf.Models
+{
+    public class Literature
+    {
+        public int LiteratureID { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Url { get; set; }
+        public ICollection<LiteratureTechnology> LiteraturesTechnologies { get; set; }
+    }
+}
