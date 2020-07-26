bbe41fb7e5f5b07c8192ca3df4fd19a5270646aa
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-07-26 08:33:20 +0200

    Angular and ASP.NET Core trials;

---
+++ 24_Angular_js/Angular_Freeman_05_NewSample_Base/src/app/component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { Model } from "./repository.model";
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+}
