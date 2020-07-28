ae3c7639b599b7a047ca0e32f19fa7f6d00310ab
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-07-28 06:32:12 +0200

    Angular trials;

---
+++ 24_Angular_js/Angular_00c_ASP.NET_WebAPI_Setup/WebApp/WebApp/Data/Models/Country.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Data.Models
+{
+    public class Country
+    {
+        #region Constructor
+        public Country()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The unique id and primary key for this Country
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Country name (in UTF8 format)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-2 format)
+        /// </summary>
+        [JsonProperty("iso2")]
+        public string ISO2 { get; set; }
+
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-3 format)
+        /// </summary>
+        [JsonProperty("iso3")]
+        public string ISO3 { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// A list containing all the cities related to this country.
+        /// </summary>
+        public virtual List<City> Cities { get; set; }
+        #endregion
+    }
+}
