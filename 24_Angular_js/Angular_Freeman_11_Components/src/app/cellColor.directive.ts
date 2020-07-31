09f78b9868722597fa90a828605f522eedca4ffa
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-07-31 23:26:13 +0200

    Playing with Angular;

---
+++ 24_Angular_js/Angular_Freeman_11_Components/src/app/cellColor.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, HostBinding } from "@angular/core";
+@Directive({
+    selector: "td"
+})
+export class PaCellColor {
+    @HostBinding("class")
+    bgClass: string = "";
+    setColor(dark: Boolean) {
+        this.bgClass = dark ? "bg-dark" : "";
+    }
+}
---
+++ 24_Angular_js/Angular_Freeman_12_Pipelines/src/app/toggleView.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "paToggleView",
+    templateUrl: "toggleView.component.html"
+})
+export class PaToggleView {
+    showContent: boolean = true;
+}
