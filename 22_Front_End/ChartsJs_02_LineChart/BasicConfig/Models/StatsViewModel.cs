c6f38f50784b430472130ebffc03cce1cf560b49
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-10-15 17:02:59 +0200

    Charts.js linear chart example;

---
+++ 22_Front_End/ChartsJs_02_LineChart/BasicConfig/Models/StatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicConfig.Models.ViewModels
+{
+    public class StatsViewModel
+    {
+        public DateTime Date { get; set; }
+        public int Moving { get; set; }
+        public int NotMoving { get; set; }
+        public int Missing { get; set; }
+        public int Expired { get; set; }
+    }
+}
