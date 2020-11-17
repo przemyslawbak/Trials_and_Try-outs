b3b8752a4249ecef0428c6f564e05d7e0a1168b7
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-17 22:54:42 +0100

    Added Angular drag and drop GH working sample;

---
+++ 24_Angular_js/Angular_Tutorial_04_Material_DropBoundary/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  // Transfer Items Between Lists
+  MoviesList = [
+    'The Far Side of the World',
+    'Morituri',
+    'Napoleon Dynamite',
+    'Pulp Fiction',
+    'Blade Runner',
+    'Cool Hand Luke',
+    'Heat',
+    'Juice',
+  ];
+
+  MoviesWatched = [];
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
