a4216941ea527e02acd1dd72dd9dd1b1eaae7079
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-12-04 23:37:55 +0100

    Small updates in code;

--- 12_String/String_19_DistinctFromCsv/List_Comparer/Program.cs
+++ 12_String/String_19_DistinctFromCsv/List_Comparer/Program.cs
@@ -10,6 +10,7 @@ namespace List_Comparer
     {
         static void Main(string[] args)
         {
+            List<string> failedEmails = new List<string>(File.ReadAllLines("failed.txt"));
             string[] keywords = new string[] { "info", "office", "conta", "admission", "agen", "career", "appl", "crew", "email", "enquir", "general", "hr@", "cv@", "jobs", "mail@", "recruit", "general", "marine", "offshore", "personnel", "resume" };
             string[] blacklist = new string[] { "ir@", "admin", "corporate", "spam", "account", "invoice", "charter", "book", "business", "cruise", "customer", "protection", "invest", "financ", "firstname", "lastname", "freight", "hotel", "airport", "it@", "library", "marketing", "media", "privacy", "proxy", "purchasing", "reservations", "sales", "webmaster"};
             List<string> items = new List<string>();
@@ -22,13 +23,16 @@ namespace List_Comparer
                 .ToList();
 
             List<EmailModel> containBlacklisted = distEmail.Where(e => blacklist.Any(k => e.Email.ToLower().Contains(k.ToLower()))).ToList();
-            var withNoBlacklisted = distEmail.Except(containBlacklisted).ToList();
+            List<EmailModel> containFailed = distEmail.Where(e => failedEmails.Any(k => e.Email.ToLower().Contains(k.ToLower()))).ToList();
 
-            List<EmailModel> containKeys = withNoBlacklisted.Where(e => keywords.Any(k => e.Email.ToLower().Contains(k.ToLower()))).ToList();
+            distEmail = distEmail.Except(containBlacklisted).ToList();
+            distEmail = distEmail.Except(containFailed).ToList();
 
-            List<EmailModel> notContainKeysFirst = withNoBlacklisted.Except(containKeys).GroupBy(c => c.Company).Select(c => c.ElementAt(0)).ToList();
-            List<EmailModel> notContainKeysSecond = withNoBlacklisted.Except(containKeys).GroupBy(c => c.Company).Where(x => x.Count() > 1).Select(c => c.ElementAt(1)).ToList();
-            List<EmailModel> notContainKeysThird = withNoBlacklisted.Except(containKeys).GroupBy(c => c.Company).Where(x => x.Count() > 2).Select(c => c.ElementAt(2)).ToList();
+            List<EmailModel> containKeys = distEmail.Where(e => keywords.Any(k => e.Email.ToLower().Contains(k.ToLower()))).ToList();
+
+            List<EmailModel> notContainKeysFirst = distEmail.Except(containKeys).GroupBy(c => c.Company).Select(c => c.ElementAt(0)).ToList();
+            List<EmailModel> notContainKeysSecond = distEmail.Except(containKeys).GroupBy(c => c.Company).Where(x => x.Count() > 1).Select(c => c.ElementAt(1)).ToList();
+            List<EmailModel> notContainKeysThird = distEmail.Except(containKeys).GroupBy(c => c.Company).Where(x => x.Count() > 2).Select(c => c.ElementAt(2)).ToList();
 
             List<string> final = new List<string>();
             final.AddRange(containKeys.Select(l => l.Email));
