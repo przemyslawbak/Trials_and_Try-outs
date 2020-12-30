8cb927e1e736b9d6784ca1513c5c0f82fe6b1ca4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-12-30 23:19:49 +0100

    Added sample project for radio buttons in Angular;

---
+++ 24_Angular_js/Angular_Trials_07_RadioBtns_1/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form = new FormGroup({
+    gender: new FormControl('', Validators.required),
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
