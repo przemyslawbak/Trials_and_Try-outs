fb191080051394bb4719b23ece711e8f902c7d4c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-04 15:24:39 +0100

    Added small program that removes strings from one list, if they contain string from another list;

---
+++ 12_String/String_21_BlacklistedDomains/List_Comparer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>(File.ReadAllLines("1.txt"));
+            List<string> blacklisted = new List<string>(File.ReadAllLines("blacklisted.txt"));
+
+            lista.RemoveAll(x => blacklisted.Any(d => x.Contains(d)));
+            File.WriteAllLines("output.txt", lista);
+            Console.WriteLine("saved");
+            Console.ReadKey();
+
+        }
+    }
+}
