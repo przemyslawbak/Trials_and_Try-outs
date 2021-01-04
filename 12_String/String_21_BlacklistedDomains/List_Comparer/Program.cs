eb6f9ff7d2dcb3a0ff0a0e12a2a142a3183373e3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-04 22:28:56 +0100

    Corrected bug when excepting from the list;

--- 12_String/String_21_BlacklistedDomains/List_Comparer/Program.cs
+++ 12_String/String_21_BlacklistedDomains/List_Comparer/Program.cs
@@ -12,8 +12,8 @@ namespace List_Comparer
             List<string> blacklisted = new List<string>(File.ReadAllLines("2.txt"));
             List<string> blacklist = new List<string>(File.ReadAllLines("3.txt"));
             List<string> lista = new List<string>(File.ReadAllLines("1.txt"));
-            List<string> containBlacklisted = lista.Where(e => blacklist.Any(k => e.ToLower().Contains(k.ToLower()))).ToList();
-            lista.Except(containBlacklisted).ToList();
+            List<string> containBlacklisted = lista.Where(e => blacklist.Any(k => e.Contains(k))).ToList();
+            lista = lista.Except(containBlacklisted).ToList();
 
             lista.RemoveAll(x => blacklisted.Any(d => x.ToLower().Contains(d.ToLower())));
             File.WriteAllLines("output.txt", lista);
