d036e7c8646e9696678cbb555e518bd974a65553
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-07 14:44:48 +0100

    Added statc method wrapper example for unit testing;

---
+++ 15_UnitTests/Unit_Test_16_MockingStaticMethod_1Wrapper/UnitTest_/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTest_
+{
+    //http://adventuresdotnet.blogspot.com/2011/03/mocking-static-methods-for-unit-testing.html
+    public class Program
+    {
+        IStaticWrapper _wrapper;
+        public Program(IStaticWrapper wrapper)
+        {
+            _wrapper = wrapper;
+        }
+        static void Main(string[] args)
+        {
+        }
+
+        public int SomeMethod(string input)
+        {
+            var value = _wrapper.SomeStaticMethod(input);
+
+            return value ? 1 : 0;
+        }
+    }
+}
---
+++ 15_UnitTests/Unit_Test_16_MockingStaticMethod_1Wrapper/UnitTest_/StaticWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTest_
+{
+    public class StaticWrapper : IStaticWrapper
+    {
+        public bool SomeStaticMethod(string input)
+        {
+            return SomeStaticClass.SomeStaticMethod(input);
+        }
+    }
+}
