7a6d2cfd397d7312f9a60a209efc597bec50f272
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-02-21 08:36:55 +0100

    In String_12 project corrected model's typo;

--- 12_String/String_19_DistinctFromCsv/List_Comparer/EmailModel.cs
+++ 12_String/String_19_DistinctFromCsv/List_Comparer/EmailModel.cs
@@ -3,7 +3,7 @@
     public class EmailModel
     {
         public string Email { get; set; }
-        public bool? Rely { get; set; }
+        public bool? Relay { get; set; }
         public string Company { get; set; }
         public bool? Delete { get; set; }
     }
