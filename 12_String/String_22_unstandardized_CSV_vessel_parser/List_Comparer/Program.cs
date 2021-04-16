50e729824d65d5ad903d52fb393943b4cc7ab8d2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-04-16 19:55:39 +0200

    Added CSV parser;

---
+++ 12_String/String_22_unstandardized_CSV_vessel_parser/List_Comparer/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputLines = new List<string>(File.ReadAllLines("_input.csv"));
+
+            for (int i = 0; i < inputLines.Count; i++)
+            {
+                if (inputLines[i][0] == '1')
+                {
+                    string[] shipLines = new[] {
+                        inputLines[i],
+                        inputLines[i + 1],
+                        inputLines[i + 2],
+                        inputLines[i + 3],
+                        inputLines[i + 4],
+                        inputLines[i + 5],
+                        inputLines[i + 6],
+                        inputLines[i + 7],
+                        inputLines[i + 8],
+                        inputLines[i + 9],
+                        inputLines[i + 10],
+                        inputLines[i + 11]
+                    };
+
+                    ParseShipAndSave(shipLines);
+                }
+            }
+
+        }
+
+        private static void ParseShipAndSave(string[] shipLines)
+        {
+            ShipModel ship = new ShipModel();
+            List<string> cells = new List<string>();
+            List<KeyValuePair<int, string>> pairs = new List<KeyValuePair<int, string>>();
+
+            foreach (string line in shipLines)
+            {
+                cells.AddRange(line.Split(';'));
+            }
+
+            string[] cellHeads = new string[] { "1", "2", "3", "4", "5", "6", "11" };
+            for (int i = 0; i < cells.Count; i++)
+            {
+                if (cellHeads.Any(cells[i].Equals))
+                {
+                    pairs.Add(new KeyValuePair<int, string>(int.Parse(cells[i]), cells[i + 1]));
+                }
+            }
+
+            //1
+            ship.RegisterNo = pairs.Where(kvp => kvp.Key == 1).ToList()[0].Value;
+            ship.Name = pairs.Where(kvp => kvp.Key == 1).ToList()[1].Value;
+            ship.GRT = pairs.Where(kvp => kvp.Key == 1).ToList()[2].Value;
+            ship.Hull = pairs.Where(kvp => kvp.Key == 1).ToList()[3].Value;
+            ship.YOB = pairs.Where(kvp => kvp.Key == 1).ToList()[4].Value.Split('-')[0];
+            ship.Shipbuilder = pairs.Where(kvp => kvp.Key == 1).ToList()[5].Value.Split(',')[0];
+            ship.BuilderCountry = pairs.Where(kvp => kvp.Key == 1).ToList()[5].Value.Split(',')[1];
+
+            //2
+            ship.Imo = pairs.Where(kvp => kvp.Key == 2).ToList()[0].Value;
+            ship.NET = pairs.Where(kvp => kvp.Key == 2).ToList()[2].Value;
+            ship.MeType = pairs.Where(kvp => kvp.Key == 2).ToList()[3].Value;
+
+            //3
+            ship.Flag = pairs.Where(kvp => kvp.Key == 3).ToList()[0].Value;
+            ship.DWT = pairs.Where(kvp => kvp.Key == 3).ToList()[1].Value;
+            ship.Type = pairs.Where(kvp => kvp.Key == 3).ToList()[1].Value; //???????????????????
+            ship.Manager = pairs.Where(kvp => kvp.Key == 3).ToList()[4].Value;
+
+            //4
+            ship.LBP = pairs.Where(kvp => kvp.Key == 4).ToList()[0].Value;
+            ship.CallSign = pairs.Where(kvp => kvp.Key == 4).ToList()[2].Value;
+            ship.MePower = pairs.Where(kvp => kvp.Key == 4).ToList()[3].Value;
+            ship.Owner = pairs.Where(kvp => kvp.Key == 4).ToList()[4].Value;
+
+            //5
+            ship.LOA = pairs.Where(kvp => kvp.Key == 5).ToList()[0].Value;
+            ship.DecksNo = pairs.Where(kvp => kvp.Key == 5).ToList()[0].Value; //???????????????????
+            ship.Boilers = pairs.Where(kvp => kvp.Key == 5).ToList()[3].Value;
+            ship.PortOfRegistry = pairs.Where(kvp => kvp.Key == 5).ToList()[4].Value;
+
+            //6
+            ship.Breadth = pairs.Where(kvp => kvp.Key == 6).ToList()[0].Value;
+            ship.Speed = pairs.Where(kvp => kvp.Key == 6).ToList()[1].Value; //???????????????????
+            ship.Mmsi = pairs.Where(kvp => kvp.Key == 6).ToList()[2].Value;
+            ship.Generators = pairs.Where(kvp => kvp.Key == 6).ToList()[3].Value;
+
+            //7
+            ship.Depth = pairs.Where(kvp => kvp.Key == 7).ToList()[0].Value;
+            ship.HoldsNo = pairs.Where(kvp => kvp.Key == 7).ToList()[1].Value;
+
+            //8
+            ship.Draught = pairs.Where(kvp => kvp.Key == 8).ToList()[0].Value;
+
+            //11
+            ship.CargoHandlingEquipment = pairs.Where(kvp => kvp.Key == 11).ToList()[0].Value;
+        }
+    }
+}
---
+++ 12_String/String_22_unstandardized_CSV_vessel_parser/List_Comparer/ShipModel.cs
@@ -0,0 +1,54 @@
+﻿namespace List_Comparer
+{
+    public class ShipModel
+    {
+        public string Name { get; set; }//
+        public string Type { get; set; }
+        public string RegisterNo { get; set; }
+        public string Imo { get; set; }
+        public string CallSign { get; set; }
+        public string Flag { get; set; }
+        public string PortOfRegistry { get; set; }
+        public string Owner { get; set; }
+        public string Manager { get; set; }
+        public string Propulsion { get; set; }
+        public string Hull { get; set; }
+        public string Machinery { get; set; }
+        public string AnchorEquipment { get; set; }
+        public string GRT { get; set; }
+        public string NET { get; set; }
+        public string DWT { get; set; }
+        public string LOA { get; set; }
+        public string LBP { get; set; }
+        public string Breadth { get; set; }
+        public string Depth { get; set; }
+        public string Draught { get; set; }
+        public string SFB { get; set; }
+        public string Shipbuilder { get; set; }
+        public string BuilderCountry { get; set; }
+        public string YOB { get; set; }
+        public string HullMaterial { get; set; }
+        public string DecksNo { get; set; }
+        public string HoldsNo { get; set; }
+        public string MePower { get; set; }
+        public string MeType { get; set; }
+        public string MeMaker { get; set; }
+        public string PropsNo { get; set; }
+        public string Generators { get; set; }
+        public string Boilers { get; set; }
+        public string AirCompressor { get; set; }
+        public string Speed { get; set; }
+        public string DeckErections { get; set; }
+        public string Bulkheads { get; set; }
+        public string Ballast { get; set; }
+        public string BoilerMaker { get; set; }
+        public string CargoHandlingEquipment { get; set; }
+        public string CargoSpaceVolume { get; set; }
+        public string RadioEquipment { get; set; }
+        public string TechManager { get; set; }
+        public string Status { get; set; }
+        public string Mmsi { get; set; }
+        public string OwnerAddress { get; set; }
+        public string ManagerAddress { get; set; }
+    }
+}
