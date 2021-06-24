3eb50ec28d1a2ea68ce84f225b8902fc662847f9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-06-24 07:48:03 +0200

    Added first program from Expert F# 4.0 book;

---
+++ 26_F#/Book_ExpertF_01_FirstProgram/Book_ExpertF_01_FirstProgram/Program.fs
@@ -0,0 +1,15 @@
+﻿let splitAtSpaces (text: string) =
+    text.Split ' '
+    |> Array.toList
+/// Analyze a string for duplicate words.
+let wordCount text =
+    let words = splitAtSpaces text
+    let numWords = words.Length
+    let distinctWords = List.distinct words
+    let numDups = numWords - distinctWords.Length
+    (numWords, numDups)
+/// Analyze a string for duplicate words and display the results.
+let showWordCount text =
+    let numWords, numDups = wordCount text
+    printfn "--> %d words in the text" numWords
+    printfn "--> %d duplicate words" numDups
