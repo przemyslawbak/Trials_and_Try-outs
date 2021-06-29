41419e7a8edbe8dcb27a62c15b64f493dd85c3c8
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-06-29 19:46:58 +0200

    Added example of SVG map with areas;

---
+++ 27_SVG/Map_01_Trial/BasicConfig/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicConfig.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
