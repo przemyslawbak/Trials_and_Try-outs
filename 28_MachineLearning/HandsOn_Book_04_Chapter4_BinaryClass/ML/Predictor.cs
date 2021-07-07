1387a028354d55a68ec474f11bfd53c5f0955f2c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-07-07 14:42:25 +0200

    Commenting ML binary class app;

--- 28_MachineLearning/HandsOn_Book_04_Chapter4_BinaryClass/ML/Predictor.cs
+++ 28_MachineLearning/HandsOn_Book_04_Chapter4_BinaryClass/ML/Predictor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-
 using chapter04.ML.Base;
 using chapter04.ML.Objects;
 
@@ -42,12 +41,20 @@ namespace chapter04.ML
                 return;
             }
 
-            var predictionEngine = MlContext.Model.CreatePredictionEngine<CarInventory, CarInventoryPrediction>(mlModel);
+            //1. The first change is in the prediction call itself. As you probably guessed, the TSrc
+            //and TDst arguments need to be adjusted to utilize both of the new classes we
+            //created, CarInventory and CarInventoryPrediction, like this
+            PredictionEngine<CarInventory, CarInventoryPrediction> predictionEngine = MlContext.Model.CreatePredictionEngine<CarInventory, CarInventoryPrediction>(mlModel);
 
             var json = File.ReadAllText(inputDataFile);
 
-            var prediction = predictionEngine.Predict(JsonConvert.DeserializeObject<CarInventory>(json));
+            //2. Given that we are no longer simply passing in the string and building an object
+            //on the fly, we need to first read in the file as text.We then deserialize the JSON
+            //into our CarInventory object, as follows
+            CarInventoryPrediction prediction = predictionEngine.Predict(JsonConvert.DeserializeObject<CarInventory>(json));
 
+            //3. Lastly, we need to adjust the output of our prediction to match our
+            //new CarInventoryPrediction properties, like this
             Console.WriteLine(
                                 $"Based on input json:{System.Environment.NewLine}" +
                                 $"{json}{System.Environment.NewLine}" + 
--- 28_MachineLearning/HandsOn_Book_04_Chapter4_BinaryClass/ML/Trainer.cs
+++ 28_MachineLearning/HandsOn_Book_04_Chapter4_BinaryClass/ML/Trainer.cs
@@ -9,6 +9,10 @@ using Microsoft.ML;
 
 namespace chapter04.ML
 {
+    /// <summary>
+    /// The FastTree trainer doesn't require normalization but does require all of the feature
+    /// columns to use a float variable type and the label column to be a bool variable type.
+    /// </summary>
     public class Trainer : BaseML
     {
         public void Train(string trainingFileName, string testFileName)
@@ -20,6 +24,8 @@ namespace chapter04.ML
                 return;
             }
 
+            //1. The first change is the check to ensure the test filename exists, shown in the
+            //following code block
             if (!File.Exists(testFileName))
             {
                 Console.WriteLine($"Failed to find test data file ({testFileName}");
@@ -27,37 +33,44 @@ namespace chapter04.ML
                 return;
             }
 
-            var trainingDataView = MlContext.Data.LoadFromTextFile<CarInventory>(trainingFileName, ',', hasHeader: false);
+            IDataView trainingDataView = MlContext.Data.LoadFromTextFile<CarInventory>(trainingFileName, ',', hasHeader: false);
 
+            //2. We then build the data process pipeline using the NormalizeMeanVariance
+            //transform method we used in Chapter 3, Regression Model, on the inputted
+            //values, like this
             IEstimator<ITransformer> dataProcessPipeline = MlContext.Transforms.Concatenate("Features",
                 typeof(CarInventory).ToPropertyList<CarInventory>(nameof(CarInventory.Label)))
                 .Append(MlContext.Transforms.NormalizeMeanVariance(inputColumnName: "Features",
                     outputColumnName: "FeaturesNormalizedByMeanVar"));
 
-            var trainer = MlContext.BinaryClassification.Trainers.FastTree(labelColumnName: nameof(CarInventory.Label),
+            //3. We can then create the FastTree trainer with the label from the CarInventory
+            //class and the normalized mean variance, as follows
+            Microsoft.ML.Trainers.FastTree.FastTreeBinaryTrainer trainer = MlContext.BinaryClassification.Trainers.FastTree(labelColumnName: nameof(CarInventory.Label),
                 featureColumnName: "FeaturesNormalizedByMeanVar",
                 numberOfLeaves: 2,
                 numberOfTrees: 1000,
                 minimumExampleCountPerLeaf: 1,
                 learningRate: 0.2);
 
-            var trainingPipeline = dataProcessPipeline.Append(trainer);
-
-            var trainedModel = trainingPipeline.Fit(trainingDataView);
+            //4. Lastly, we call the Regression.Evaluate method to provide regression-specific
+            //metrics, followed by a Console.WriteLine call to provide these metrics to your
+            //console output. We will go into detail about what each of these means in the last
+            //section of the chapter, but for now, the code can be seen here
+            Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Data.BinaryPredictionTransformer<Microsoft.ML.Calibrators.CalibratedModelParametersBase<Microsoft.ML.Trainers.FastTree.FastTreeBinaryModelParameters, Microsoft.ML.Calibrators.PlattCalibrator>>> trainingPipeline = dataProcessPipeline.Append(trainer);
+            Microsoft.ML.Data.TransformerChain<Microsoft.ML.Data.BinaryPredictionTransformer<Microsoft.ML.Calibrators.CalibratedModelParametersBase<Microsoft.ML.Trainers.FastTree.FastTreeBinaryModelParameters, Microsoft.ML.Calibrators.PlattCalibrator>>> trainedModel = trainingPipeline.Fit(trainingDataView);
             MlContext.Model.Save(trainedModel, trainingDataView.Schema, ModelPath);
 
-            var evaluationPipeline = trainedModel.Append(MlContext.Transforms
+            //Now, we evaluate the model we just trained
+            Microsoft.ML.Data.TransformerChain<Microsoft.ML.Transforms.FeatureContributionCalculatingTransformer> evaluationPipeline = trainedModel.Append(MlContext.Transforms
                 .CalculateFeatureContribution(trainedModel.LastTransformer)
                 .Fit(dataProcessPipeline.Fit(trainingDataView).Transform(trainingDataView)));
-
-            var testDataView = MlContext.Data.LoadFromTextFile<CarInventory>(testFileName, ',', hasHeader: false);
-
-            var testSetTransform = evaluationPipeline.Transform(testDataView);
-
-            var modelMetrics = MlContext.BinaryClassification.Evaluate(data: testSetTransform,
+            IDataView testDataView = MlContext.Data.LoadFromTextFile<CarInventory>(testFileName, ',', hasHeader: false);
+            IDataView testSetTransform = evaluationPipeline.Transform(testDataView);
+            Microsoft.ML.Data.CalibratedBinaryClassificationMetrics modelMetrics = MlContext.BinaryClassification.Evaluate(data: testSetTransform,
                 labelColumnName: nameof(CarInventory.Label),
                 scoreColumnName: "Score");
 
+            //Finally, we output all of the classification metrics
             Console.WriteLine($"Accuracy: {modelMetrics.Accuracy:P2}");
             Console.WriteLine($"Area Under Curve: {modelMetrics.AreaUnderRocCurve:P2}");
             Console.WriteLine($"Area under Precision recall Curve: {modelMetrics.AreaUnderPrecisionRecallCurve:P2}");
