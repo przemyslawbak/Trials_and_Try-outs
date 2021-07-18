247a2db2b49a1be3129101a4944aa26861bbefec
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-07-18 16:49:18 +0200

    Added new example project for TA indicators;

---
+++ 29_Financial/Trial_01_SimpleIndicator/Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        private static readonly string _dataFilename = "jsw_d.csv";
+        static void Main(string[] args)
+        {
+            string dataPath = Path.Combine(AppContext.BaseDirectory, "Data", _dataFilename);
+            using (StreamReader reader = new StreamReader(dataPath))
+            {
+                List<string> listA = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(';');
+
+                    listA.Add(values[4]);
+                }
+            }
+        }
+    }
+}
