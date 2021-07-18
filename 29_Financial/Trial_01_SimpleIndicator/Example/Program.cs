aa6c9ec7f2800cc00e78337ab84fc60dbbf90db2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-07-18 19:41:32 +0200

    Working on basic TA indicator with Stock.Indicators;

--- 29_Financial/Trial_01_SimpleIndicator/Example/Program.cs
+++ 29_Financial/Trial_01_SimpleIndicator/Example/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Skender.Stock.Indicators;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -9,18 +10,41 @@ namespace Example
         private static readonly string _dataFilename = "jsw_d.csv";
         static void Main(string[] args)
         {
+            IEnumerable<Quote> quotes = GetHistory();
+            IEnumerable<SmaResult> results = quotes.GetSma(10);
+        }
+
+        private static IEnumerable<Quote> GetHistory()
+        {
             string dataPath = Path.Combine(AppContext.BaseDirectory, "Data", _dataFilename);
+            List<Quote> quotes = new List<Quote>();
+
             using (StreamReader reader = new StreamReader(dataPath))
             {
-                List<string> listA = new List<string>();
                 while (!reader.EndOfStream)
                 {
                     string line = reader.ReadLine();
                     string[] values = line.Split(';');
-
-                    listA.Add(values[4]);
+                    try
+                    {
+                        quotes.Add(new Quote()
+                        {
+                            Date = DateTime.Parse(values[0]),
+                            Open = decimal.Parse(values[1]),
+                            High = decimal.Parse(values[2]),
+                            Low = decimal.Parse(values[3]),
+                            Close = decimal.Parse(values[4]),
+                            Volume = decimal.Parse(values[5]),
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        //
+                    }
                 }
             }
+
+            return quotes;
         }
     }
 }
