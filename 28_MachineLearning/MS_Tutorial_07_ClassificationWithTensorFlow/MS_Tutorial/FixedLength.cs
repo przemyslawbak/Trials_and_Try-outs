c02c3e9e5a7d606f94d0aab4d2114170d963d5a2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-07-18 09:31:17 +0200

    ML with TensorFlow pre-trained model example;

---
+++ 28_MachineLearning/MS_Tutorial_07_ClassificationWithTensorFlow/MS_Tutorial/FixedLength.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+namespace MS_Tutorial
+{
+    /// <summary>
+    /// Class to hold the fixed length feature vector. Used to define the
+    /// column names used as output from the custom mapping action,
+    /// </summary>
+    public class FixedLength
+    {
+        public const int FeatureLength = 600;
+
+        /// <summary>
+        /// This is a fixed length vector designated by VectorType attribute.
+        /// </summary>
+        [VectorType(FeatureLength)]
+        public int[] Features { get; set; }
+    }
+}
