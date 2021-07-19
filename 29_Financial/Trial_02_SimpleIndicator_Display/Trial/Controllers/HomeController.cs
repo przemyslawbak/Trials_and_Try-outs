17057df87dc684fbaba58d5797188f67d89e7ce7
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-07-19 08:46:05 +0200

    Added new service methods;

--- 29_Financial/Trial_02_SimpleIndicator_Display/Trial/Controllers/HomeController.cs
+++ 29_Financial/Trial_02_SimpleIndicator_Display/Trial/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using Skender.Stock.Indicators;
+using System.Collections.Generic;
+using System.Linq;
+using Trial.Services;
 
 namespace Trial.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IFileReader _fileReader;
+        private readonly ITaCalculator _taCalculator;
+
+        public HomeController(IFileReader fileReader, ITaCalculator taCalculator)
+        {
+            _fileReader = fileReader;
+            _taCalculator = taCalculator;
+        }
+
         public IActionResult Index()
         {
+            List<Quote> history = _fileReader.GetHistory();
+            List<Quote> quotes = _taCalculator.GetQuotes(history);
+            List<SmaResult> results = quotes.GetSma(10).ToList();
+
             return View();
         }
     }
--- 29_Financial/Trial_02_SimpleIndicator_Display/Trial/Services/ITaCalculator.cs
+++ 29_Financial/Trial_02_SimpleIndicator_Display/Trial/Services/ITaCalculator.cs
@@ -1,6 +1,10 @@
-﻿namespace Trial.Services
+﻿using Skender.Stock.Indicators;
+using System.Collections.Generic;
+
+namespace Trial.Services
 {
     public interface ITaCalculator
     {
+        List<Quote> GetQuotes(List<Quote> history);
     }
 }
