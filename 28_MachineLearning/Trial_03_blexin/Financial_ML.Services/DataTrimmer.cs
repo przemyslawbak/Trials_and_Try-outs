542e4dc8c5990e8db0b03408d182129d9655124b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-06 14:07:31 +0200

    New sample project added;

---
+++ 28_MachineLearning/Trial_03_blexin/Financial_ML.Services/DataTrimmer.cs
@@ -0,0 +1,17 @@
+﻿using Financial_ML.ViewModels;
+using System;
+using System.Linq;
+
+namespace Financial_ML.Services
+{
+    public class DataTrimmer : IDataTrimmer
+    {
+        public ResultsDisplay TrimList(ResultsDisplay display, int records)
+        {
+            return new ResultsDisplay()
+            {
+                AllTotalQuotes = display.AllTotalQuotes.Skip(Math.Max(0, display.AllTotalQuotes.Count - 50)).ToList()
+            };
+        }
+    }
+}
