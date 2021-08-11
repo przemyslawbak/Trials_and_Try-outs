045559b78fed07e21b7861291f0ebd4d1a3236c9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-11 10:18:01 +0200

    Testing System.Activator;

---
+++ 25_Other/Other_03_Activator/Activator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Activator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize array of characters from a to z.
+            char[] chars = new char[26];
+            for (int ctr = 0; ctr < 26; ctr++)
+                chars[ctr] = (char)(ctr + 0x0061);
+            object obj = System.Activator.CreateInstance(typeof(string), new object[] { chars, 13, 10 });
+            Console.WriteLine(obj);
+        }
+    }
+}
