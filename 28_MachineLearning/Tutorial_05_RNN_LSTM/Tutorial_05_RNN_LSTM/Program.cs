bcb4a36174fa2905167a084b7ac1c8e3a9dc4d02
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-18 23:24:49 +0200

    New ML RNN LSTM sample project added;

---
+++ 28_MachineLearning/Tutorial_05_RNN_LSTM/Tutorial_05_RNN_LSTM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial_05_RNN_LSTM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
