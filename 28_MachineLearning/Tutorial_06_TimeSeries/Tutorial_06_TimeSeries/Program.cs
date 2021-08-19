37874070713a6ca14917ae93da3b0f25aeb8cab0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-19 10:14:23 +0200

    Completed time-series tutorial;

--- 28_MachineLearning/Tutorial_06_TimeSeries/Tutorial_06_TimeSeries/Program.cs
+++ 28_MachineLearning/Tutorial_06_TimeSeries/Tutorial_06_TimeSeries/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms.TimeSeries;
+using System;
 
 namespace Tutorial_06_TimeSeries
 {
@@ -6,7 +8,31 @@ namespace Tutorial_06_TimeSeries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MLContext context = new MLContext();
+
+            IDataView data = context.Data.LoadFromTextFile<EnergyData>("./energy_hourly.csv",
+                hasHeader: true, separatorChar: ',');
+
+            SsaForecastingEstimator pipeline = context.Forecasting.ForecastBySsa(
+                nameof(EnergyForecast.Forecast),
+                nameof(EnergyData.Energy),
+                windowSize: 500,
+                seriesLength: 1000,
+                trainSize: 10000,
+                horizon: 400);
+
+            SsaForecastingTransformer model = pipeline.Fit(data);
+
+            TimeSeriesPredictionEngine<EnergyData, EnergyForecast> forecastingEngine = model.CreateTimeSeriesEngine<EnergyData, EnergyForecast>(context);
+
+            EnergyForecast forecasts = forecastingEngine.Predict();
+
+            foreach (float forecast in forecasts.Forecast)
+            {
+                Console.WriteLine(forecast);
+            }
+
+            Console.ReadLine();
         }
     }
 }
