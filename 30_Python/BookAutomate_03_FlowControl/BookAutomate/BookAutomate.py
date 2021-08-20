a457fa8765306b4bd67bf00ab907d75e14c0c370
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-20 10:22:21 +0200

    Added new sample for Python's flow control;

---
+++ 30_Python/BookAutomate_03_FlowControl/BookAutomate/BookAutomate.py
@@ -0,0 +1,10 @@
+name = 'Mary'
+password = 'swordfish'
+if name == 'Mary':
+    print('Hello, Mary')
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
+
+print("finito")
