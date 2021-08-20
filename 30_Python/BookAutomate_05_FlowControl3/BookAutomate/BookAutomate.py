c3df8f9ca9a2f83fc9b86218a514e2cec19e354c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-20 12:33:05 +0200

    3 new Python samples added

---
+++ 30_Python/BookAutomate_05_FlowControl3/BookAutomate/BookAutomate.py
@@ -0,0 +1,10 @@
+print('My name is')
+for i in range(5):
+    print('Bak Five Times (' + str(i) + ')')
+
+for i in range(12, 16):
+    print(i)
+
+#0 - from; 10 - to; 2 - step;
+for i in range(0, 10, 2):
+    print(i)
