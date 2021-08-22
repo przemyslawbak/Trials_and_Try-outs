14177fc9e5d08ef211d74d4c55beb77d50a2b32e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-22 11:41:50 +0200

    New TF.NET sample project created;

---
+++ 28_MachineLearning/TenserFlow_01_HelloWorld/Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static Tensorflow.Binding;
+
+namespace Sample
+{
+    //https://tensorflownet.readthedocs.io/en/latest/HelloWorld.html
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tensorflow.Tensor hello = tf.constant("Hello, TensorFlow!");
+            Console.WriteLine(hello);
+            Console.ReadKey();
+        }
+    }
+}
