e3c5ad17a2f55a4a7f3048bc07a5a14275f29f71
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-23 14:08:25 +0200

    Working on web app in Flask;

--- 30_Python/BookHeadFirst_05_WebApp_Flask/Sample/Sample.py
+++ 30_Python/BookHeadFirst_05_WebApp_Flask/Sample/Sample.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-@app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')      
-app.run()
+@app.route('/search4', methods=['POST'])
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+app.run(debug = True)
+
+#!!!!!!!!not completed
