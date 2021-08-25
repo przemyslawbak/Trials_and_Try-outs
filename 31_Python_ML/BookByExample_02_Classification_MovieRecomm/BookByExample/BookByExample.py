e643eb2d7a529f856b37242ebc308a5f5fa2a3e7
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-25 21:31:19 +0200

    Updated classification example;

--- 31_Python_ML/BookByExample_02_Classification_MovieRecomm/BookByExample/BookByExample.py
+++ 31_Python_ML/BookByExample_02_Classification_MovieRecomm/BookByExample/BookByExample.py
@@ -1,5 +1,14 @@
 import numpy as np
 from collections import defaultdict
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+
 data_path = 'ml-1m/ratings.dat'
 n_users = 6040
 n_movies = 3706
@@ -47,3 +56,89 @@ X = X_raw[Y_raw > 0]
 Y = Y_raw[Y_raw > 0]
 display_distribution(Y)
 
+recommend = 3
+Y[Y <= recommend] = 0
+Y[Y > recommend] = 1
+n_pos = (Y == 1).sum()
+n_neg = (Y == 0).sum()
+print(f'{n_pos} positive samples and {n_neg} negative samples.')
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+clf = MultinomialNB(alpha=1.0, fit_prior=True)
+clf.fit(X_train, Y_train)
+prediction_prob = clf.predict_proba(X_test)
+print(prediction_prob[0:10])
+prediction = clf.predict(X_test)
+print(prediction[:10])
+accuracy = clf.score(X_test, Y_test)
+print(f'The accuracy is: {accuracy*100:.1f}%')
+print(confusion_matrix(Y_test, prediction, labels=[0, 1]))
+precision_score(Y_test, prediction, pos_label=1)
+recall_score(Y_test, prediction, pos_label=1)
+f1_score(Y_test, prediction, pos_label=1)
+f1_score(Y_test, prediction, pos_label=0)
+report = classification_report(Y_test, prediction)
+print(report)
+
+pos_prob = prediction_prob[:, 1]
+thresholds = np.arange(0.0, 1.1, 0.05)
+true_pos, false_pos = [0]*len(thresholds), [0]*len(thresholds)
+for pred, y in zip(pos_prob, Y_test):
+    for i, threshold in enumerate(thresholds):
+        if pred >= threshold:
+        # if truth and prediction are both 1
+            if y == 1:
+                true_pos[i] += 1
+            # if truth is 0 while prediction is 1
+            else:
+                false_pos[i] += 1
+        else:
+            break
+
+n_pos_test = (Y_test == 1).sum()
+n_neg_test = (Y_test == 0).sum()
+
+true_pos_rate = [tp / n_pos_test for tp in true_pos]
+false_pos_rate = [fp / n_neg_test for fp in false_pos]
+
+plt.figure()
+lw = 2
+plt.plot(false_pos_rate, true_pos_rate,
+color='darkorange', lw=lw)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+roc_auc_score(Y_test, pos_prob)
+k = 5
+k_fold = StratifiedKFold(n_splits=k, random_state=42)
+smoothing_factor_option = [1, 2, 3, 4, 5, 6]
+fit_prior_option = [True, False]
+auc_record = {}
+
+for train_indices, test_indices in k_fold.split(X, Y):
+    X_train, X_test = X[train_indices], X[test_indices]
+    Y_train, Y_test = Y[train_indices], Y[test_indices]
+    for alpha in smoothing_factor_option:
+        if alpha not in auc_record:
+            auc_record[alpha] = {}
+        for fit_prior in fit_prior_option:
+            clf = MultinomialNB(alpha=alpha, fit_prior=fit_prior)
+            clf.fit(X_train, Y_train)
+            prediction_prob = clf.predict_proba(X_test)
+            pos_prob = prediction_prob[:, 1]
+            auc = roc_auc_score(Y_test, pos_prob)
+            auc_record[alpha][fit_prior] = auc + auc_record[alpha].get(fit_prior, 0.0)
+
+for smoothing, smoothing_record in auc_record.items():
+    for fit_prior, auc in smoothing_record.items():
+        print(f' {smoothing} {fit_prior} {auc/k:.5f}')
+
+clf = MultinomialNB(alpha=2.0, fit_prior=False)
+clf.fit(X_train, Y_train)
+pos_prob = clf.predict_proba(X_test)[:, 1]
+print('AUC with the best model:', roc_auc_score(Y_test, pos_prob))
