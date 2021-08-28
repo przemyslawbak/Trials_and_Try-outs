f08ba0f6f2691b97399a602540c67b1adaf7e668
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-28 08:52:08 +0200

    Extracting housing.csv from the `tgz` file;

--- 31_Python_ML/BookHandsOn_01_HousingPrices/BookHandsOn/BookHandsOn.py
+++ 31_Python_ML/BookHandsOn_01_HousingPrices/BookHandsOn/BookHandsOn.py
@@ -1,1 +1,18 @@
 
+import os
+import tarfile
+import urllib
+
+DOWNLOAD_ROOT = "/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+#retreiving file and extracting
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+fetch_housing_data()
