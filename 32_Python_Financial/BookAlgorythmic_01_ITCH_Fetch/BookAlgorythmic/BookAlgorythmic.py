b029435f06e48166d373e710e43f6afd2e3b27d8
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-28 23:25:21 +0200

    Working on the first sample project;

--- 32_Python_Financial/BookAlgorythmic_01_ITCH_Fetch/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/BookAlgorythmic_01_ITCH_Fetch/BookAlgorythmic/BookAlgorythmic.py
@@ -1,1 +1,60 @@
 
+import warnings
+warnings.filterwarnings('ignore')
+import gzip
+import shutil
+from struct import unpack
+from collections import namedtuple, Counter, defaultdict
+from pathlib import Path
+from urllib.request import urlretrieve
+from urllib.parse import urljoin
+from datetime import timedelta
+from time import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import seaborn as sns
+
+sns.set_style('whitegrid')
+
+def format_time(t):
+    """Return a formatted time string 'HH:MM:SS
+    based on a numeric time() value"""
+    m, s = divmod(t, 60)
+    h, m = divmod(m, 60)
+    return f'{h:0>2.0f}:{m:0>2.0f}:{s:0>5.2f}'
+
+data_path = Path('data') # set to e.g. external harddrive
+itch_store = str(data_path / 'itch.h5')
+order_book_store = data_path / 'order_book.h5'
+
+FTP_URL = 'ftp://emi.nasdaq.com/ITCH/'
+SOURCE_FILE = '08272021.NASDAQ_ITCH50.gz'
+
+def may_be_download(url):
+    """Download & unzip ITCH data if not yet available"""
+    if not data_path.exists():
+        print('Creating directory')
+        data_path.mkdir()
+    else: 
+        print('Directory exists')
+
+    filename = data_path / url.split('/')[-1]        
+    if not filename.exists():
+        print('Downloading...', url)
+        urlretrieve(url, filename)
+    else: 
+        print('File exists')        
+
+    unzipped = data_path / (filename.stem + '.bin')
+    if not unzipped.exists():
+        print('Unzipping to', unzipped)
+        with gzip.open(str(filename), 'rb') as f_in:
+            with open(unzipped, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    else: 
+        print('File already unpacked')
+    return unzipped
+
+file_name = may_be_download(urljoin(FTP_URL, SOURCE_FILE))
+date = file_name.name.split('.')[0]
