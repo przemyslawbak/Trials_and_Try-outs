09bcad56fc5999fa195645a809078d3bb19d3369
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-29 17:18:11 +0200

    Completed project;

--- 32_Python_Financial/BookAlgorythmic_01_ITCH_Fetch/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/BookAlgorythmic_01_ITCH_Fetch/BookAlgorythmic/BookAlgorythmic.py
@@ -59,5 +59,218 @@ def may_be_download(url, file, dir):
         print('File already unpacked')
     return unzipped
 
+#The function clean_message_types() just runs a few basic string cleaning steps.
+def clean_message_types(df):
+    df.columns = [c.lower().strip() for c in df.columns]
+    df.value = df.value.str.strip()
+    df.name = (df.name
+               .str.strip() # remove whitespace
+               .str.lower()
+               .str.replace(' ', '_')
+               .str.replace('-', '_')
+               .str.replace('/', '_'))
+    df.notes = df.notes.str.strip()
+    df['message_type'] = df.loc[df.name == 'message_type', 'value']
+    return df
+
 file_name = may_be_download(FTP_URL, SOURCE_FILE, FTP_DIR)
 date = file_name.name.split('.')[0]
+message_data = (pd.read_excel('data/message_types.xlsx',
+                              sheet_name='messages')
+                .sort_values('id')
+                .drop('id', axis=1))
+print(message_data.head())
+message_types = clean_message_types(message_data)
+
+#We extract message type codes and names so we can later make the results more readable.
+message_labels = (message_types.loc[:, ['message_type', 'notes']]
+                  .dropna()
+                  .rename(columns={'notes': 'name'}))
+message_labels.name = (message_labels.name
+                       .str.lower()
+                       .str.replace('message', '')
+                       .str.replace('.', '')
+                       .str.strip().str.replace(' ', '_'))
+# message_labels.to_csv('message_labels.csv', index=False)
+print(message_labels.head())
+
+#Each message consists of several fields that are defined by offset, length and type of value. The struct module will use this format information to parse the binary source data.
+message_types.message_type = message_types.message_type.ffill()
+message_types = message_types[message_types.name != 'message_type']
+message_types.value = (message_types.value
+                       .str.lower()
+                       .str.replace(' ', '_')
+                       .str.replace('(', '')
+                       .str.replace(')', ''))
+message_types.info()
+
+event_codes = {'O': 'Start of Messages',
+               'S': 'Start of System Hours',
+               'Q': 'Start of Market Hours',
+               'M': 'End of Market Hours',
+               'E': 'End of System Hours',
+               'C': 'End of Messages'}
+
+encoding = {'primary_market_maker': {'Y': 1, 'N': 0},
+            'printable'           : {'Y': 1, 'N': 0},
+            'buy_sell_indicator'  : {'B': 1, 'S': -1},
+            'cross_type'          : {'O': 0, 'C': 1, 'H': 2},
+            'imbalance_direction' : {'B': 0, 'S': 1, 'N': 0, 'O': -1}}
+
+formats = {
+    ('integer', 2): 'H', # int of length 2 => format string 'H'
+    ('integer', 4): 'I',
+    ('integer', 6): '6s', # int of length 6 => parse as string, convert later
+
+    ('integer', 8): 'Q',
+    ('alpha', 1)  : 's',
+    ('alpha', 2)  : '2s',
+    ('alpha', 4)  : '4s',
+    ('alpha', 8)  : '8s',
+    ('price_4', 4): 'I',
+    ('price_8', 8): 'Q',
+}
+
+#The parser translates the message specs into format strings and namedtuples that capture the message content. First, we create (type, length) formatting tuples from ITCH spec
+message_types.loc[:, 'formats'] = (message_types[['value', 'length']]
+                            .apply(tuple, axis=1).map(formats))
+
+#Then, we extract formatting details for alphanumerical fields
+alpha_fields = message_types[message_types.value == 'alpha'].set_index('name')
+alpha_msgs = alpha_fields.groupby('message_type')
+alpha_formats = {k: v.to_dict() for k, v in alpha_msgs.formats}
+alpha_length = {k: v.add(5).to_dict() for k, v in alpha_msgs.length}
+
+#We generate message classes as named tuples and format strings
+message_fields, fstring = {}, {}
+for t, message in message_types.groupby('message_type'):
+    message_fields[t] = namedtuple(typename=t, field_names=message.name.tolist())
+    fstring[t] = '>' + ''.join(message.formats.tolist())
+alpha_fields.info()
+
+#Fields of alpha type (alphanumeric) require post-processing as defined in the format_alpha function
+def format_alpha(mtype, data):
+    """Process byte strings of type alpha"""
+
+    for col in alpha_formats.get(mtype).keys():
+        if mtype != 'R' and col == 'stock':
+            data = data.drop(col, axis=1)
+            continue
+        data.loc[:, col] = data.loc[:, col].str.decode("utf-8").str.strip()
+        if encoding.get(col):
+            data.loc[:, col] = data.loc[:, col].map(encoding.get(col))
+    return data
+
+def store_messages(m):
+    """Handle occasional storing of all messages"""
+    with pd.HDFStore(itch_store) as store:
+        for mtype, data in m.items():
+            # convert to DataFrame
+            data = pd.DataFrame(data)
+
+            # parse timestamp info
+            data.timestamp = data.timestamp.apply(int.from_bytes, byteorder='big')
+            data.timestamp = pd.to_timedelta(data.timestamp)
+
+            # apply alpha formatting
+            if mtype in alpha_formats.keys():
+                data = format_alpha(mtype, data)
+
+            s = alpha_length.get(mtype)
+            if s:
+                s = {c: s.get(c) for c in data.columns}
+            dc = ['stock_locate']
+            if m == 'R':
+                dc.append('stock')
+            try:
+                store.append(mtype,
+                         data,
+                         format='t',
+                         min_itemsize=s,
+                         data_columns=dc)
+            except Exception as e:
+                print(e)
+                print(mtype)
+                print(data.info())
+                print(pd.Series(list(m.keys())).value_counts())
+                data.to_csv('data.csv', index=False)
+                return 1
+    return 0
+
+messages = defaultdict(list)
+message_count = 0
+message_type_counter = Counter()
+
+start = time()
+with file_name.open('rb') as data:
+    while True:
+
+        # determine message size in bytes
+        message_size = int.from_bytes(data.read(2), byteorder='big', signed=False)
+        
+        # get message type by reading first byte
+        message_type = data.read(1).decode('ascii')        
+        message_type_counter.update([message_type])
+
+        # read & store message
+        record = data.read(message_size - 1)
+        message = message_fields[message_type]._make(unpack(fstring[message_type], record))
+        messages[message_type].append(message)
+        
+        # deal with system events
+        if message_type == 'S':
+            seconds = int.from_bytes(message.timestamp, byteorder='big') * 1e-9
+            print('\n', event_codes.get(message.event_code.decode('ascii'), 'Error'))
+            print(f'\t{format_time(seconds)}\t{message_count:12,.0f}')
+            if message.event_code.decode('ascii') == 'C':
+                store_messages(messages)
+                break
+        message_count += 1
+
+        if message_count % 2.5e7 == 0:
+            seconds = int.from_bytes(message.timestamp, byteorder='big') * 1e-9
+            d = format_time(time() - start)
+            print(f'\t{format_time(seconds)}\t{message_count:12,.0f}\t{d}')
+            res = store_messages(messages)
+            if res == 1:
+                print(pd.Series(dict(message_type_counter)).sort_values())
+                break
+            messages.clear()
+
+print('Duration:', format_time(time() - start))
+
+counter = pd.Series(message_type_counter).to_frame('# Trades')
+counter['Message Type'] = counter.index.map(message_labels.set_index('message_type').name.to_dict())
+counter = counter[['Message Type', '# Trades']].sort_values('# Trades', ascending=False)
+print(counter)
+
+with pd.HDFStore(itch_store) as store:
+    store.put('summary', counter)
+
+with pd.HDFStore(itch_store) as store:
+    stocks = store['R'].loc[:, ['stock_locate', 'stock']]
+    trades = store['P'].append(store['Q'].rename(columns={'cross_price': 'price'}), sort=False).merge(stocks)
+
+trades['value'] = trades.shares.mul(trades.price)
+trades['value_share'] = trades.value.div(trades.value.sum())
+
+trade_summary = trades.groupby('stock').value_share.sum().sort_values(ascending=False)
+trade_summary.iloc[:50].plot.bar(figsize=(14, 6), color='darkblue', title='Share of Traded Value')
+
+plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
+sns.despine()
+plt.tight_layout()
+
+with pd.HDFStore(itch_store) as store:
+    stocks = store['R'].loc[:, ['stock_locate', 'stock']]
+    trades = store['P'].append(store['Q'].rename(columns={'cross_price': 'price'}), sort=False).merge(stocks)
+
+trades['value'] = trades.shares.mul(trades.price)
+trades['value_share'] = trades.value.div(trades.value.sum())
+
+trade_summary = trades.groupby('stock').value_share.sum().sort_values(ascending=False)
+trade_summary.iloc[:50].plot.bar(figsize=(14, 6), color='darkblue', title='Share of Traded Value')
+
+plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
+sns.despine()
+plt.tight_layout()
