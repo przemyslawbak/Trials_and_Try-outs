be47fc0bda7f31d6d28064cd9fbecb13e83464aa
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-29 18:48:33 +0200

    Finished with data providers example;

--- 32_Python_Financial/BookAlgorythmic_02_ApiAndHtml/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_02_ApiAndHtml/Sample/Sample.py
@@ -22,6 +22,45 @@ yahoo= web.DataReader('FB', 'yahoo', start=start, end=end)
 yahoo.info()
 
 #drawing plot
-mpf.plot(yahoo.drop('Adj Close', axis=1), type='candle')
-plt.tight_layout()
+#mpf.plot(yahoo.drop('Adj Close', axis=1), type='candle') #commented
+#plt.tight_layout() #commented
 
+#IEX api
+#IEX_API_KEY = 'xxx'
+#start = datetime(2015, 2, 9)
+# end = datetime(2017, 5, 24)
+#iex = web.DataReader('FB', 'iex', start, api_key=IEX_API_KEY)
+#iex.info()
+
+book = web.get_iex_book('AAPL')
+list(book.keys())
+orders = pd.concat([pd.DataFrame(book[side]).assign(side=side) for side in ['bids', 'asks']])
+orders.head()
+for key in book.keys():
+    try:
+        print(f'\n{key}')
+        print(pd.DataFrame(book[key]))
+    except:
+        print(book[key])
+
+"""
+OTHER:
+Quandl
+FRED
+Fama/French
+World Bank
+OECD
+Tiingo
+"""
+
+#stooq
+print('stooq')
+index_url = 'https://stooq.com/t/'
+ix = pd.read_html(index_url)
+len(ix)
+sp500_stooq = web.DataReader('^SPX', 'stooq')
+sp500_stooq.info()
+sp500_stooq.head()
+sp500_stooq.Close.plot(figsize=(14,4))
+sns.despine()
+plt.tight_layout()
