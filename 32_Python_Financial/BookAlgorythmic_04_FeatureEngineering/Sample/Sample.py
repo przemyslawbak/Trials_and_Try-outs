fad211e9bba6a05d57ea2823514ec83789a4deef
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-30 22:11:54 +0200

    Working on feature engineering sample project;

--- 32_Python_Financial/BookAlgorythmic_04_FeatureEngineering/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_04_FeatureEngineering/Sample/Sample.py
@@ -87,3 +87,123 @@ data.describe()
 
 # cmap = sns.diverging_palette(10, 220, as_cmap=True)
 sns.clustermap(data.corr('spearman'), annot=True, center=0, cmap='Blues');
+data.index.get_level_values('ticker').nunique()
+
+#Rolling Factor Betas to estimate the exposure of assets to common risk factors using linear regression
+factors = ['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA']
+factor_data = web.DataReader('F-F_Research_Data_5_Factors_2x3', 'famafrench', start='2000')[0].drop('RF', axis=1)
+factor_data.index = factor_data.index.to_timestamp()
+factor_data = factor_data.resample('M').last().div(100)
+factor_data.index.name = 'date'
+factor_data.info()
+
+factor_data = factor_data.join(data['return_1m']).sort_index()
+factor_data.info()
+
+T = 24
+betas = (factor_data.groupby(level='ticker',
+                             group_keys=False)
+         .apply(lambda x: RollingOLS(endog=x.return_1m,
+                                     exog=sm.add_constant(x.drop('return_1m', axis=1)),
+                                     window=min(T, x.shape[0]-1))
+                .fit(params_only=True)
+                .params
+                .drop('const', axis=1)))
+
+betas.describe().join(betas.sum(1).describe().to_frame('total'))
+
+cmap = sns.diverging_palette(10, 220, as_cmap=True)
+sns.clustermap(betas.corr(), annot=True, cmap=cmap, center=0);
+data = (data
+        .join(betas
+              .groupby(level='ticker')
+              .shift()))
+data.info()
+data.loc[:, factors] = data.groupby('ticker')[factors].apply(lambda x: x.fillna(x.mean()))
+data.info()
+
+#Momentum factors based on the difference between returns over longer periods and the most recent monthly return, as well as for the difference between 3 and 12 month returns as follows
+for lag in [2,3,6,9,12]:
+    data[f'momentum_{lag}'] = data[f'return_{lag}m'].sub(data.return_1m)
+data[f'momentum_3_12'] = data[f'return_12m'].sub(data.return_3m)
+
+#Date Indicators
+dates = data.index.get_level_values('date')
+data['year'] = dates.year
+data['month'] = dates.month
+
+#Lagged returns as input variables or features associated with the current observations, we use the .shift() method to move historical returns up to the current period
+for t in range(1, 7):
+    data[f'return_1m_t-{t}'] = data.groupby(level='ticker').return_1m.shift(t)
+data.info()
+
+#Target: Holding Period Returns - we use the normalized period returns computed previously and shift them back to align them with the current financial features
+for t in [1,2,3,6,12]:
+    data[f'target_{t}m'] = data.groupby(level='ticker')[f'return_{t}m'].shift(-t)
+cols = ['target_1m',
+        'target_2m',
+        'target_3m', 
+        'return_1m',
+        'return_2m',
+        'return_3m',
+        'return_1m_t-1',
+        'return_1m_t-2',
+        'return_1m_t-3']
+data[cols].dropna().sort_index().head(10)
+data.info()
+
+#Create age proxy - We use quintiles of IPO year as a proxy for company age
+data = (data
+        .join(pd.qcut(stocks.ipoyear, q=5, labels=list(range(1, 6)))
+              .astype(float)
+              .fillna(0)
+              .astype(int)
+              .to_frame('age')))
+data.age = data.age.fillna(-1)
+stocks.info()
+
+#Create dynamic size proxy
+size_factor = (monthly_prices
+               .loc[data.index.get_level_values('date').unique(),
+                    data.index.get_level_values('ticker').unique()]
+               .sort_index(ascending=False)
+               .pct_change()
+               .fillna(0)
+               .add(1)
+               .cumprod())
+size_factor.info()
+
+msize = (size_factor
+         .mul(stocks
+              .loc[size_factor.columns, 'marketcap'])).dropna(axis=1, how='all')
+
+#Create Size indicator as deciles per period (month)
+data['msize'] = (msize
+                 .apply(lambda x: pd.qcut(x, q=10, labels=list(range(1, 11)))
+                        .astype(int), axis=1)
+                 .stack()
+                 .swaplevel())
+data.msize = data.msize.fillna(-1)
+
+data = data.join(stocks[['sector']])
+data.sector = data.sector.fillna('Unknown')
+data.info()
+
+#Store data
+with pd.HDFStore(DATA_STORE) as store:
+    store.put('engineered_features', data.sort_index().loc[idx[:, :datetime(2018, 3, 1)], :])
+    print(store.info())
+
+#Create Dummy variables
+dummy_data = pd.get_dummies(data,
+                            columns=['year','month', 'msize', 'age',  'sector'],
+                            prefix=['year','month', 'msize', 'age', ''],
+                            prefix_sep=['_', '_', '_', '_', ''])
+dummy_data = dummy_data.rename(columns={c:c.replace('.0', '') for c in dummy_data.columns})
+dummy_data.info()
+
+
+
+
+
+
