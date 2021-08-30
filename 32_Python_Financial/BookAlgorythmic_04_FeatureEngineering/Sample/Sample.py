178c7f2d51be42cce767807355968248dee8cd5b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-08-30 22:11:27 +0200

    Remove ignored files;

--- 32_Python_Financial/BookAlgorythmic_04_FeatureEngineering/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_04_FeatureEngineering/Sample/Sample.py
@@ -22,16 +22,18 @@ pd.set_option('display.expand_frame_repr', False)
 
 DATA_STORE = 'data/assets.h5'
 
-#Quandl makes available a dataset with stock prices
-df = pd.read_csv('wiki_stocks.csv')
-# no longer needed
-# df = pd.concat([df.loc[:, 'code'].str.strip(),
-#                 df.loc[:, 'name'].str.split('(', expand=True)[0].str.strip().to_frame('name')], axis=1)
-
-print(df.info(null_counts=True))
-with pd.HDFStore(DATA_STORE) as store:
-    store.put('quandl/wiki/prices', df)
-
+if not Path('data', 'assets.h5').exists():
+    print('fetching data...')
+    #Quandl makes available a dataset with stock prices
+    df = pd.read_csv('wiki_prices.csv')
+    # no longer needed
+    # df = pd.concat([df.loc[:, 'code'].str.strip(),
+    #                 df.loc[:, 'name'].str.split('(', expand=True)[0].str.strip().to_frame('name')], axis=1)
+    print(df.info(null_counts=True))
+    with pd.HDFStore(DATA_STORE) as store:
+        store.put('quandl/wiki/prices', df)
+else:
+    print('data exists...')
 
 #Set data store location
 START = 2000
@@ -43,3 +45,45 @@ with pd.HDFStore(DATA_STORE) as store:
     stocks = store['us_equities/stocks'].loc[:, ['marketcap', 'ipoyear', 'sector']]
 
 prices.info()
+
+#Remove `stocks` duplicates and align index names for later joining
+stocks = stocks[~stocks.index.duplicated()]
+stocks.index.name = 'ticker'
+
+#Get tickers with both price information and metdata
+shared = prices.columns.intersection(stocks.index)
+stocks = stocks.loc[shared, :]
+stocks.info()
+prices = prices.loc[:, shared]
+prices.info()
+assert prices.shape[1] == stocks.shape[0]
+
+#Create monthly return series
+monthly_prices = prices.resample('M').last()
+monthly_prices.info()
+outlier_cutoff = 0.01
+data = pd.DataFrame()
+lags = [1, 2, 3, 6, 9, 12]
+for lag in lags:
+    data[f'return_{lag}m'] = (monthly_prices
+                           .pct_change(lag)
+                           .stack()
+                           .pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),
+                                                  upper=x.quantile(1-outlier_cutoff)))
+                           .add(1)
+                           .pow(1/lag)
+                           .sub(1)
+                           )
+data = data.swaplevel().dropna()
+data.info()
+
+#Drop stocks with less than 10 yrs of returns
+min_obs = 120
+nobs = data.groupby(level='ticker').size()
+keep = nobs[nobs>min_obs].index
+data = data.loc[idx[keep,:], :]
+data.info()
+data.describe()
+
+# cmap = sns.diverging_palette(10, 220, as_cmap=True)
+sns.clustermap(data.corr('spearman'), annot=True, center=0, cmap='Blues');
