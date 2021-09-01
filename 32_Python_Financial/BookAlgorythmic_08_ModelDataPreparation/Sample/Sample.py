419f8436be13062db0aadb659f4ee344f1ea8fda
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-01 15:12:29 +0200

    Removed FAMA and added new sample projects;

---
+++ 32_Python_Financial/BookAlgorythmic_08_ModelDataPreparation/Sample/Sample.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import pearsonr, spearmanr
+from talib import RSI, BBANDS, MACD, ATR
+
