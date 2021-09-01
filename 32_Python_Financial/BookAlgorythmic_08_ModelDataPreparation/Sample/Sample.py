80862ef9fdfff8afda40693c6af8ef277398fdf4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-01 21:32:45 +0200

    Working on model data preparation sample project;

--- 32_Python_Financial/BookAlgorythmic_08_ModelDataPreparation/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_08_ModelDataPreparation/Sample/Sample.py
@@ -7,3 +7,35 @@ import seaborn as sns
 from scipy.stats import pearsonr, spearmanr
 from talib import RSI, BBANDS, MACD, ATR
 
+#settings
+MONTH = 21
+YEAR = 12 * MONTH
+START = '2013-01-01'
+END = '2017-12-31'
+sns.set_style('whitegrid')
+idx = pd.IndexSlice
+
+#Loading Quandl Wiki Stock Prices & Meta Data
+ohlcv = ['adj_open', 'adj_close', 'adj_low', 'adj_high', 'adj_volume']
+DATA_STORE = 'data/assets.h5'
+with pd.HDFStore(DATA_STORE) as store:
+    prices = (store['quandl/wiki/prices']
+              .loc[idx[START:END, :], ohlcv]
+              .rename(columns=lambda x: x.replace('adj_', ''))
+              .assign(volume=lambda x: x.volume.div(1000))
+              .swaplevel()
+              .sort_index())
+
+    stocks = (store['us_equities/stocks']
+              .loc[:, ['marketcap', 'ipoyear', 'sector']])
+
+# want at least 2 years of data
+min_obs = 2 * YEAR
+
+# have this much per ticker 
+nobs = prices.groupby(level='ticker').size()
+
+# keep those that exceed the limit
+keep = nobs[nobs > min_obs].index
+
+prices = prices.loc[idx[keep, :], :]
