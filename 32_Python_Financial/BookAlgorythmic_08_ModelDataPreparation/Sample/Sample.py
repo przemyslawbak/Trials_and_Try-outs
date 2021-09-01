c7649096493071a0c0237cb17061c84afc0253e7
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-01 23:41:24 +0200

    Fixing some of the bugs from the source code;

--- 32_Python_Financial/BookAlgorythmic_08_ModelDataPreparation/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_08_ModelDataPreparation/Sample/Sample.py
@@ -10,24 +10,39 @@ from talib import RSI, BBANDS, MACD, ATR
 #settings
 MONTH = 21
 YEAR = 12 * MONTH
-START = '2013-01-01'
-END = '2017-12-31'
+START = 2000
+END = 2018
 sns.set_style('whitegrid')
 idx = pd.IndexSlice
-
+print('fetching data...')
 #Loading Quandl Wiki Stock Prices & Meta Data
 ohlcv = ['adj_open', 'adj_close', 'adj_low', 'adj_high', 'adj_volume']
 DATA_STORE = 'data/assets.h5'
+print('next...')
+df = pd.read_csv('wiki_prices.csv')
+# no longer needed
+# df = pd.concat([df.loc[:, 'code'].str.strip(),
+#                 df.loc[:, 'name'].str.split('(', expand=True)[0].str.strip().to_frame('name')], axis=1)
+
+print(df.info(null_counts=True))
+with pd.HDFStore(DATA_STORE) as store:
+    store.put('quandl/wiki/prices', df)
+df = pd.read_csv('us_equities_meta_data.csv')
+df.info()
+with pd.HDFStore(DATA_STORE) as store:
+    store.put('us_equities/stocks', df.set_index('ticker'))
 with pd.HDFStore(DATA_STORE) as store:
     prices = (store['quandl/wiki/prices']
-              .loc[idx[START:END, :], ohlcv]
+              .loc[idx[START:END], ohlcv]
               .rename(columns=lambda x: x.replace('adj_', ''))
               .assign(volume=lambda x: x.volume.div(1000))
-              .swaplevel()
               .sort_index())
-
     stocks = (store['us_equities/stocks']
               .loc[:, ['marketcap', 'ipoyear', 'sector']])
+
+print('ok...')
+print(prices)
+print(stocks)
 
 # want at least 2 years of data
 min_obs = 2 * YEAR
