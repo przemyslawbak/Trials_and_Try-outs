dc9b9975c7afe0cd2844fd69f3a1d022883c183e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-01 13:54:36 +0200

    Completed linear regression intro samples;

--- 32_Python_Financial/BookAlgorythmic_06_LinearRegressionIntro/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_06_LinearRegressionIntro/Sample/Sample.py
@@ -8,3 +8,33 @@ import statsmodels.api as sm
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
 
+sns.set_style('whitegrid')
+pd.options.display.float_format = '{:,.2f}'.format
+
+#Generate random data
+x = np.linspace(-5, 50, 100)
+y = 50 + 2 * x + np.random.normal(0, 20, size=len(x))
+data = pd.DataFrame({'X': x, 'Y': y})
+ax = data.plot.scatter(x='X', y='Y', figsize=(14, 6))
+sns.despine()
+plt.tight_layout()
+
+#Estimate a simple regression with statsmodels
+X = sm.add_constant(data['X'])
+model = sm.OLS(data['Y'], X).fit()
+print(model.summary())
+
+#Verify calculation
+beta = np.linalg.inv(X.T.dot(X)).dot(X.T.dot(y))
+pd.Series(beta, index=X.columns)
+
+#Display model & residuals
+data['y-hat'] = model.predict()
+data['residuals'] = model.resid
+ax = data.plot.scatter(x='X', y='Y', c='darkgrey', figsize=(14,6))
+data.plot.line(x='X', y='y-hat', ax=ax);
+for _, row in data.iterrows():
+    plt.plot((row.X, row.X), (row.Y, row['y-hat']), 'k-')    
+sns.despine()
+plt.tight_layout();
+plt.show()
