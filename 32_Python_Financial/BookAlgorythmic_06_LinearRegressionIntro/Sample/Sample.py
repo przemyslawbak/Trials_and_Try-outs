9b5437bbc90a8ca8f976386b9c35871f09a51e79
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-01 13:31:03 +0200

    Linear regression intro sample project added;

---
+++ 32_Python_Financial/BookAlgorythmic_06_LinearRegressionIntro/Sample/Sample.py
@@ -0,0 +1,10 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler
+
