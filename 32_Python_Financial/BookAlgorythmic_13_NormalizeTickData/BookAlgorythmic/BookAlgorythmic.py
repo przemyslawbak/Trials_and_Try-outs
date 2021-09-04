ff3bc07d7afd22b55333197ccd4116bd695175bd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-04 12:13:54 +0200

    Trade summary from the sheet;

--- 32_Python_Financial/BookAlgorythmic_13_NormalizeTickData/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/BookAlgorythmic_13_NormalizeTickData/BookAlgorythmic/BookAlgorythmic.py
@@ -8,6 +8,40 @@ from matplotlib.ticker import FuncFormatter
 from math import pi
 from bokeh.plotting import figure, show
 from scipy.stats import normaltest
+import datetime as d
 
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 sns.set_style('whitegrid')
+
+data_path = Path('data')
+itch_store = str(data_path / 'itch.h5') #see: BookAlgorythmic_01_ITCH_Fetch
+order_book_store = str(data_path / 'order_book.h5')
+stock = 'AAPL'
+date = '20191030'
+title = '{} | {}'.format(stock, pd.to_datetime(date).date())
+
+with pd.HDFStore(itch_store) as store:
+    sys_events = store['S'].set_index('event_code').drop_duplicates()
+    sys_events.timestamp = sys_events.timestamp.add(pd.to_datetime(date)).dt.time
+    market_open = sys_events.loc['Q', 'timestamp']
+    market_close = d.time(17, 30, 0, 70) #changed, data limited to save disc space
+
+#Trade Summary
+with pd.HDFStore(itch_store) as store:
+    stocks = store['R']
+stocks.info()
+
+with pd.HDFStore(itch_store) as store:
+    stocks = store['R'].loc[:, ['stock_locate', 'stock']]
+    trades = store['P'].append(store['Q'].rename(columns={'cross_price': 'price'}), sort=False).merge(stocks)
+
+trades['value'] = trades.shares.mul(trades.price)    
+trades['value_share'] = trades.value.div(trades.value.sum())
+trade_summary = trades.groupby('stock').value_share.sum().sort_values(ascending=False)
+trade_summary.iloc[:50].plot.bar(figsize=(14, 6), color='darkblue', title='% of Traded Value')
+plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
+
+
+
+
+plt.show()
