3a7cd413d775d7cddc9c9003aa0a787c522d0dca
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-05 09:51:07 +0200

    Completed yfinance sample project;

--- 32_Python_Financial/BookAlgorythmic_15_YFinanceDemo/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_15_YFinanceDemo/Sample/Sample.py
@@ -3,3 +3,31 @@ warnings.filterwarnings('ignore')
 import pandas as pd
 import yfinance as yf
 
+symbol = 'fb'
+ticker = yf.Ticker(symbol)
+print(ticker.info)
+
+#Get market data
+data = ticker.history(period='5d',
+                      interval='1m',
+                      start=None,
+                      end=None,
+                      actions=True,
+                      auto_adjust=True,
+                      back_adjust=False)
+data.info()
+
+#View company actions
+# show actions (dividends, splits)
+print(ticker.actions)
+print(ticker.dividends)
+print(ticker.splits)
+print(ticker.financials)
+print(ticker.quarterly_financials)
+print(ticker.balance_sheet)
+
+print(ticker.sustainability)
+
+ticker.recommendations.info()
+
+#and some other stuff.........
