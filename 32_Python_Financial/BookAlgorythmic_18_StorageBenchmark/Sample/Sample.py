00264203d6d639dceb9b62173016807484cbed27
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-05 11:38:45 +0200

    Created store benchmark sample project;

---
+++ 32_Python_Financial/BookAlgorythmic_18_StorageBenchmark/Sample/Sample.py
@@ -0,0 +1,14 @@
+import warnings
+warnings.filterwarnings('ignore')
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+import string
+
+sns.set_style('whitegrid')
+results = {}
+
+stor
