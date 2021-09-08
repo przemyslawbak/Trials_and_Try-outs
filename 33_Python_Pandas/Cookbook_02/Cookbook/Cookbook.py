fcb9870a782a6f462f6133b069654633e0e8d83a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-08 22:56:54 +0200

    Completed Pandas cb chapter 2 examples;

--- 33_Python_Pandas/Cookbook_02/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Cookbook_02/Cookbook/Cookbook.py
@@ -24,4 +24,19 @@ movies = movies.rename(columns=shorten) #using custom method
 movies.select_dtypes(include="int").head() #select only the integer columns
 movies.select_dtypes(include=["int", "object"]).head() #If we wanted integer and string columns
 movies.select_dtypes(exclude="float").head() #exclufing floats
-movies.filter(like="fb").head() #alternative way
+movies.filter(like="fb").head() #alternative way, allows regex
+#Summarizing a DataFrame
+print(movies.describe().T)
+#Chaining DataFrame methods
+print(movies.isnull().sum().sum())
+print(movies.isnull().any().any())
+print(movies.isnull().dtypes.value_counts())
+movies.select_dtypes(["object"]).fillna("").max() #selects obj types and fills empty
+#DataFrame operations
+colleges = pd.read_csv("data/college.csv", index_col="INSTNM")
+college_ugds = colleges.filter(like="UGDS_")
+print(college_ugds.head())
+college_ugds_op_round = (
+(college_ugds + 0.00501) // 0.01 / 100 #rounding
+)
+print(college_ugds_op_round.head())
