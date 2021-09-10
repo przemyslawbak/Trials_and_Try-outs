d1d39bce65ce96b48543ae44d2b183f9bd1dce74
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-10 11:55:58 +0200

    Added a few examples to the Pandas cb c.4 project;

--- 33_Python_Pandas/Cookbook_04/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Cookbook_04/Cookbook/Cookbook.py
@@ -1,84 +1,33 @@
 import pandas as pd
 import numpy as np
-from io import StringIO
-import zipfile
+import datetime
+import pandas_datareader.data as web
+import requests_cache
 
-#Creating DataFrames from scratch
-fname = ["Paul", "John", "Richard", "George"]
-lname = ["McCartney", "Lennon", "Starkey", "Harrison"]
-birth = [1942, 1940, 1940, 1943]
-people = {"first": fname, "last": lname, "birth": birth} #Create a dictionary from the lists, mapping the column name to the list
-beatles = pd.DataFrame(people) #Create a DataFrame from the dictionary
-print(beatles)
-print(beatles.index)
+college = pd.read_csv("data/college.csv")
 
-#create a DataFrame from a list of dictionaries
-pd.DataFrame(
-[
-{
-"first": "Paul",
-"last": "McCartney",
-"birth": 1942,
-},
-{
-"first": "John",
-"last": "Lennon",
-"birth": 1940,
-},
-{
-"first": "Richard",
-"last": "Starkey",
-"birth": 1940,
-},
-{
-"first": "George",
-"last": "Harrison",
-"birth": 1943,
-},
-]
+print(college.sample(random_state=42))
+#Selecting the smallest of the largest
+movie = pd.read_csv("data/movie.csv")
+movie2 = movie[["movie_title", "imdb_score", "budget"]]
+movie2.head()
+print(movie2.nlargest(100, "imdb_score").head())
+#Selecting the largest of each group by sorting
+movie = pd.read_csv("data/movie.csv")
+movie[
+["movie_title", "title_year", "imdb_score"]
+].sort_values("title_year", ascending=True)
+
+#calculating trailing stop for stocks
+session = requests_cache.CachedSession(
+cache_name="cache",
+backend="sqlite",
+expire_after=datetime.timedelta(days=90),
 )
-
-#createing CSV file
-fout = StringIO()
-beatles.to_csv(fout) # use a filename instead of fout to save file
-print(fout.getvalue())
-
-#loading from file
-diamonds = pd.read_csv("data/diamonds.csv", nrows=1000)
-print(diamonds.info())#78.2+ KB
-#changed data type precission
-diamonds2 = pd.read_csv(
-"data/diamonds.csv",
-nrows=1000,
-dtype={
-"carat": np.float32,
-"depth": np.float32,
-"table": np.float32,
-"x": np.float32,
-"y": np.float32,
-"z": np.float32,
-"price": np.int16,
-},
+tsla = web.DataReader(
+"tsla",
+data_source="yahoo",
+start="2017-1-1",
+session=session, #exception
 )
-diamonds2.info() #49.0+ KB
-#save to inary format
-diamonds2.to_feather("data/d.arr") #optional: diamonds2.to_parquet("data/d.pqt")
-#save to excel
-beatles.to_excel("data/beat.xlsx")
-#reading excel
-beat2 = pd.read_excel("data/beat.xls") #optional select sheet name
-#reading zipped single file
-autos = pd.read_csv("data/vehicles.csv.zip")
-#reading zipped multiple files
-with zipfile.ZipFile(
-"data/kaggle-survey-2018.zip"
-) as z:
-    print("\n".join(z.namelist()))
-    kag = pd.read_csv(z.open("multipleChoiceResponses.csv"))
-    kag_questions = kag.iloc[0]
-    survey = kag.iloc[1:]
-print(survey.head(2).T)
-#also reading:
-#sql: pd.read_sql
-#json: pd.read_json
-#html table
+print(tsla.head(8))
