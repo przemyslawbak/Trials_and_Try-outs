3fd579e8002c7f2dad7365ee5494eacc5ef07fe2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-19 16:23:49 +0200

    New empty sample project added;

---
+++ 33_Python_Pandas/Cookbook_13b_VisualizationPandas/Cookbook/Cookbook.py
@@ -0,0 +1,11 @@
+#Visualization with Pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
+
+
+plt.show()
