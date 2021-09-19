0171dfc210dc89e597856d43e3aa9c0f35644cd4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-19 16:40:15 +0200

    Added new examples to the visualization with Pandas;

--- 33_Python_Pandas/Cookbook_13b_VisualizationPandas/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Cookbook_13b_VisualizationPandas/Cookbook/Cookbook.py
@@ -2,10 +2,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import matplotlib.pyplot as plt
 
-
-
+#basics
+df = pd.DataFrame(index=['Atiya', 'Abbas', 'Cornelia',
+'Stephanie', 'Monte'],
+data={'Apples':[20, 10, 40, 20, 50],
+'Oranges':[35, 40, 25, 19, 33]})
+print(df)
+color = ['.2', '.7']
+ax = df.plot.bar(color=color, figsize=(16,4)) #Bar plots use the index as the labels for the x-axis and the column values as the bar highlights
+ax = df.plot.kde(color=color, figsize=(16,4)) #A KDE plot ignores the index and uses the column names along the x-axis and uses the column values to calculate a probability density along the y values
+#multiple variables
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16,4))
+fig.suptitle('One Variable Plots', size=20, y=1.02)
+df.plot.kde(color=color, ax=ax1, title='KDE plot')
+df.plot.box(ax=ax2, title='Boxplot')
+df.plot.hist(color=color, ax=ax3, title='Histogram')
 plt.show()
