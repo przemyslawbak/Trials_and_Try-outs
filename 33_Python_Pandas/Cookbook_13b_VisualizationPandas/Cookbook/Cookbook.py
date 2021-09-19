272b1ecb10d33e94a4b15b618aeaff17bcfafcee
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-19 23:44:27 +0200

    Completed Pandas visualization examples;

--- 33_Python_Pandas/Cookbook_13b_VisualizationPandas/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Cookbook_13b_VisualizationPandas/Cookbook/Cookbook.py
@@ -18,4 +18,54 @@ fig.suptitle('One Variable Plots', size=20, y=1.02)
 df.plot.kde(color=color, ax=ax1, title='KDE plot')
 df.plot.box(ax=ax2, title='Boxplot')
 df.plot.hist(color=color, ax=ax3, title='Histogram')
+#visualization of flights dataset
+flights = pd.read_csv('data/flights.csv')
+#display types
+cols = ['DIVERTED', 'CANCELLED', 'DELAYED']
+(flights
+.assign(DELAYED=flights['ARR_DELAY'].ge(15).astype(int),
+ON_TIME=lambda df_:1 - df_[cols].any(axis=1))
+.select_dtypes(int)
+.sum()
+)
+fig, ax_array = plt.subplots(2, 3, figsize=(18,8))
+(ax1, ax2, ax3), (ax4, ax5, ax6) = ax_array
+fig.suptitle('2015 US Flights - Univariate Summary', size=20)
+ac = flights['AIRLINE'].value_counts()
+ac.plot.barh(ax=ax1, title='Airline')
+(flights
+['ORG_AIR']
+.value_counts()
+.plot.bar(ax=ax2, rot=0, title='Origin City')
+)
+(flights
+['DEST_AIR']
+.value_counts()
+.head(10)
+.plot.bar(ax=ax3, rot=0, title='Destination City')
+)
+(flights
+.assign(DELAYED=flights['ARR_DELAY'].ge(15).astype(int),
+ON_TIME=lambda df_:1 - df_[cols].any(axis=1))
+[['DIVERTED', 'CANCELLED', 'DELAYED', 'ON_TIME']]
+.sum()
+.plot.bar(ax=ax4, rot=0,
+log=True, title='Flight Status')
+)
+flights['DIST'].plot.kde(ax=ax5, xlim=(0, 3000),
+title='Distance KDE')
+flights['ARR_DELAY'].plot.hist(ax=ax6,
+title='Arrival Delay',
+range=(0,200)
+)
+
+
+
+
+
+
+
+
+
+
 plt.show()
