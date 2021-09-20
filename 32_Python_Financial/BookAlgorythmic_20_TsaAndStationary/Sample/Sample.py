632c99367e70f4bbf0c044ca8ea5bcf7b58ca4c3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-20 20:28:44 +0200

    Completed financial time series basic example;

--- 32_Python_Financial/BookAlgorythmic_20_TsaAndStationary/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_20_TsaAndStationary/Sample/Sample.py
@@ -36,5 +36,80 @@ def plot_correlogram(x, lags=None, title=None):
 
 industrial_production = web.DataReader('IPGMFN', 'fred', '1988', '2017-12').squeeze().dropna()
 nasdaq = web.DataReader('NASDAQCOM', 'fred', '1990', '2017-12-31').squeeze().dropna()
+components = tsa.seasonal_decompose(industrial_production, model='additive')
 print(industrial_production)
 print(nasdaq)
+print(components)
+ts = (industrial_production.to_frame('Original')
+      .assign(Trend=components.trend)
+      .assign(Seasonality=components.seasonal)
+      .assign(Residual=components.resid))
+with sns.axes_style('white'):
+    ts.plot(subplots=True, figsize=(14, 8), title=['Original Series', 'Trend Component', 'Seasonal Component','Residuals'], legend=False)
+    plt.suptitle('Seasonal Decomposition', fontsize=14)
+    sns.despine()
+    plt.tight_layout()
+    plt.subplots_adjust(top=.91);
+
+#Double check fo zero values
+(nasdaq == 0).any(), (industrial_production==0).any()
+nasdaq_log = np.log(nasdaq)
+industrial_production_log = np.log(industrial_production)
+#Differencing
+nasdaq_log_diff = nasdaq_log.diff().dropna()
+# seasonal differencing => yoy instantanteous returns
+industrial_production_log_diff = industrial_production_log.diff(12).dropna()
+#Plot Series
+with sns.axes_style('dark'):
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(14, 8))
+
+    nasdaq.plot(ax=axes[0][0],
+                title='NASDAQ  Composite Index')
+    axes[0][0].text(x=.03,
+                    y=.85,
+                    s=f'ADF: {tsa.adfuller(nasdaq.dropna())[1]:.4f}',
+                    transform=axes[0][0].transAxes)
+    axes[0][0].set_ylabel('Index')
+
+    nasdaq_log.plot(ax=axes[1][0],
+                    sharex=axes[0][0])
+    axes[1][0].text(x=.03, y=.85,
+                    s=f'ADFl: {tsa.adfuller(nasdaq_log.dropna())[1]:.4f}',
+                    transform=axes[1][0].transAxes)
+    axes[1][0].set_ylabel('Log')
+
+    nasdaq_log_diff.plot(ax=axes[2][0],
+                         sharex=axes[0][0])
+    axes[2][0].text(x=.03, y=.85,
+                    s=f'ADF: {tsa.adfuller(nasdaq_log_diff.dropna())[1]:.4f}',
+                    transform=axes[2][0].transAxes)
+    axes[2][0].set_ylabel('Log, Diff')
+
+    industrial_production.plot(ax=axes[0][1],
+                               title='Industrial Production: Manufacturing')
+    axes[0][1].text(x=.03, y=.85,
+                    s=f'ADF: {tsa.adfuller(industrial_production)[1]:.4f}',
+                    transform=axes[0][1].transAxes)
+    axes[0][1].set_ylabel('Index')
+
+    industrial_production_log.plot(ax=axes[1][1],
+                                   sharex=axes[0][1])
+    axes[1][1].text(x=.03, y=.85,
+                    s=f'ADF: {tsa.adfuller(industrial_production_log.dropna())[1]:.4f}',
+                    transform=axes[1][1].transAxes)
+    axes[1][1].set_ylabel('Log')
+
+    industrial_production_log_diff.plot(ax=axes[2][1],
+                                        sharex=axes[0][1])
+    axes[2][1].text(x=.83, y=.85,
+                    s=f'ADF: {tsa.adfuller(industrial_production_log_diff.dropna())[1]:.4f}',
+                    transform=axes[2][1].transAxes)
+    axes[2][1].set_ylabel('Log, Seasonal Diff')
+    sns.despine()
+    fig.tight_layout()
+    fig.align_ylabels(axes)
+#Correlogram
+plot_correlogram(nasdaq_log_diff, lags=100, title='NASDAQ Composite (Log, Diff)')
+plot_correlogram(industrial_production_log_diff, title='Industrial Production (Seasonal Diff)')
+
+plt.show()
