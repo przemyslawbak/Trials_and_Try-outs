4043ed6e40dfd93f395444416f1a1fc36b8787dd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-22 11:26:37 +0200

    New sample project added;

---
+++ 32_Python_Financial/BookAlgorythmic_28_BaggedDecisionTr/Sample/Sample.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.filterwarnings('ignore')
+from pathlib import Path
+import numpy as np
+from numpy.random import choice, normal
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import BaggingRegressor
+
