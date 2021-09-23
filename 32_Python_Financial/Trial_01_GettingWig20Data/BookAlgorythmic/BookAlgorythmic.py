6105f6376807278a6a8787c97a839664fca695be
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-23 22:36:47 +0200

    Weight dictionary added;

--- 32_Python_Financial/Trial_01_GettingWig20Data/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/Trial_01_GettingWig20Data/BookAlgorythmic/BookAlgorythmic.py
@@ -9,6 +9,27 @@ DATA_STORE = 'data/assets.h5'
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
+udzial = {'pko': 0.14514,
+          'ale': 0.0948,
+          'kgh': 0.08804,
+          'pkn': 0.08767,
+          'pzu': 0.08346,
+          'peo': 0.0723,
+          'dnp': 0.06627,
+          'lpp': 0.06076,
+          'cdr': 0.05405,
+          'spl': 0.03879,
+          'cps': 0.03515,
+          'pge': 0.02887,
+          'opl': 0.02029,
+          'lts': 0.02014,
+          'acp': 0.01869,
+          'ccc': 0.01817,
+          'tpe': 0.01445,
+          'jsw': 0.01082,
+          'mrc': 0.00239,
+          }
+
 wig20_d = pd.read_csv('../../data/wig20_d.csv')
 peaks = pd.DataFrame();
 peaks['Data'] = wig20_d['Data']
@@ -26,7 +47,7 @@ def verifyPeakDirection(otwarcie, zamkniecie, rozmiar):
 
 def processWig20Csv(df, file):
     global peaks
-    name = os.path.basename(file)
+    name = os.path.basename(file).split('.', 1)[0] .split('_', 1)[0]
     df['Waga'] = 0.0187
     df['Sredni_wolumen_10'] = df['Wolumen'].rolling(min_periods=1, window=10).sum() / 10
     for i, row in df.iterrows():
