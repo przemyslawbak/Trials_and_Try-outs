13a46ee3e518c48eaa6e9f673e2e3240e1d46927
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-24 10:52:14 +0200

    Completed Pyplot examples;

--- 34_Python_Matplotlib/Tutorial_02_Pyplot/Sample/Sample.py
+++ 34_Python_Matplotlib/Tutorial_02_Pyplot/Sample/Sample.py
@@ -1,2 +1,91 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+#pyplot sample
+plt.plot([1, 2, 3, 4], [2,4,6,20])
+plt.ylabel('some numbers')
+plt.xlabel('other numbers')
+
+#style format
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plt.axis([0, 6, 0, 20])
+
+#numpy
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+
+#Plotting with categorical variables
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+plt.figure(figsize=(9, 3))
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+
+#Working with multiple figures and axes
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+
+#Logarithmic and other nonlinear axes
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# make up some data in the open interval (0, 1)
+y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+y = y[(y > 0) & (y < 1)]
+y.sort()
+x = np.arange(len(y))
+
+# plot with various axes scales
+plt.figure()
+
+# linear
+plt.subplot(221)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.title('linear')
+plt.grid(True)
+
+# log
+plt.subplot(222)
+plt.plot(x, y)
+plt.yscale('log')
+plt.title('log')
+plt.grid(True)
+
+# symmetric log
+plt.subplot(223)
+plt.plot(x, y - y.mean())
+plt.yscale('symlog', linthresh=0.01)
+plt.title('symlog')
+plt.grid(True)
+
+# logit
+plt.subplot(224)
+plt.plot(x, y)
+plt.yscale('logit')
+plt.title('logit')
+plt.grid(True)
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+
+plt.show()
