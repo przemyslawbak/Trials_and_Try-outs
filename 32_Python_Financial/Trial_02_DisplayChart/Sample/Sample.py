c336e260bfa37ea58357899b95be6296465da360
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-09-24 16:17:07 +0200

    Changed approach;

--- 32_Python_Financial/Trial_02_DisplayChart/Sample/Sample.py
+++ 32_Python_Financial/Trial_02_DisplayChart/Sample/Sample.py
@@ -1,43 +1,38 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.cbook as cbook
+from mplfinance.original_flavor import candlestick_ohlc #fixed
+import pandas as pd
+import matplotlib.dates as mpl_dates
 
+plt.style.use('ggplot')
+
+#https://saralgyaan.com/posts/python-candlestick-chart-matplotlib-tutorial-chapter-11/
 # Load a numpy structured array from yahoo csv data with fields date, open,
 # close, volume, adj_close from the mpl-data/example directory.  This array
 # stores the date as an np.datetime64 with a day unit ('D') in the 'date'
 # column.
-#data = cbook.get_sample_data('goog.npz', np_load=True)['price_data']
+#data1 = cbook.get_sample_data('goog.npz', np_load=True)['price_data']
 data = pd.read_csv('../../data/wig20_d.csv')
 
+ohlc = data.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
+ohlc['Date'] = pd.to_datetime(ohlc['Date'])
+ohlc['Date'] = ohlc['Date'].apply(mpl_dates.date2num)
+ohlc = ohlc.astype(float)
+
+# Creating Subplots
 fig, ax = plt.subplots()
-ax.plot('date', 'adj_close', data=data)
 
-# Major ticks every 6 months.
-fmt_half_year = mdates.MonthLocator(interval=6)
-ax.xaxis.set_major_locator(fmt_half_year)
+candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
 
-# Minor ticks every month.
-fmt_month = mdates.MonthLocator()
-ax.xaxis.set_minor_locator(fmt_month)
+# Setting labels & titles
+ax.set_xlabel('Date')
+ax.set_ylabel('Price')
+fig.suptitle('Daily Candlestick Chart of anything')
 
-# Text in the x axis will be displayed in 'YYYY-mm' format.
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-
-# Round to nearest years.
-datemin = np.datetime64(data['date'][0], 'Y')
-datemax = np.datetime64(data['date'][-1], 'Y') + np.timedelta64(1, 'Y') #exception
-ax.set_xlim(datemin, datemax)
-
-# Format the coords message box, i.e. the numbers displayed as the cursor moves
-# across the axes within the interactive GUI.
-ax.format_xdata = mdates.DateFormatter('%Y-%m')
-ax.format_ydata = lambda x: f'${x:.2f}'  # Format the price.
-ax.grid(True)
-
-# Rotates and right aligns the x labels, and moves the bottom of the
-# axes up to make room for them.
+# Formatting Date
+date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+ax.xaxis.set_major_formatter(date_format)
 fig.autofmt_xdate()
+
+fig.tight_layout()
 
 plt.show()
