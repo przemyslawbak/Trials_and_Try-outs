0b0e899fba47c1d18752f09aa8a4d06903a6e12f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-01 16:47:43 +0200

    Polynomial regression sample added;

--- 31_Python_ML/BookHandsOn_02_TrainingLinearModel/BookHandsOn/BookHandsOn.py
+++ 31_Python_ML/BookHandsOn_02_TrainingLinearModel/BookHandsOn/BookHandsOn.py
@@ -1,9 +1,12 @@
 import numpy as np
+import numpy.random as rnd
 import sys
 import sklearn
 import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 #Linear regression using the Normal Equation
 X = 2 * np.random.rand(100, 1)
@@ -136,5 +139,32 @@ plt.xlabel(r"$\theta_0$", fontsize=20)
 plt.ylabel(r"$\theta_1$   ", fontsize=20, rotation=0)
 plt.axis([2.5, 4.5, 2.3, 3.9])
 
+#Polynomial regression
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+X[0]
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+X_new=np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([-3, 3, 0, 10])
 
 plt.show()
