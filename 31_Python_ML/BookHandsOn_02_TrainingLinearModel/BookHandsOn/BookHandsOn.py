c72bc46112e0fbfa3d2411c6f65fed6d32868f8e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-01 16:41:32 +0200

    Mini-batch gradient descent sample added;

--- 31_Python_ML/BookHandsOn_02_TrainingLinearModel/BookHandsOn/BookHandsOn.py
+++ 31_Python_ML/BookHandsOn_02_TrainingLinearModel/BookHandsOn/BookHandsOn.py
@@ -94,7 +94,47 @@ for epoch in range(n_epochs):
 plt.plot(X, y, "b.")                                 # not shown
 plt.xlabel("$x_1$", fontsize=18)                     # not shown
 plt.ylabel("$y$", rotation=0, fontsize=18)           # not shown
-plt.axis([0, 2, 0, 15])  
+plt.axis([0, 2, 0, 15])
+
+#Mini-batch gradient descent
+theta_path_mgd = []
+
+n_iterations = 50
+minibatch_size = 20
+
+np.random.seed(42)
+theta = np.random.randn(2,1)  # random initialization
+
+t0, t1 = 200, 1000
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+t = 0
+for epoch in range(n_iterations):
+    shuffled_indices = np.random.permutation(m)
+    X_b_shuffled = X_b[shuffled_indices]
+    y_shuffled = y[shuffled_indices]
+    for i in range(0, m, minibatch_size):
+        t += 1
+        xi = X_b_shuffled[i:i+minibatch_size]
+        yi = y_shuffled[i:i+minibatch_size]
+        gradients = 2/minibatch_size * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(t)
+        theta = theta - eta * gradients
+        theta_path_mgd.append(theta)
+
+theta_path_bgd = np.array(theta_path_bgd)
+theta_path_sgd = np.array(theta_path_sgd)
+theta_path_mgd = np.array(theta_path_mgd)
+
+plt.figure(figsize=(7,4))
+plt.plot(theta_path_sgd[:, 0], theta_path_sgd[:, 1], "r-s", linewidth=1, label="Stochastic")
+plt.plot(theta_path_mgd[:, 0], theta_path_mgd[:, 1], "g-+", linewidth=2, label="Mini-batch")
+plt.plot(theta_path_bgd[:, 0], theta_path_bgd[:, 1], "b-o", linewidth=3, label="Batch")
+plt.legend(loc="upper left", fontsize=16)
+plt.xlabel(r"$\theta_0$", fontsize=20)
+plt.ylabel(r"$\theta_1$   ", fontsize=20, rotation=0)
+plt.axis([2.5, 4.5, 2.3, 3.9])
 
 
 plt.show()
