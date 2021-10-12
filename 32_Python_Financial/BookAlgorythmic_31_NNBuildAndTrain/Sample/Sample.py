fa8c8469496ce1b9bf00973b6d6a24ddf4b18f1e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-12 21:04:46 +0200

    Sample project completed;

--- 32_Python_Financial/BookAlgorythmic_31_NNBuildAndTrain/Sample/Sample.py
+++ 32_Python_Financial/BookAlgorythmic_31_NNBuildAndTrain/Sample/Sample.py
@@ -1,2 +1,301 @@
 import warnings
 warnings.filterwarnings('ignore')
+from pathlib import Path
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.datasets import make_circles
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from mpl_toolkits.mplot3d import Axes3D  # 3D plots
+import seaborn as sns
+
+# plotting style
+sns.set_style('white')
+# for reproducibility
+np.random.seed(seed=42)
+
+results_path = Path('results')
+if not results_path.exists():
+    results_path.mkdir()
+
+#input
+# dataset params
+N = 50000
+factor = 0.1
+noise = 0.1
+
+n_iterations = 50000
+learning_rate = 0.0001
+momentum_factor = .5
+
+# generate data
+X, y = make_circles(
+    n_samples=N,
+    shuffle=True,
+    factor=factor,
+    noise=noise)
+
+# define outcome matrix
+Y = np.zeros((N, 2))
+for c in [0, 1]:
+    Y[y == c, c] = 1
+
+ax = sns.scatterplot(x=X[:, 0], 
+                y=X[:, 1], 
+                hue=y,
+               style=y,
+               markers=['_', '+'])
+ax.set_title('Synthetic Classification Data')
+sns.despine()
+plt.tight_layout()
+plt.savefig(results_path / 'ffnn_data', dpi=300);
+
+def logistic(z):
+    """Logistic function."""
+    return 1 / (1 + np.exp(-z))
+
+def hidden_layer(input_data, weights, bias):
+    """Compute hidden activations"""
+    return logistic(input_data @ weights + bias)
+
+def softmax(z):
+    """Softmax function"""
+    return np.exp(z) / np.sum(np.exp(z), axis=1, keepdims=True)
+
+def output_layer(hidden_activations, weights, bias):
+    """Compute the output y_hat"""
+    return softmax(hidden_activations @ weights + bias)
+
+def forward_prop(data, hidden_weights, hidden_bias, output_weights, output_bias):
+    """Neural network as function."""
+    hidden_activations = hidden_layer(data, hidden_weights, hidden_bias)
+    return output_layer(hidden_activations, output_weights, output_bias)
+
+def predict(data, hidden_weights, hidden_bias, output_weights, output_bias):
+    """Predicts class 0 or 1"""
+    y_pred_proba = forward_prop(data,
+                                hidden_weights,
+                                hidden_bias,
+                                output_weights,
+                                output_bias)
+    return np.around(y_pred_proba)
+
+def loss(y_hat, y_true):
+    """Cross-entropy"""
+    return - (y_true * np.log(y_hat)).sum()
+
+def loss_gradient(y_hat, y_true):
+    """output layer gradient"""
+    return y_hat - y_true
+
+def output_weight_gradient(H, loss_grad):
+    """Gradients for the output layer weights"""
+    return  H.T @ loss_grad
+
+def output_bias_gradient(loss_grad):
+    """Gradients for the output layer bias"""
+    return np.sum(loss_grad, axis=0, keepdims=True)
+
+def hidden_layer_gradient(H, out_weights, loss_grad):
+    """Error at the hidden layer.
+    H * (1-H) * (E . Wo^T)"""
+    return H * (1 - H) * (loss_grad @ out_weights.T)
+
+def hidden_weight_gradient(X, hidden_layer_grad):
+    """Gradient for the weight parameters at the hidden layer"""
+    return X.T @ hidden_layer_grad
+
+def hidden_bias_gradient(hidden_layer_grad):
+    """Gradient for the bias parameters at the output layer"""
+    return np.sum(hidden_layer_grad, axis=0, keepdims=True)
+
+def initialize_weights():
+    """Initialize hidden and output weights and biases"""
+
+    # Initialize hidden layer parameters
+    hidden_weights = np.random.randn(2, 3)
+    hidden_bias = np.random.randn(1, 3)
+
+    # Initialize output layer parameters
+    output_weights = np.random.randn(3, 2)
+    output_bias = np.random.randn(1, 2)
+    return hidden_weights, hidden_bias, output_weights, output_bias
+
+def compute_gradients(X, y_true, w_h, b_h, w_o, b_o):
+    """Evaluate gradients for parameter updates"""
+
+    # Compute hidden and output layer activations
+    hidden_activations = hidden_layer(X, w_h, b_h)
+    y_hat = output_layer(hidden_activations, w_o, b_o)
+
+    # Compute the output layer gradients
+    loss_grad = loss_gradient(y_hat, y_true)
+    out_weight_grad = output_weight_gradient(hidden_activations, loss_grad)
+    out_bias_grad = output_bias_gradient(loss_grad)
+
+    # Compute the hidden layer gradients
+    hidden_layer_grad = hidden_layer_gradient(hidden_activations, w_o, loss_grad)
+    hidden_weight_grad = hidden_weight_gradient(X, hidden_layer_grad)
+    hidden_bias_grad = hidden_bias_gradient(hidden_layer_grad)
+
+    return [hidden_weight_grad, hidden_bias_grad, out_weight_grad, out_bias_grad]
+
+# change individual parameters by +/- eps
+eps = 1e-4
+
+# initialize weights and biases
+params = initialize_weights()
+
+# Get all parameter gradients
+grad_params = compute_gradients(X, Y, *params)
+
+# Check each parameter matrix
+for i, param in enumerate(params):
+    # Check each matrix entry
+    rows, cols = param.shape
+    for row in range(rows):
+        for col in range(cols):
+            # change current entry by +/- eps
+            params_low = deepcopy(params)
+            params_low[i][row, col] -= eps
+
+            params_high = deepcopy(params)
+            params_high[i][row, col] += eps
+
+            # Compute the numerical gradient
+            loss_high = loss(forward_prop(X, *params_high), Y)
+            loss_low = loss(forward_prop(X, *params_low), Y)
+            numerical_gradient = (loss_high - loss_low) / (2 * eps)
+
+            backprop_gradient = grad_params[i][row, col]
+            
+            # Raise error if numerical and backprop gradient differ
+            assert np.allclose(numerical_gradient, backprop_gradient), ValueError(
+                    f'Numerical gradient of {numerical_gradient:.6f} not close to '
+                    f'backprop gradient of {backprop_gradient:.6f}!')
+
+print('No gradient errors found')
+
+def update_momentum(X, y_true, param_list,
+                    Ms, momentum_term,
+                    learning_rate):
+    """Update the momentum matrices."""
+    # param_list = [hidden_weight, hidden_bias, out_weight, out_bias]
+    # gradients = [hidden_weight_grad, hidden_bias_grad,
+    #               out_weight_grad, out_bias_grad]
+    gradients = compute_gradients(X, y_true, *param_list)
+    return [momentum_term * momentum - learning_rate * grads
+            for momentum, grads in zip(Ms, gradients)]
+
+def update_params(param_list, Ms):
+    """Update the parameters."""
+    # param_list = [Wh, bh, Wo, bo]
+    # Ms = [MWh, Mbh, MWo, Mbo]
+    return [P + M for P, M in zip(param_list, Ms)]
+
+def train_network(iterations=1000, lr=.01, mf=.1):
+    # Initialize weights and biases
+    param_list = list(initialize_weights())
+
+    # Momentum Matrices = [MWh, Mbh, MWo, Mbo]
+    Ms = [np.zeros_like(M) for M in param_list]
+
+    train_loss = [loss(forward_prop(X, *param_list), Y)]
+    for i in range(iterations):
+        if i % 1000 == 0: print(f'{i:,d}', end=' ', flush=True)
+        # Update the moments and the parameters
+        Ms = update_momentum(X, Y, param_list, Ms, mf, lr)
+
+        param_list = update_params(param_list, Ms)
+        train_loss.append(loss(forward_prop(X, *param_list), Y))
+
+    return param_list, train_loss
+
+trained_params, train_loss = train_network(iterations=n_iterations, 
+                                           lr=learning_rate, 
+                                           mf=momentum_factor)
+
+hidden_weights, hidden_bias, output_weights, output_bias = trained_params
+
+ax = pd.Series(train_loss).plot(figsize=(12, 3), title='Loss per Iteration', xlim=(0, n_iterations), logy=True)
+ax.set_xlabel('Iteration')
+ax.set_ylabel('$\\log \\xi$', fontsize=12)
+sns.despine()
+plt.tight_layout()
+plt.savefig(results_path / 'ffnn_loss', dpi=300)
+
+n_vals = 200
+x1 = np.linspace(-1.5, 1.5, num=n_vals)
+x2 = np.linspace(-1.5, 1.5, num=n_vals)
+xx, yy = np.meshgrid(x1, x2)  # create the grid
+
+# Initialize and fill the feature space
+feature_space = np.zeros((n_vals, n_vals))
+for i in range(n_vals):
+    for j in range(n_vals):
+        X_ = np.asarray([xx[i, j], yy[i, j]])
+        feature_space[i, j] = np.argmax(predict(X_, *trained_params))
+
+# Create a color map to show the classification colors of each grid point
+cmap = ListedColormap([sns.xkcd_rgb["pale red"],
+                       sns.xkcd_rgb["denim blue"]])
+
+# Plot the classification plane with decision boundary and input samples
+plt.contourf(xx, yy, feature_space, cmap=cmap, alpha=.25)
+
+# Plot both classes on the x1, x2 plane
+data = pd.DataFrame(X, columns=['$x_1$', '$x_2$']).assign(Class=pd.Series(y).map({0:'negative', 1:'positive'}))
+sns.scatterplot(x='$x_1$', y='$x_2$', hue='Class', data=data, style=y, markers=['_', '+'], legend=False)
+plt.title('Decision Boundary')
+sns.despine()
+plt.tight_layout()
+plt.savefig(results_path / 'boundary', dpi=300);
+
+n_vals = 25
+x1 = np.linspace(-1.5, 1.5, num=n_vals)
+x2 = np.linspace(-1.5, 1.5, num=n_vals)
+xx, yy = np.meshgrid(x1, x2)  # create the grid
+X_ = np.array([xx.ravel(), yy.ravel()]).T
+
+fig = plt.figure(figsize=(6, 4))
+with sns.axes_style("whitegrid"):
+    ax = Axes3D(fig)
+
+ax.plot(*hidden_layer(X[y == 0], hidden_weights, hidden_bias).T,
+        '_', label='negative class', alpha=0.75)
+ax.plot(*hidden_layer(X[y == 1], hidden_weights, hidden_bias).T,
+        '+', label='positive class', alpha=0.75)
+
+ax.set_xlabel('$h_1$', fontsize=12)
+ax.set_ylabel('$h_2$', fontsize=12)
+ax.set_zlabel('$h_3$', fontsize=12)
+ax.view_init(elev=30, azim=-20)
+# plt.legend(loc='best')
+plt.title('Projection of X onto the hidden layer H')
+sns.despine()
+plt.tight_layout()
+plt.savefig(results_path / 'projection3d', dpi=300)
+
+zz = forward_prop(X_, hidden_weights, hidden_bias, output_weights, output_bias)[:, 1].reshape(25, -1)
+zz.shape
+
+fig = plt.figure()
+with sns.axes_style("whitegrid"):
+    ax = fig.gca(projection='3d')
+ax.plot_surface(xx, yy, zz, alpha=.25)
+ax.set_title('Learned Function')
+ax.set_xlabel('$x_1$', fontsize=12)
+ax.set_ylabel('$x_2$', fontsize=12)
+ax.set_zlabel('$y$', fontsize=12)
+ax.view_init(elev=45, azim=-20)
+sns.despine()
+fig.tight_layout()
+fig.savefig(results_path / 'surface', dpi=300);
+
+
+
+
+
+plt.show()
