3999ce60c5415f4afae9b840c3d7a02accfd1721
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-14 11:07:23 +0200

    Working on TF sample;

--- 36_Python_NN/TutorialTf_01_TimeSeriesForecasting/Sample/Sample.py
+++ 36_Python_NN/TutorialTf_01_TimeSeriesForecasting/Sample/Sample.py
@@ -13,3 +13,8 @@ import tensorflow as tf
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
