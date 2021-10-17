f4deb5dff588ed5dbea2df56733fd458309dcd77
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-17 18:31:51 +0200

    New sample projects added for NN future prediction;

---
+++ 36_Python_NN/TutorialTf_09_SimpleExample/Sample/Sample.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+items = np.array([7,2,3,1,9,6,4,4,8,10,13,17,19,17,16,19,22,23,18,15,13,11,17,14,11,9,7,9,14,18,16,17,14,20,21,24,22])
+times = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37])
+
+#preparing dataset
+split_time = 15
+x_train = items[:split_time]
+y_train = times[:split_time]
+x_valid = items[split_time:]
+y_valid = times[split_time:]
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    return ds.batch(batch_size).prefetch(1)
+
+#creating windowed dataset
+tf.keras.backend.clear_session()
+shuffle_buffer_size = 10
+window_size = 5
+batch_size = 10
+train_set = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+print(x_train.shape)
+
+#creating model
+tf.keras.backend.clear_session()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv1D(filters=60, kernel_size=5,
+                      strides=1, padding="causal",
+                      activation="relu",
+                      input_shape=[None, 1]),
+  tf.keras.layers.LSTM(60, return_sequences=True),
+  tf.keras.layers.LSTM(60, return_sequences=True),
+  tf.keras.layers.Dense(30, activation="relu"),
+  tf.keras.layers.Dense(10, activation="relu"),
+  tf.keras.layers.Dense(1),
+  tf.keras.layers.Lambda(lambda x: x * 400)
+])
+
+#model summary
+model.summary()
+
+#compiling and training model
+optimizer = tf.keras.optimizers.SGD(lr=1e-5, momentum=0.9)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mae"])
+history = model.fit(train_set,epochs=500)
+
+
+#evaluating model
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+loss=history.history['loss']
+epochs=range(len(loss)) # Get number of epochs
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r')
+plt.title('Training loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["Loss"])
+plt.figure() #loss graph
+
+
+zoomed_loss = loss[200:]
+zoomed_epochs = range(200,500)
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(zoomed_epochs, zoomed_loss, 'r')
+plt.title('Training loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["Loss"])
+
+plt.figure()
+
+#predicting
+def model_forecast(model, series, window_size):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size))
+    ds = ds.batch(32).prefetch(1)
+    forecast = model.predict(ds)
+    return forecast
+
+#using function to do forecast
+rnn_forecast = model_forecast(model, items[..., np.newaxis], window_size)
+rnn_forecast = rnn_forecast[split_time - window_size:-1, -1, 0]
+
+plt.figure(figsize=(10, 6))
+plot_series(y_valid, x_valid)
+plot_series(y_valid, rnn_forecast)
+
+tf.keras.metrics.mean_absolute_error(x_valid, rnn_forecast).numpy()
+
+
+
+plt.show()
