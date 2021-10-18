90fa0a316047f05c5409ae42b1e937de91d2ac5f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-10-18 09:48:11 +0200

    2 problems solved;

--- 36_Python_NN/TutorialTf_17_Weights/Sample/Sample.py
+++ 36_Python_NN/TutorialTf_17_Weights/Sample/Sample.py
@@ -106,7 +106,7 @@ def split_window(self, features):
 
 WindowGenerator.split_window = split_window
 
-def plot(self, model=None, plot_col='T (degC)', max_subplots=3):
+def plot(self, model=None, plot_col='T (degC)', max_subplots=1):
   inputs, labels = self.example
   plt.figure(figsize=(12, 8))
   plot_col_index = self.column_indices[plot_col]
