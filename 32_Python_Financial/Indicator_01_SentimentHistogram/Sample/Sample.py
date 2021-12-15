95aee7ab4498561b4cadfe5ae25f0065942642f4
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-12-15 11:27:42 +0100

    Reorganized code;

--- 32_Python_Financial/Indicator_01_SentimentHistogram/Sample/Sample.py
+++ 32_Python_Financial/Indicator_01_SentimentHistogram/Sample/Sample.py
@@ -18,6 +18,40 @@ mpl.rcParams['axes.grid'] = False
 csv_path = 'GPW_DLY WIG20, 15.csv'
 df = pd.read_csv(csv_path)
 
+def ma(typ, src, len):
+    result = 0
+    #more to be chosen in the link
+    if typ=="WMA":
+        result = wma(src, len)
+    return result
+
+#https://www.askpython.com/python/weighted-moving-average
+def wma(Data, period):
+    weighted = []
+    for i in range(len(Data)):
+            try:
+                total = numpy.arange(1, period + 1, 1)
+                matrix = Data[i - period + 1: i + 1, 3:4]
+                matrix = numpy.ndarray.flatten(matrix)
+                matrix = total * matrix
+                wma = (matrix.sum()) / (total.sum()) # WMA
+                weighted = numpy.append(weighted, wma) 
+            except ValueError:
+                pass
+    return weighted
+
+#https://www.alpharithms.com/calculating-moving-averages-in-python-585117/
+def sma(Data, period):
+    return Data.rolling(period).mean()
+
+def highest(Data, period):
+    list = Data.rolling(period).sort()
+    return list1[-1]
+
+def lowest(Data, period):
+    list = Data.rolling(period).sort()
+    return list1[0]
+
 #https://www.tradingview.com/script/RlRxtEuQ-Sentiment-Histogram/
 print('reading inputs...')
 period = 14
@@ -69,37 +103,3 @@ if (Bear <  Bear[1] and Bear > Bull):
 
 #todo
 plot(diff,"Sentiment",_color,2,plot.style_columns)
-
-def ma(typ, src, len):
-    result = 0
-    #more to be chosen in the link
-    if typ=="WMA":
-        result = wma(src, len)
-    return result
-
-#https://www.askpython.com/python/weighted-moving-average
-def wma(Data, period):
-    weighted = []
-    for i in range(len(Data)):
-            try:
-                total = numpy.arange(1, period + 1, 1)
-                matrix = Data[i - period + 1: i + 1, 3:4]
-                matrix = numpy.ndarray.flatten(matrix)
-                matrix = total * matrix
-                wma = (matrix.sum()) / (total.sum()) # WMA
-                weighted = numpy.append(weighted, wma) 
-            except ValueError:
-                pass
-    return weighted
-
-#https://www.alpharithms.com/calculating-moving-averages-in-python-585117/
-def sma(Data, period):
-    return Data.rolling(period).mean()
-
-def highest(Data, period):
-    list = Data.rolling(period).sort()
-    return list1[-1]
-
-def lowest(Data, period):
-    list = Data.rolling(period).sort()
-    return list1[0]
