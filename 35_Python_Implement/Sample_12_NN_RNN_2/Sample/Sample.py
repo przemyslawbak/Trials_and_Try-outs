2f01837a2ff44c3270ecc00d44e37aa122d0399a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-12-28 22:34:43 +0100

    Working on future display;

--- 35_Python_Implement/Sample_12_NN_RNN_2/Sample/Sample.py
+++ 35_Python_Implement/Sample_12_NN_RNN_2/Sample/Sample.py
@@ -12,28 +12,29 @@ data = pd.read_csv(csv_path)
 
 df = data.drop(['time'], axis = 1)
 df.fillna(0)
-
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(df)
-
-X = []
-y = []
+df['forecast'] = 0
 split_percent = 0.80
 split = int(split_percent*len(df))
 test = 60
 num_batch = 32
 num_epochs = 5
+future_prediction = 30
 
+#scaling
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df)
+X = []
+y = []
 for i in range(test, scaled_data.shape[0]):
     X.append(scaled_data [i-test:i])
     y.append(scaled_data [i, 0])
 
 #splitting data
 X, y = np.array(X), np.array(y)
-X_train = X[:split]
-y_train = y[:split]
-X_test = X[split:]
-y_test = y[split:]
+X_train = X[:split] #close_train
+y_train = y[:split] #date_train
+X_test = X[split:] #close_test
+y_test = y[split:] #date_test
 
 model = Sequential()
 model.add(LSTM(units= 20, activation = 'relu',\
@@ -49,26 +50,25 @@ model.add(Dropout(0.5))
 model.add(Dense(units = 1))
 
 model.compile(optimizer='adam', loss = 'mean_squared_error')
-model.fit(X_train, y_train, epochs=num_epochs, batch_size=num_batch)#
+model.fit(X_train, y_train, epochs=num_epochs, batch_size=num_batch)
+
+#future
+future_data=[X_test.index[-1]+ x for x in range(0,future_prediction)]
+future_df=pd.DataFrame(index=future_data[1:],columns=X_test.columns)
+future_df.tail()
+X_test=pd.concat([X_test,future_df])
+
 y_pred = model.predict(X_test)#
 
 #reverse scale resuts
 res_scaler = MinMaxScaler()
 res_scaler.min_, res_scaler.scale_ = scaler.min_[0], scaler.scale_[0]
-y_pred = res_scaler.inverse_transform(y_pred)#
-y_test = res_scaler.inverse_transform(y_test.reshape(-1,1))#
 
-plt.plot(y_test[-test:], color = 'black')#
-plt.plot(y_pred[-test:], color = 'blue')
-plt.show()
-
-
-#future
-future_dates=[df.index[-1]+ x for x in range(0,futures)]
-future_df=pd.DataFrame(index=future_dates[1:],columns=df.columns)
-future_df.tail()
+y_pred = res_scaler.inverse_transform(y_pred)
+y_test = res_scaler.inverse_transform(y_test.reshape(-1,1))
 
 #results
-resut_df=pd.concat([df,future_df])
-resut_df['forecast'] = model_fit.predict(start = len(df.index), end = len(df.index) + futures, dynamic= True)
+resut_df['forecast'] = model.predict(start = len(df.index), end = len(df.index) + future_prediction, dynamic= True)
 resut_df[['close', 'forecast']].plot(figsize=(12, 8))
+plt.show()
+
