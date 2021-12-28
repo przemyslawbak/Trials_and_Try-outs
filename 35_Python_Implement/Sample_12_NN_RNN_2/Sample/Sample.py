176dd8a841c3a05096a6fb6f2d786de0d70a43ea
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-12-28 20:34:42 +0100

    Added future prediction logic; TODO file updated; Marked splitted data;

--- 35_Python_Implement/Sample_12_NN_RNN_2/Sample/Sample.py
+++ 35_Python_Implement/Sample_12_NN_RNN_2/Sample/Sample.py
@@ -33,9 +33,7 @@ y_test = y[split:]
 
 model = Sequential()
 model.add(LSTM(units= 20, activation = 'relu',\
-return_sequences = True,\
-input_shape = (X_train.shape[1], X_train.
-shape[2])))
+return_sequences = True))
 model.add(Dropout(0.5))
 model.add(LSTM(units= 40, \
 activation = 'relu', \
@@ -46,18 +44,28 @@ activation = 'relu'))
 model.add(Dropout(0.5))
 model.add(Dense(units = 1))
 
-#TODO: futures from Sample_06_AutoArima
+#future
+future_dates=[df.index[-1]+ x for x in range(0,futures)]
+future_df=pd.DataFrame(index=future_dates[1:],columns=df.columns)
+future_df.tail()
 
+#results
 model.compile(optimizer='adam', loss = 'mean_squared_error')
-model.fit(X_train, y_train, epochs=5, batch_size=32)
-y_pred = model.predict(X_test)
+resut_df=pd.concat([df,future_df])
+resut_df['forecast'] = model_fit.predict(start = len(df.index), end = len(df.index) + futures, dynamic= True)
+resut_df[['close', 'forecast']].plot(figsize=(12, 8))
+
+
+
+model.fit(X_train, y_train, epochs=5, batch_size=32)#
+y_pred = model.predict(X_test)#
 
 #reverse scale resuts
 res_scaler = MinMaxScaler()
 res_scaler.min_, res_scaler.scale_ = scaler.min_[0], scaler.scale_[0]
-y_pred = res_scaler.inverse_transform(y_pred)
-y_test = res_scaler.inverse_transform(y_test.reshape(-1,1))
+y_pred = res_scaler.inverse_transform(y_pred)#
+y_test = res_scaler.inverse_transform(y_test.reshape(-1,1))#
 
-plt.plot(y_test[-test:], color = 'black')
+plt.plot(y_test[-test:], color = 'black')#
 plt.plot(y_pred[-test:], color = 'blue')
 plt.show()
