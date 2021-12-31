9b7463beafe06961608e22c9d5bed35b5e41e353
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-12-31 12:12:46 +0100

    Splitting data into training and test data sets;

--- 36_Python_NN/Tutorial_03_RNN_FuturePrediction/Sample/Sample.py
+++ 36_Python_NN/Tutorial_03_RNN_FuturePrediction/Sample/Sample.py
@@ -16,20 +16,31 @@ training_set = dataset_train[['close', 'high', 'low', 'open', 'volume']]
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
-#Create a data structure with 60-time steps and 1 output
-X_train = []
-y_train = []
-for i in range(60, len(training_set_scaled)):
-    X_train.append(training_set_scaled[i-60: i, 0])
-    y_train.append(training_set_scaled[i, 0])
+#Variables
+time_step = 60
+split_percent = 0.80
+split = int(split_percent*len(df))
 
-X_train_arr, y_train_arr = np.array(X_train), np.array(y_train)
+#Create a data structure with 60-time steps and 1 output
+X = []
+y = []
+for i in range(time_step, len(training_set_scaled)):
+    X.append(training_set_scaled[i-time_step: i, 0])
+    y.append(training_set_scaled[i, 0])
+
+X_train_arr, y_train_arr = np.array(X), np.array(y)
+
+#Reshaping
+X_train_arr = np.reshape(X_train_arr, (X_train_arr.shape[0], X_train_arr.shape[1], 1))
 
 print(X_train_arr.shape)
 print(y_train_arr.shape)
 
-#Reshaping
-X_train_arr = np.reshape(X_train_arr, (X_train_arr.shape[0], X_train_arr.shape[1], 1))
+#Split data
+X_train_splitted = X_train_arr[:split]
+y_train_splitted = y_train_arr[:split]
+X_test_splitted = X_train_arr[split:]
+y_test_splitted = y_train_arr[split:]
 
 print(X_train_arr.shape)
 print(y_train_arr.shape)
@@ -52,6 +63,10 @@ model.add(Dense(units = 1))
 model.compile(optimizer='adam', loss = 'mean_squared_error')
 
 #Fit to the training set
-model.fit(X_train_arr, y_train_arr, epochs=10, batch_size=32)
+model.fit(X_train_splitted, y_train_splitted, epochs=10, batch_size=32)
+
+#plot
+plt.plot(day_new,sc.inverse_transform(df1[1158:]))
+plt.plot(day_pred,sc.inverse_transform(lst_output))
 
 #todo: future prediction
