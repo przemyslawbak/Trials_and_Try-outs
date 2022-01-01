5d967c8b4b526cddcf251b24370626e5a5c2e3f5
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-01-01 23:48:03 +0100

    Working on target data with several cols; Reshaping y_train_arr;

--- 35_Python_Implement/Sample_15_NN_RNN_5/Sample/Sample.py
+++ 35_Python_Implement/Sample_15_NN_RNN_5/Sample/Sample.py
@@ -13,7 +13,6 @@ training_set = dataset_train[['close', 'high', 'low', 'open', 'Volume']]
 #Perform feature scaling to transform the data
 scaler = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = scaler.fit_transform(training_set)
-print(training_set_scaled)
 
 #Variables
 future_prediction = 30
@@ -21,31 +20,37 @@ time_step = 60
 split_percent = 0.80
 split = int(split_percent*len(training_set_scaled))
 
-#Create a data structure with 60-time steps and 1 output
+#Create a data structure with n-time steps
 X = []
 y = []
 for i in range(time_step, len(training_set_scaled)):
     X.append(training_set_scaled[i-time_step:i, 0:len(training_set.columns)]) #take all columns into the set
-    y.append(training_set_scaled[i, 0])
+    y.append(training_set_scaled[i, 0:len(training_set.columns)]) #take all columns into the set
 
 X_train_arr, y_train_arr = np.array(X), np.array(y)
+print(X_train_arr.shape) #(2494, 60, 5)
+print(y_train_arr.shape) #(2494, 5)
 
-#Reshaping
+#Reshaping to rows/time_step/columns
 X_train_arr = np.reshape(X_train_arr, (X_train_arr.shape[0], X_train_arr.shape[1], X_train_arr.shape[2])) #rows/time_step/columns
+#y_train_arr = np.reshape(y_train_arr, (y_train_arr.shape[0], y_train_arr.shape[2])) #rows/columns
 
+print(X_train_arr.shape) #(2494, 60, 5)
+print(y_train_arr.shape) #(2494, 5)
 
 #Split data
 X_train_splitted = X_train_arr[:split]
 y_train_splitted = y_train_arr[:split]
 X_test_splitted = X_train_arr[split:]
 y_test_splitted = y_train_arr[split:]
-print(X_train_splitted)
+print(X_train_splitted.shape)
+print(y_train_splitted.shape)
 
 #Initialize the RNN
 model = Sequential()
 
 #Add the LSTM layers and some dropout regularization
-model.add(LSTM(units= 50, activation = 'relu', return_sequences = True, input_shape = (X_train_arr.shape[1], 1))) #todo: update
+model.add(LSTM(units= 50, activation = 'relu', return_sequences = True, input_shape = (X_train_arr.shape[1], X_train_arr.shape[2]))) #time_step/columns
 model.add(Dropout(0.2))
 model.add(LSTM(units= 40, activation = 'relu', return_sequences = True))
 model.add(Dropout(0.2))
@@ -63,15 +68,14 @@ model.fit(X_train_splitted, y_train_splitted, epochs=3, batch_size=32)
 
 #Test results
 y_pred = model.predict(X_test_splitted)
-print(y_pred.shape)
-y_pred = y_pred.reshape(-1,1)
-print(y_pred.reshape(-1,1))
 
-#Reverse scale resuts
+#Reverse scale resuts and reshaping data to display
 res_scaler = MinMaxScaler()
 res_scaler.min_, res_scaler.scale_ = scaler.min_[0], scaler.scale_[0]
-y_pred = res_scaler.inverse_transform(y_pred.reshape(-1,1))
-y_test_splitted = res_scaler.inverse_transform(y_test_splitted.reshape(-1,1))
+print(y_pred.shape)
+print(y_test_splitted.shape)
+y_pred = res_scaler.inverse_transform(y_pred.reshape(-1,1)) #todo: verify if close data is displayed
+y_test_splitted = res_scaler.inverse_transform(y_test_splitted.reshape(-1,1)) #todo: verify if close data is displayed
 
 #plot
 plt.figure(figsize=(14,5))
