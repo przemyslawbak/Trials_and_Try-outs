5cd5fb11d7fe1f654b5523e8ce20d0fdd54749ae
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-01-01 20:15:11 +0100

    Working on many-to-many RNN

--- 35_Python_Implement/Sample_15_NN_RNN_5/Sample/Sample.py
+++ 35_Python_Implement/Sample_15_NN_RNN_5/Sample/Sample.py
@@ -13,8 +13,10 @@ training_set = dataset_train[['close', 'high', 'low', 'open', 'Volume']]
 #Perform feature scaling to transform the data
 scaler = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = scaler.fit_transform(training_set)
+print(training_set_scaled)
 
 #Variables
+future_prediction = 30
 time_step = 60
 split_percent = 0.80
 split = int(split_percent*len(training_set_scaled))
@@ -23,31 +25,27 @@ split = int(split_percent*len(training_set_scaled))
 X = []
 y = []
 for i in range(time_step, len(training_set_scaled)):
-    X.append(training_set_scaled[i-time_step: i, 0])
+    X.append(training_set_scaled[i-time_step:i, 0:len(training_set.columns)]) #take all columns into the set
     y.append(training_set_scaled[i, 0])
 
 X_train_arr, y_train_arr = np.array(X), np.array(y)
 
 #Reshaping
-X_train_arr = np.reshape(X_train_arr, (X_train_arr.shape[0], X_train_arr.shape[1], 1))
+X_train_arr = np.reshape(X_train_arr, (X_train_arr.shape[0], X_train_arr.shape[1], X_train_arr.shape[2])) #rows/time_step/columns
 
-print(X_train_arr.shape)
-print(y_train_arr.shape)
 
 #Split data
 X_train_splitted = X_train_arr[:split]
 y_train_splitted = y_train_arr[:split]
 X_test_splitted = X_train_arr[split:]
 y_test_splitted = y_train_arr[split:]
-
-print(X_train_arr.shape)
-print(y_train_arr.shape)
+print(X_train_splitted)
 
 #Initialize the RNN
 model = Sequential()
 
 #Add the LSTM layers and some dropout regularization
-model.add(LSTM(units= 50, activation = 'relu', return_sequences = True, input_shape = (X_train_arr.shape[1], 1)))
+model.add(LSTM(units= 50, activation = 'relu', return_sequences = True, input_shape = (X_train_arr.shape[1], 1))) #todo: update
 model.add(Dropout(0.2))
 model.add(LSTM(units= 40, activation = 'relu', return_sequences = True))
 model.add(Dropout(0.2))
@@ -61,10 +59,13 @@ model.add(Dense(units = 1))
 model.compile(optimizer='adam', loss = 'mean_squared_error')
 
 #Fit to the training set
-model.fit(X_train_splitted, y_train_splitted, epochs=10, batch_size=32)
+model.fit(X_train_splitted, y_train_splitted, epochs=3, batch_size=32)
 
 #Test results
 y_pred = model.predict(X_test_splitted)
+print(y_pred.shape)
+y_pred = y_pred.reshape(-1,1)
+print(y_pred.reshape(-1,1))
 
 #Reverse scale resuts
 res_scaler = MinMaxScaler()
@@ -74,8 +75,8 @@ y_test_splitted = res_scaler.inverse_transform(y_test_splitted.reshape(-1,1))
 
 #plot
 plt.figure(figsize=(14,5))
-plt.plot(y_test_splitted[-60:], label = "Real values")
-plt.plot(y_pred[-60:], label = 'Predicted values')
+plt.plot(y_test_splitted[-time_step:], label = "Real values")
+plt.plot(y_pred[-time_step:], label = 'Predicted values')
 plt.title('WIG20 prediction test')
 plt.xlabel('time')
 plt.ylabel('Close price')
