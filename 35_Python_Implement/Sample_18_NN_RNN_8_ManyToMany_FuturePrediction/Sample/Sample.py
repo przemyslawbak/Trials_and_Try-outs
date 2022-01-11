ca0a67618191d519ef1aa67ba81a4ab97b984910
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-01-12 00:56:32 +0100

    Working on future predictions;

--- 35_Python_Implement/Sample_18_NN_RNN_8_ManyToMany_FuturePrediction/Sample/Sample.py
+++ 35_Python_Implement/Sample_18_NN_RNN_8_ManyToMany_FuturePrediction/Sample/Sample.py
@@ -25,16 +25,16 @@ num_verbose=1
 #Variables
 features = len(training_set.columns)
 future_steps = 33
-time_step = 100 #learning step
+time_steps = 100 #learning step
 split_percent = 0.80 #train/test daa split percent (80%)
 split = int(split_percent*len(training_set_scaled)) #split percent multiplying by data rows
 
 #Create a data structure with n-time steps
 X = []
 y = []
-for i in range(time_step + 1, len(training_set_scaled)):
-    X.append(training_set_scaled[i-time_step-1:i-1, 0:features]) #take all columns into the set, including time_step legth
-    y.append(training_set_scaled[i-time_step:i, 0:features]) #take all columns into the set, including time_step legth
+for i in range(time_steps + 1, len(training_set_scaled)):
+    X.append(training_set_scaled[i-time_steps-1:i-1, 0:features]) #take all columns into the set, including time_steps legth
+    y.append(training_set_scaled[i-time_steps:i, 0:features]) #take all columns into the set, including time_steps legth
 
 X_train_arr, y_train_arr = np.array(X), np.array(y)
 
@@ -47,11 +47,11 @@ y_train_splitted = y_train_arr[:split] #80%) model train target data
 X_test_splitted = X_train_arr[split:] #(20%) test prediction input data
 y_test_splitted = y_train_arr[split:] #(20%) test prediction compare data
 
-#Reshaping to rows/time_step/columns
-X_train_splitted = np.reshape(X_train_splitted, (X_train_splitted.shape[0], time_step, features))
-y_train_splitted = np.reshape(y_train_splitted, (y_train_splitted.shape[0], time_step, features))
-X_test_splitted = np.reshape(X_test_splitted, (X_test_splitted.shape[0], time_step, features))
-y_test_splitted = np.reshape(y_test_splitted, (y_test_splitted.shape[0], time_step, features))
+#Reshaping to rows/time_steps/columns
+X_train_splitted = np.reshape(X_train_splitted, (X_train_splitted.shape[0], time_steps, features))
+y_train_splitted = np.reshape(y_train_splitted, (y_train_splitted.shape[0], time_steps, features))
+X_test_splitted = np.reshape(X_test_splitted, (X_test_splitted.shape[0], time_steps, features))
+y_test_splitted = np.reshape(y_test_splitted, (y_test_splitted.shape[0], time_steps, features))
 
 print(X_train_arr.shape) #(2494, 100, 4)
 print(y_train_arr.shape) #(2494, 100, 4)
@@ -63,7 +63,7 @@ model = Sequential()
 
 #Add Bidirectional LSTM, has better performance than stacked LSTM
 model = Sequential()
-model.add(Bidirectional(LSTM(128, activation='relu', input_shape = (time_step, features), return_sequences=True))) #todo: tune qty o layers
+model.add(Bidirectional(LSTM(128, activation='relu', input_shape = (time_steps, features), return_sequences=True))) #todo: tune qty o layers
 model.add(Dense(4)) #4 outputs, gives output shape (x, 100, 4)
 
 #Compile many-to-many
@@ -74,11 +74,12 @@ model.fit(X_train_splitted, y_train_splitted, epochs=num_epochs, batch_size=num_
 
 #Predicting future
 def predict_future():
+    global X_test_splitted
     pred = model.predict(X_test_splitted, verbose=num_verbose) #predict from updated X_test_splitted
     last_pred_item = pred[-1]
     X_test_splitted_shape = X_test_splitted.shape
     X_test_splitted = X_test_splitted = np.append(X_test_splitted, last_pred_item)
-    X_test_splitted = np.reshape(X_test_splitted, (X_test_splitted_shape[0] + 1, time_step, features)) #reshape X_test_splitted to (x + 1, 100, 4)
+    X_test_splitted = np.reshape(X_test_splitted, (X_test_splitted_shape[0] + 1, time_steps, features)) #reshape X_test_splitted to (x + 1, 100, 4)
     return pred
 
 def nextFutureStep():
@@ -93,23 +94,21 @@ for next in range(future_steps):
     new_item = nextFutureStep() #assign item
     future_results = np.append(future_results, new_item) #add last step to future_results prediction array
 
-#Combine future time_step based prediction results with initial prediction based on test data
-future_results = np.reshape(future_results, (future_steps, time_step, features)) #reshaping to (33, 100, 4)
-y_pred_future = future_results[:, -1:, :] #from future_results copying an array, where we do not need 60 time_steps
-y_pred = np.append(y_pred, y_pred_future) #combine initial y_pred result array with predicted y_pred_future
+#Combine future time_steps based prediction results with initial prediction based on test data and reshape
+y_pred = np.append(y_pred, future_results) #combine initial y_pred result array with predicted y_pred_future
+y_pred = np.reshape(y_pred, (y_test_splitted.shape[0] + future_steps, time_steps, features)) #reshaping to (450 + 33, 100, 4)
 
-#Reshaping data for inverse transforming
-y_test_splitted = np.reshape(y_test_splitted, (y_test_splitted.shape[0], features)) #reshaping to (450, 1, 4)
-y_pred = np.reshape(y_pred, (y_test_splitted.shape[0] + future_steps, features)) #reshaping to (450 + 33, 1, 4)
+#Reshaping data for reverse transforming
+#y_test_splitted = np.reshape(y_test_splitted, (y_test_splitted.shape[0], features)) #reshaping to (450, 1, 4)
 
 #Reversing transform to get proper data values
-y_test_splitted = scaler.inverse_transform(y_test_splitted)
-y_pred = scaler.inverse_transform(y_pred)
+y_test_splitted = scaler.inverse_transform(y_test_splitted) #Found array with dim 3. Estimator expected <= 2.
+y_pred = scaler.inverse_transform(y_pred) #Found array with dim 3. Estimator expected <= 2.
 
 #Plot data
 plt.figure(figsize=(14,features))
-plt.plot(y_test_splitted[-time_step:, 3], label = "Real values") #I am interested only with display of column index 3
-plt.plot(y_pred[-time_step - future_steps:, 3], label = 'Predicted values') # #I am interested only with display of column index 3
+plt.plot(y_test_splitted[-time_steps:, 3], label = "Real values") #I am interested only with display of column index 3
+plt.plot(y_pred[-time_steps - future_steps:, 3], label = 'Predicted values') # #I am interested only with display of column index 3
 plt.title('Prediction test')
 plt.xlabel('Time')
 plt.ylabel('Column index 3')
