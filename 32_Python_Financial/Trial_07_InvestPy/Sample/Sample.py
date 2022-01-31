553ee9c8f064d53ef7aeb036a2c4bab1d0f8d6e6
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-01-31 02:45:01 +0100

    Link added; Time range changed;

--- 32_Python_Financial/Trial_07_InvestPy/Sample/Sample.py
+++ 32_Python_Financial/Trial_07_InvestPy/Sample/Sample.py
@@ -1,10 +1,11 @@
 #https://stackoverflow.com/a/69166303
 #https://investpy.readthedocs.io/_api/news.html
+#https://www.investing.com/economic-calendar/
 
 import investpy
 
 data = investpy.economic_calendar(
     from_date='12/09/2021',
-    to_date  ='13/09/2021'
+    to_date  ='13/10/2021'
 )
-print(data.head())
+print(data)
