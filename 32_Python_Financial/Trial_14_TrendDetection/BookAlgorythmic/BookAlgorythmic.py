d2e34640afb6521ea11af0a3ece198e93b7b2b2e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-02-28 23:48:30 +0100

    Trend detection samples added;

--- 32_Python_Financial/Trial_14_TrendDetection/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/Trial_14_TrendDetection/BookAlgorythmic/BookAlgorythmic.py
@@ -3,16 +3,6 @@ import numpy as np
 t = np.linspace(0, 5, 100)
 x = t + np.random.normal(size=100)
 
-from scipy import signal
-x_detrended = signal.detrend(x)
-
-from matplotlib import pyplot as plt
-plt.figure(figsize=(5, 4))
-plt.plot(t, x, label="x")
-plt.plot(t, x_detrended, label="x_detrended")
-plt.legend(loc='best')
-#plt.show()
-
 #trend
 import numpy as np
 from matplotlib import pyplot as plt
