e13e9cdee9cf38f301b130ae9f98c589f9cf5a29
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-03-01 00:01:53 +0100

    New approach for trend detection;

--- 32_Python_Financial/Trial_14_TrendDetection/BookAlgorythmic/BookAlgorythmic.py
+++ 32_Python_Financial/Trial_14_TrendDetection/BookAlgorythmic/BookAlgorythmic.py
@@ -7,10 +7,14 @@ from scipy import signal
 import pandas as pd
 
 wig20_d = pd.read_csv('../../data/wig20_d_1.csv')
+x_raw = wig20_d['Zamkniecie'].to_numpy()
+period = 100
 
-n = 150
-t = np.linspace(0, 10, n)
-x_raw = .4 * t + np.random.normal(size=n)
+#n = 150
+#t = np.linspace(0, 10, n)
+#x_raw = .4 * t + np.random.normal(size=n)
+
+x_raw = x_raw[-period:]
 
 x = signal.detrend(x_raw)
 d = x_raw - x
@@ -19,7 +23,6 @@ is_positive_trend = d[-1] > d[0]
 m = "+" if is_positive_trend else "-"
 
 plt.figure(figsize=(5, 4))
-plt.plot(t, x_raw, label=f"Raw data ({m})")
-plt.plot(t, d)
+plt.plot(x_raw, label=f"Raw data ({m})")
 plt.legend(loc='best')
 plt.show()
