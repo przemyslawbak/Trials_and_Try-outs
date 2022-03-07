a81d1be4e20d36f9d7ec567f5e4bb6f345a226f8
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-03-07 11:56:55 +0100

    Added list of banned phrases;

--- 12_String/String_21_BlacklistedDomains/List_Comparer/Program.cs
+++ 12_String/String_21_BlacklistedDomains/List_Comparer/Program.cs
@@ -9,13 +9,18 @@ namespace List_Comparer
     {
         static void Main(string[] args)
         {
-            List<string> blacklisted = new List<string>(File.ReadAllLines("2.txt"));
+            List<string> toRemove = new List<string>(File.ReadAllLines("2.txt"));
             List<string> blacklist = new List<string>(File.ReadAllLines("3.txt"));
+            List<string> phrasesBanned = new List<string>(File.ReadAllLines("4.txt"));
             List<string> lista = new List<string>(File.ReadAllLines("1.txt"));
             List<string> containBlacklisted = lista.Where(e => blacklist.Any(k => e.ToLower().Contains(k.ToLower()))).ToList();
+            List<string> containPhrases = lista.Where(e => phrasesBanned.Any(k => e.ToLower().Contains(k.ToLower()))).ToList();
+            List<string> removed = lista.Where(e => toRemove.Any(k => e.ToLower().Contains(k.ToLower()))).ToList();
             lista = lista.Except(containBlacklisted).ToList();
+            lista = lista.Except(containPhrases).ToList();
+            lista = lista.Except(removed).ToList();
 
-            lista.RemoveAll(x => blacklisted.Any(d => x.ToLower().Contains(d.ToLower())));
+            //lista.RemoveAll(x => toRemove.Any(d => x.ToLower().Contains(d.ToLower())));
             File.WriteAllLines("output.txt", lista);
             Console.WriteLine("saved");
             Console.ReadKey();
