2236ed627e5c019285349830dda47cdf18f77c0a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-03-19 16:33:13 +0100

    SMTP SimpleExample added;

---
+++ 11_SMTP/SMTP_08_SmtpServer_SimpleExample/SMTP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SMTP
+{
+    //https://github.com/cosullivan/SmtpServer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleExample.Run();
+        }
+    }
+}
