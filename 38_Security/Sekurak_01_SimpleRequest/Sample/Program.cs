2de9d0def7e0632de7963515f14f6ff0943018d1
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-03-22 17:07:05 +0100

    Test;

--- 38_Security/Sekurak_01_SimpleRequest/Sample/Program.cs
+++ 38_Security/Sekurak_01_SimpleRequest/Sample/Program.cs
@@ -6,7 +6,7 @@ namespace Sample
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main(string[] args) 
         {
             HttpClient _client = new HttpClient();
             _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0");
