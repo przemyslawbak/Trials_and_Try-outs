a54d73b5b668037ce91c69489c074f10cc1659c9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-04-25 00:04:21 +0200

    Reading about arrays;

--- 01_AlbahariC#/9.0/02 Types/Albahari/Program.cs
+++ 01_AlbahariC#/9.0/02 Types/Albahari/Program.cs
@@ -67,6 +67,27 @@ Drugi wiersz";
             Console.Write($"Kwadrat ma {x} boki."); // drukuje: Kwadrat ma 4 boki.
             string s = $"255 w systemie szesnastkowym to {byte.MaxValue:X2}"; // X2 = cyfra szesnastkowa. Wynik: 255 w systemie szesnastkowym to FF"
 
+            //Tablice
+            char[] vowels = new char[6]; // deklaracja tablicy sześciu znaków
+            for (int i = 0; i < vowels.Length; i++)
+                Console.Write(vowels[i]); // aeiouy
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            //lub
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int[] a = new int[1000];
+            Console.Write(a[123]); // 0
+            int[] a = null; //poprawna instr
+            Index first = 0;
+            Index last = ^1;
+            char firstElement = vowels[first]; // 'a'
+            char lastElement = vowels[last]; // 'y'
+            char[] firstTwo = vowels[..2]; // 'a', 'e'
+            char[] lastThree = vowels[2..]; // 'o', 'u', 'y'
+            char[] middleOne = vowels[2..3]; // 'i'
+            int[,] matrix = new int[3, 3];
+
+
+
         }
 
     }
