6fa15737ad016917eaf6292993c1af624bbbd8ce
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-01 23:31:47 +0200

    New sample added;

---
+++ 39_Chanllenges/C#/HakerRank_03_TimeConversion/Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "07:05:45PM";
+
+            DateTime t = DateTime.Parse(s);
+
+            Console.WriteLine(t.ToString("HH:mm:ss")); //NOTE: in the challenge it has to return value
+
+        }
+    }
+}
