65f6c39edd2a45cfb52eec4a0b98cf03f6cce68d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-02 21:12:56 +0200

    New sample added;

---
+++ 39_Chanllenges/C#/HakerRank_14_CountingValleys/Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string path = "DDUUUUDD";
+
+        char[] arr = path.ToCharArray();
+
+        int elevation = 0;
+        int valleys = 0;
+
+        foreach (char step in arr)
+        {
+            int currentStep = step.Equals('U') ? 1 : -1;
+
+            if (elevation < 0 && (elevation + currentStep) == 0)
+            {
+                valleys++;
+            }
+
+            elevation += currentStep;
+        }
+
+        //return valleys
+    }
+}
