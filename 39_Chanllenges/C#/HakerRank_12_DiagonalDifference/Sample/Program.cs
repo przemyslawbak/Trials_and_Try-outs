a142e4bfd5cf59664ba2cbcf193f2eaad0b2e981
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-02 17:24:15 +0200

    New challenge added;

---
+++ 39_Chanllenges/C#/HakerRank_12_DiagonalDifference/Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        List<List<int>> arr = new List<List<int>>()
+        {
+            new List<int>() { 11, 2, 4 },
+            new List<int>() { 4, 5, 6 },
+            new List<int>() { 10, 8, -12 }
+        };
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            sum1 = sum1 + arr[i][i];
+            sum2 = sum2 + arr[i][arr.Count - i - 1];
+        }
+
+        var res = Math.Abs(sum1 - sum2);
+    }
+}
