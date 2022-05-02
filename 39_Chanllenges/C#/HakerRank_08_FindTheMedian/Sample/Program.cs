244913872fb96b07c9c30f020a715cc23bc3f3bf
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-02 14:41:51 +0200

    New challenge added;

---
+++ 39_Chanllenges/C#/HakerRank_08_FindTheMedian/Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        List<int> arr = new List<int>()
+        {
+            0,1,2,3,4,6,5,3
+        };
+
+        arr = arr.OrderBy(l => l).ToList();
+
+        int median = arr[(arr.Count - 1) / 2];
+        Console.WriteLine(median);
+    }
+}
