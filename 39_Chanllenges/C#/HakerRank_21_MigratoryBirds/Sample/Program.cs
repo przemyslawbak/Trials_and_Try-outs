a0da7578050730507f32064a088877e9a1bd88f9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-09 12:11:07 +0200

    New challenge added;

---
+++ 39_Chanllenges/C#/HakerRank_21_MigratoryBirds/Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        List<int> arr = new List<int>() { 1, 1,1, 2, 2,2, 3,3,3,3 };
+
+        int result = arr
+            .OrderBy(id => id)
+            .GroupBy(id => id)
+            .OrderByDescending(id => id.Count())
+            .Select(g => g.Key)
+            .FirstOrDefault();
+    }
+}
