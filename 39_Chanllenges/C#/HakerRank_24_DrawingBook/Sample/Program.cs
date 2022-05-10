f81a839845576b1cceb2c21268485ce99c4dd22e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-10 16:17:48 +0200

    Corrected code;

--- 39_Chanllenges/C#/HakerRank_24_DrawingBook/Sample/Program.cs
+++ 39_Chanllenges/C#/HakerRank_24_DrawingBook/Sample/Program.cs
@@ -6,5 +6,9 @@ class Solution
 {
     static void Main(String[] args)
     {
+        int n = 6;
+        int p = 5;
+
         int result = p / 2 < ((n % 2 == 1 ? n : n + 1) - p) / 2 ? p / 2 : ((n % 2 == 1 ? n : n + 1) - p) / 2;
     }
+}
