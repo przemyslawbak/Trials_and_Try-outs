1ba12d744dff196b242455731723eb06f4045c7a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-10 16:11:38 +0200

    New challenge added;

---
+++ 39_Chanllenges/C#/HakerRank_24_DrawingBook/Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int result = p / 2 < ((n % 2 == 1 ? n : n + 1) - p) / 2 ? p / 2 : ((n % 2 == 1 ? n : n + 1) - p) / 2;
+    }
