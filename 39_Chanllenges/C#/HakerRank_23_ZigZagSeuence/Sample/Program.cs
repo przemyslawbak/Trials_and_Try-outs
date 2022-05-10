37cff748b8e0ebac52a378a63269cec7f4832034
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-10 15:41:29 +0200

    New challenge in Python 3 added;

---
+++ 39_Chanllenges/C#/HakerRank_23_ZigZagSeuence/Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /*
+         * #Python 3
+         * 
+         * def findZigZagSequence(a, n):
+    a.sort()
+    mid = int(n/2) # change 1
+    a[mid], a[n-1] = a[n-1], a[mid]
+    st = mid + 1
+    ed = n - 2 # change 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1 # change 3
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+         */
+    }
+}
