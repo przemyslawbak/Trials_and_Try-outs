5466423a7d3400602dca9619362d123c391d494e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-11 20:25:18 +0200

    Python code changed;

--- 39_Chanllenges/C#/HakerRank_23_ZigZagSeuence/Sample/Program.cs
+++ 39_Chanllenges/C#/HakerRank_23_ZigZagSeuence/Sample/Program.cs
@@ -11,14 +11,15 @@ class Solution
          * 
          * def findZigZagSequence(a, n):
     a.sort()
-    mid = int(n/2) # change 1
+    mid = int(n / 2)
     a[mid], a[n-1] = a[n-1], a[mid]
+
     st = mid + 1
-    ed = n - 2 # change 2
+    ed = n - 2
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st]
         st = st + 1
-        ed = ed - 1 # change 3
+        ed = ed - 1
 
     for i in range (n):
         if i == n-1:
