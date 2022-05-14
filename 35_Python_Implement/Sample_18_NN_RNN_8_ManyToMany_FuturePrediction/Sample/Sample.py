8ea5428a3b94c6470f13f66541d6616b94868f71
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-05-14 16:58:55 +0200

    Updated model values, comments added;

--- 35_Python_Implement/Sample_18_NN_RNN_8_ManyToMany_FuturePrediction/Sample/Sample.py
+++ 35_Python_Implement/Sample_18_NN_RNN_8_ManyToMany_FuturePrediction/Sample/Sample.py
@@ -30,13 +30,13 @@ training_set_scaled = scaler.fit_transform(training_set)
 #Model values !!!!!!!!!SAMPLE SETUP!!!!!!!!!!
 dropout_rate=0.1
 num_layers=2
-future_steps = 30
-time_steps = 1000
-lstm_units = 64
-num_batch = 64
-num_epochs = 1
-num_validation=0.2
-num_verbose=1
+future_steps = 10
+time_steps = 500
+lstm_units = 512 #allow to learn very long sequences
+num_batch = 128 #number of samples to work through before updating the internal model parameters
+num_epochs = 10 #number times that the learning algorithm will work through the entire training dataset
+num_validation=0.2 #% split for validation set
+num_verbose=1 #how to display model fit progress (0 = silent, 1 = progress bar, 2 = one line per epoch)
 es_patinence = 5
 
 #Variables
