2d8f9d45b2f12adfee4af95e6c91a0bf4e8020ff
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-06-25 19:09:15 +0200

    Added MassTransit mediator sample;

---
+++ 41_MassTransit/GettingStarted_05_Mediator/IMediator.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+
+namespace GettingStarted
+{
+    public interface IMediator :
+        ISendEndpoint,
+        IPublishEndpoint,
+        IClientFactory,
+        IConsumePipeConnector,
+        IRequestPipeConnector,
+        IConsumeObserverConnector,
+        IConsumeMessageObserverConnector
+    {
+    }
+}
