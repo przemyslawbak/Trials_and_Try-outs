91b97e37a86e86dd281068dcfbac6a084a2be5ff
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-07-15 17:09:41 +0200

    Comment added;

--- 25_Other/Other_05_PollyClient/Other_04_Polly/Program.cs
+++ 25_Other/Other_05_PollyClient/Other_04_Polly/Program.cs
@@ -4,8 +4,13 @@ using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 var httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+    .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30));
 
-    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt));
+/*
+ * The above line will open the circuit and will start giving Circuit Open Exception
+ * after 2 attempts and keep the circuit Open for 30 seconds,
+ * which means you cannot make further calls to the API for that duration.
+ * */
 
 // Add services to the container.
 
