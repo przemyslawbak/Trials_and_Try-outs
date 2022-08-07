2da1e045f0cda16392065f8891c3d50700fc2e52
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-08-07 08:58:53 +0200

    New test sample project added;

---
+++ 15_Tests/03_Net_Integration_Tests/Tutorial_03_EmployeesApp/EmployeesApp/EmployeesApp.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,46 @@
+﻿using EmployeesApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace EmployeesApp.IntegrationTests
+{
+    public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<EmployeeContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<EmployeeContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryEmployeeTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                using (var appContext = scope.ServiceProvider.GetRequiredService<EmployeeContext>())
+                {
+                    try
+                    {
+                        appContext.Database.EnsureCreated();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            });
+        }
+    }
+}
