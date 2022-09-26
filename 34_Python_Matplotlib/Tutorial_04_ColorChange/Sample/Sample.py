00748ab13d30192979c146bd60017e3c29ddb739
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-09-26 10:29:22 +0200

    Parsing to numpy and df;

--- 34_Python_Matplotlib/Tutorial_04_ColorChange/Sample/Sample.py
+++ 34_Python_Matplotlib/Tutorial_04_ColorChange/Sample/Sample.py
@@ -6,10 +6,13 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 #pyplot sample https://stackoverflow.com/a/47470635
 sample = [10,4,6,20, 23, 29, 20]
 df = pd.DataFrame(sample)
-df = df.reset_index(drop=False)
+arr = df.to_numpy()
+df1 = pd.DataFrame(arr)
+print(df1)
+length = df1.shape[0]
 
-ax = df.iloc[:len(sample) -2,:].plot(y=0, color="crimson")
-df.iloc[len(sample) -3:,:].plot(y=0, color="C0", ax=ax)
+ax = df1.iloc[:length -2,:].plot(y=0, color="crimson")
+df1.iloc[length -3:,:].plot(y=0, color="C0", ax=ax)
 
 
 
