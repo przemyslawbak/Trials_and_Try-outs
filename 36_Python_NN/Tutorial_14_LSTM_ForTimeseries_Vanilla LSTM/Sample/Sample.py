70ad05fa3f27514d07715c02277182e2443e5d5a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-10-10 09:57:16 +0200

    New LSTM sample added;

---
+++ 36_Python_NN/Tutorial_14_LSTM_ForTimeseries_Vanilla LSTM/Sample/Sample.py
@@ -0,0 +1,54 @@
+# univariate data preparation
+from numpy import array
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Activation, Bidirectional
+
+#https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/
+
+#The split_sequence() function below implements this behavior and will split a given univariate sequence into multiple samples where each sample 
+#has a specified number of time steps and the output is a single time step.
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 3
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# summarize the data
+for i in range(len(X)):
+	print(X[i], y[i])
+
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+# define model
+model = Sequential()
+model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+
+# fit model
+model.fit(X, y, epochs=200, verbose=0)
+
+# demonstrate prediction
+x_input = array([70, 80, 90])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+
+# display result
+print(yhat)
