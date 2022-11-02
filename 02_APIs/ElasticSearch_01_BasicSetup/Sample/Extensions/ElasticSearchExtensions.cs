1fee7904364d552114915957b5fb748519085bf5
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-11-02 11:46:56 +0100

    ElasticSearch basic setup sample;

---
+++ 02_APIs/ElasticSearch_01_BasicSetup/Sample/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Nest;
+
+namespace Sample.Extensions
+{
+    public static class ElasticSearchExtensions
+    {
+        public static void AddElasticsearch(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var userName = "user";
+            var pass = "pass";
+            var url = configuration["ELKConfiguration:url"];
+            var defaultIndex = configuration["ELKConfiguration:index"];
+
+            var settings = new ConnectionSettings(new Uri(url)).BasicAuthentication(userName, pass)
+                .PrettyJson()
+                .DefaultIndex(defaultIndex);
+
+            AddDefaultMappings(settings);
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            CreateIndex(client, defaultIndex);
+        }
+
+        private static void AddDefaultMappings(ConnectionSettings settings)
+        {
+            settings
+                .DefaultMappingFor<Product>(m => m
+                    .Ignore(p => p.Price)
+                    .Ignore(p => p.Measurement)
+                );
+        }
+
+        private static void CreateIndex(IElasticClient client, string indexName)
+        {
+            var createIndexResponse = client.Indices.Create(indexName,
+                index => index.Map<Product>(x => x.AutoMap())
+            );
+        }
+    }
+}
