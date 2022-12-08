09f84fe1e9811717fbb50b2a65891f09e332db1e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-08 22:45:08 +0100

    Movement and Player updated; Animations updated;

--- 42_Unity_Engine/RPG/Assets/Scripts/MovementController.cs
+++ 42_Unity_Engine/RPG/Assets/Scripts/MovementController.cs
@@ -4,30 +4,73 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-//1
 	public float movementSpeed = 3.0f;
-	// 2
 	Vector2 movement = new Vector2();
-	// 3
+	// 1
+	Animator animator;
+	// 2
+	string animationState = "AnimationState";
 	Rigidbody2D rb2D;
+	// 3
+	enum CharStates
+	{
+		walkEast = 1,
+		walkSouth = 2,
+		walkWest = 3,
+		walkNorth = 4,
+		idleSouth = 5
+	}
 	private void Start()
 	{
 	// 4
+		animator = GetComponent<Animator>();
 		rb2D = GetComponent<Rigidbody2D>();
 	}
 	private void Update()
 	{
-	// Keep this empty for now
-	}
 	// 5
+		UpdateState();
+	}
 		void FixedUpdate()
 	{
 	// 6
+		MoveCharacter();
+	}
+	private void MoveCharacter()
+	{
 		movement.x = Input.GetAxisRaw("Horizontal");
 		movement.y = Input.GetAxisRaw("Vertical");
-	// 7
 		movement.Normalize();
-	// 8
 		rb2D.velocity = movement * movementSpeed;
+	}
+	private void UpdateState()
+	{
+	// 7
+		if (movement.x > 0)
+		{
+			animator.SetInteger(animationState, (int)
+			CharStates.walkEast);
+		}
+		else if (movement.x < 0)
+		{
+			animator.SetInteger(animationState, (int)
+			CharStates.walkWest);
+		}
+		else if (movement.y > 0)
+		{
+			animator.SetInteger(animationState, (
+			int)
+			CharStates.walkNorth);
+		}
+		else if (movement.y < 0)
+		{
+			animator.SetInteger(animationState, (int)
+			CharStates.walkSouth);
+		}
+		else
+		{
+			animator.SetInteger(animationState, (int)
+			CharStates.idleSouth);
+		}
 	}
 }
