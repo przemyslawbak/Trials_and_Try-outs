fb8ab2a004bda9bc17eac064cf0cfd3aea42e850
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-24 01:00:37 +0100

    Completed sample project;

--- 33_Python_Pandas/Trial_02_DfIterationAndUpdate/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Trial_02_DfIterationAndUpdate/Cookbook/Cookbook.py
@@ -10,55 +10,65 @@ df = df.reset_index()  # make sure indexes pair with number of rows
 
 buy_signals = []
 sell_signals = []
-profits = []
+transactions = []
 
-def verifySignals(buy, sell, is_long, is_short):
+def verifySignals(buy, sell, is_long, is_short, last_close):
+
+
+    if buy != 0 and sell != 0:
+
+        profit = sell - buy
+        transactions.append(profit)
+
+        buy = 0
+        sell = 0
+        is_long = False
+        is_short = False
+        
+        print('transaction closed')
 
     if len(buy_signals) < 3:
         return buy, sell, is_long, is_short
 
-    buy_signal = [-1.0, -1.0, -1.0]
-    sell_signal = [1.0, 1.0, 1.0]
+    buy_signal = [-1.0]
+    sell_signal = [1.0]
     
-    buy_sample = buy_signals[-3:]
-    sell_sample = sell_signals[-3:]
+    buy_sample = buy_signals[-1:]
+    sell_sample = sell_signals[-1:]
 
     if buy_signal == buy_sample and sell_signal == sell_sample and (is_long or is_short):
-        is_long = False
-        is_short = False
-        #todo: close position
+        if is_long:
+            sell = last_close
+        if is_short:
+            buy = last_close
+
         print('close position')
 
     if buy_signal == buy_sample and sell_signal != sell_sample and not is_short and not is_long:
+
+        buy = last_close
         is_long = True
-        is_short = False
-        #todo: long position
-        print('long position')
+
+        print('open long position')
 
     if buy_signal != buy_sample and sell_signal == sell_sample and not is_long and not is_short:
-        is_long = False
+
+        sell = last_close
         is_short = True
-        #todo: short position
-        print('short position')
+
+        print('open short position')
         
     if buy_signal == buy_sample and sell_signal != sell_sample and is_short:
-        is_long = False
-        is_short = False
-        #todo: close position
-        print('close position')
+
+        buy = last_close
+
+        print('close position with buy')
 
     if buy_signal != buy_sample and sell_signal == sell_sample and is_long:
-        is_long = False
-        is_short = False
-        #todo: close position
-        print('close position')
 
+        sell = last_close
 
-    if buy != 0 and sell != 0:
-        #todo:
-        print('transaction closed')
-        buy = 0
-        sell = 0
+        print('close position with sell')
 
     return buy, sell, is_long, is_short
 
@@ -70,8 +80,13 @@ is_short = False
 for index, row in df.iterrows():
     buy_signals.append(row['direction_pred_med'])
     sell_signals.append(row['direction_pred'])
-    buy, sell, is_long, is_short = verifySignals(buy, sell, is_long, is_short)
+    buy, sell, is_long, is_short = verifySignals(buy, sell, is_long, is_short, row['last_close'])
 
+print('')
+print('SUMMARY:')
+print('transactions no.: ' + str(len(transactions)))
+print('profits total: ' + str(sum(transactions)))
+print('max income: ' + str(max(transactions)))
+print('max dropdown: ' + str(min(transactions)))
 
-
-print(df)
+#todo: save to the file
