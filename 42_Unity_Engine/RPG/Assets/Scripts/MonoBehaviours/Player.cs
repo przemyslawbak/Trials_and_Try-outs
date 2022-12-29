c558920b1f657888ffaff8d7cd9693e628810769
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-29 19:01:51 +0100

    Circle collider added; Player script updated; Item object added;

--- 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class Player : Character
 {
-    
+    // 1
+void OnTriggerEnter2D(Collider2D collision)
+{
+// 2
+if (collision.gameObject.CompareTag("CanBePickedUp"))
+{
+// 3
+collision.gameObject.SetActive(false);
+}
+}
 }
