274dd1be68bda5b0269543a94ce4b7fefe02685b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-29 22:44:42 +0100

    Working on players hit points;

--- 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public class Player : Character
 {
-    public Inventory inventoryPrefab;
-    Inventory inventory;
-
     public HealthBar healthBarPrefab;
     HealthBar healthBar;
 
     public void Start()
 	{
         hitPoints.value = startingHitPoints;
-        inventory = Instantiate(inventoryPrefab);
         healthBar = Instantiate(healthBarPrefab);
         healthBar.character = this;
 	}
@@ -29,7 +25,6 @@ public class Player : Character
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
-                        shouldDisappear = inventory.AddItem(hitObject);
                         break;
                     case Item.ItemType.HEALTH:
                         shouldDisappear = AdjustHitPoints(hitObject.quantity);
