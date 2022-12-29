ef193de9b61c31271cad3ce73724b2cf74f512b9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-29 19:55:12 +0100

    Adding heart object with scripts etc;

--- 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -8,8 +8,17 @@ void OnTriggerEnter2D(Collider2D collision)
 // 2
 if (collision.gameObject.CompareTag("CanBePickedUp"))
 {
+// 1
+// Note: This should all be on a single line
+Item hitObject = collision.gameObject.
+GetComponent<Consumable>().item;
+// 2
+if (hitObject != null)
+{
 // 3
+print("it: " + hitObject.objectName);
 collision.gameObject.SetActive(false);
+}
 }
 }
 }
