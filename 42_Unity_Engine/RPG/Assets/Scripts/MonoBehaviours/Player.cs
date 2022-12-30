4b018e504bd04ba600859af2bc69ee84e00c8b1d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2022-12-30 01:37:43 +0100

    Added inventory object;

--- 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 42_Unity_Engine/RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Player : Character
 {
+    public Inventory inventoryPrefab;
+    Inventory inventory;
+
     public HealthBar healthBarPrefab;
     HealthBar healthBar;
 
     public void Start()
 	{
         hitPoints.value = startingHitPoints;
+        inventory = Instantiate(inventoryPrefab);
         healthBar = Instantiate(healthBarPrefab);
         healthBar.character = this;
 	}
@@ -25,6 +29,7 @@ public class Player : Character
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
+                        shouldDisappear = inventory.AddItem(hitObject);
                         break;
                     case Item.ItemType.HEALTH:
                         shouldDisappear = AdjustHitPoints(hitObject.quantity);
