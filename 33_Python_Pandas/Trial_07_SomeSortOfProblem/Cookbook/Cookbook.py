c983fc99e04e3476d456504eda401c787f16181f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-08 20:38:49 +0100

    Spiking code;

---
+++ 33_Python_Pandas/Trial_07_SomeSortOfProblem/Cookbook/Cookbook.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import signals_model
+from os import listdir
+from os.path import isfile, join
+
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
+
+models_accuracies_path = 'models_accuracies.csv'
+
+#sorting models accuracies
+dfAcc = pd.read_csv(models_accuracies_path, sep='|', dtype = 'float64', converters = {'file_name': str})
+modelFilePaths = []
+modelFilePaths.append(dfAcc['file_name'].iloc[0])
+#dfAcc = dfAcc.query('val_acc >= 0.85')
+dfAcc = dfAcc[(dfAcc['val_acc'] >= 0.85) & (dfAcc['acc'] >= 0.85)]
+dfAcc = dfAcc[(dfAcc['val_loss'] <= 0.01) & (dfAcc['acc'] <= 0.01)]
+dfAcc = dfAcc[(dfAcc['val_mse'] <= 0.01) & (dfAcc['mse'] <= 0.01)]
+dfAcc = dfAcc.sort_values(['val_mse', 'mse', 'val_acc', 'acc', 'val_loss', 'loss'], ascending=[True, True, False, False, True, True])
+if dfAcc.shape[0] < rng:
+    rng = dfAcc.shape[0]
+for index, row in range(rng):
+    modelFilePaths.append(row['file_name'])
+modelFilePaths = list(set(modelFilePaths))
---
+++ 33_Python_Pandas/Trial_07_SomeSortOfProblem/Cookbook/signals_model.py
@@ -0,0 +1,7 @@
+class AllSignals():
+    direction_pred_med_signals = []
+    direction_pred_signals = []
+    combined_result_signals = []
+    pattern_sum_result_trend_signals = []
+    sentiment_trend_signals = []
+    med_peak_sums_trend_signals = []
