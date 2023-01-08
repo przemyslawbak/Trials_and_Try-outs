8945c6d4985340e0cb6818e31daf1323ef552eae
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-08 22:40:03 +0100

    Corrected iteration;

--- 33_Python_Pandas/Trial_07_SomeSortOfProblem/Cookbook/Cookbook.py
+++ 33_Python_Pandas/Trial_07_SomeSortOfProblem/Cookbook/Cookbook.py
@@ -10,16 +10,16 @@ pd.set_option('display.width', 1000)
 models_accuracies_path = 'models_accuracies.csv'
 
 #sorting models accuracies
-dfAcc = pd.read_csv(models_accuracies_path, sep='|', dtype = 'float64', converters = {'file_name': str})
+dfAcc = pd.read_csv(models_accuracies_path, sep='|')
 modelFilePaths = []
 modelFilePaths.append(dfAcc['file_name'].iloc[0])
-#dfAcc = dfAcc.query('val_acc >= 0.85')
 dfAcc = dfAcc[(dfAcc['val_acc'] >= 0.85) & (dfAcc['acc'] >= 0.85)]
-dfAcc = dfAcc[(dfAcc['val_loss'] <= 0.01) & (dfAcc['acc'] <= 0.01)]
+dfAcc = dfAcc[(dfAcc['val_loss'] <= 0.01) & (dfAcc['loss'] <= 0.01)]
 dfAcc = dfAcc[(dfAcc['val_mse'] <= 0.01) & (dfAcc['mse'] <= 0.01)]
 dfAcc = dfAcc.sort_values(['val_mse', 'mse', 'val_acc', 'acc', 'val_loss', 'loss'], ascending=[True, True, False, False, True, True])
+rng = 10
 if dfAcc.shape[0] < rng:
     rng = dfAcc.shape[0]
-for index, row in range(rng):
+for index, row in dfAcc.iterrows():
     modelFilePaths.append(row['file_name'])
 modelFilePaths = list(set(modelFilePaths))
