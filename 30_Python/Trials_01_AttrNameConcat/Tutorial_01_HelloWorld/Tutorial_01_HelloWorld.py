ac1a28bfc157d037f769ca6e40069e8df32b5a22
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-09 18:35:48 +0100

    Printing with getattr;

--- 30_Python/Trials_01_AttrNameConcat/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
+++ 30_Python/Trials_01_AttrNameConcat/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
@@ -6,7 +6,4 @@ some_object = models.SomeModel()
 for i in iters:
     prop_name = 'attr_' + str(i)
     setattr(some_object, prop_name, str(i))
-    
-print(some_object.attr_1)
-print(some_object.attr_2)
-print(some_object.attr_3)
+    print(getattr(some_object, prop_name))
