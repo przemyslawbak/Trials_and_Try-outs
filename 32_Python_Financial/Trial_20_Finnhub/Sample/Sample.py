2ad580ba61c4319efc98ef15696c13ab6b47ba7b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-18 20:37:48 +0100

    Finnhub API sample added;

---
+++ 32_Python_Financial/Trial_20_Finnhub/Sample/Sample.py
@@ -0,0 +1,12 @@
+import finnhub
+
+# Setup client
+finnhub_client = finnhub.Client(api_key="YOUR API KEY")
+
+# Stock candles
+res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+print(res)
+
+#Convert to Pandas Dataframe
+import pandas as pd
+print(pd.DataFrame(res))
