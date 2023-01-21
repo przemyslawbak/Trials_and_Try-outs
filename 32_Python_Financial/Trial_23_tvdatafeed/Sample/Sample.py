efb128e2dcaeae81a328da8f79f1a124ec6ad20a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-21 12:01:43 +0100

    Playing with tvDatafeed API;

--- 32_Python_Financial/Trial_23_tvdatafeed/Sample/Sample.py
+++ 32_Python_Financial/Trial_23_tvdatafeed/Sample/Sample.py
@@ -1,10 +1,25 @@
 from tvDatafeed import TvDatafeed, Interval
 import login_data
+import appModels
+
+#docs: https://github.com/StreamAlpha/tvdatafeed
 
 username, password = login_data.getLoginData()
 
 tv = TvDatafeed(username, password)
 
-crudeoil_data = tv.get_hist(symbol='CRUDEOIL',exchange='MCX',interval=Interval.in_1_hour,n_bars=5000,fut_contract=1)
+#symbols = tv.search_symbol('GOLD')
+#for symbol in symbols:
+#    print()
+#    #print('symbol:' + symbol['symbol'] + ' exchange:' + symbol['exchange'] + ' type:' + symbol['type'] + ' provider_id:' + symbol['provider_id'] + ' description:' + symbol['description']) 
+#    print()
 
-print(crudeoil_data)
+sth = appModels.TvSearchModel('NATURALGAS', 'CURRENCYCOM', False)
+
+
+if not sth.futures:
+    data = tv.get_hist(symbol=sth.symbol,exchange=sth.exchange,interval=Interval.in_1_hour,n_bars=20000)
+else:
+    data = tv.get_hist(symbol=sth.symbol,exchange=sth.exchange,interval=Interval.in_1_hour,n_bars=20000, fut_contract=1)
+
+print(data)
