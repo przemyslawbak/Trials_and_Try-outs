ce95c1d535baf93b5c8718d0a0f20f197554a3d5
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-01-29 18:31:10 +0100

    New unittest sample added;

---
+++ 15_Tests/04_Python_Unit_Tests/Unittest_03_Simple_Examples/Sample/assertions.py
@@ -0,0 +1,28 @@
+#assertEqual(a, b) a == b
+#assertNotEqual(a, b) a != b
+#assertTrue(x) bool(x) is True
+#assertFalse(x) bool(x) is False
+#assertIs(a, b) a is b
+#assertIsNot(a, b) a is not b
+#assertIsNone(x) x is None
+#assertIsNotNone(x) x is not None
+#assertIn(a, b) a in b
+#assertNotIn(a, b) a not in b
+#assertIsInstance(a, b) isinstance(a, b)
+#assertNotIsInstance(a, b) not isinstance(a, b)
+#assertAlmostEqual(a, b) round(a-b, 7) == 0
+#assertNotAlmostEqual(a, b) round(a-b, 7) != 0
+#assertGreater(a, b) a > b
+#assertGreaterEqual(a, b) a >= b
+#assertLess(a, b) a < b
+#assertLessEqual(a, b) a <= b
+#assertRegexpMatches(s, r) r.search(s)
+#assertNotRegexpMatches(s, r) not r.search(s)
+#assertItemsEqual(a, b) sorted(a) == sorted(b)
+#assertDictContainsSubset(a, b) all the key/value pairs in a exist in b
+#assertMultiLineEqual(a, b) Strings
+#assertSequenceEqual(a, b) Sequences
+#assertListEqual(a, b) Lists
+#assertTupleEqual(a, b) Tuples
+#assertSetEqual(a, b) sets or frozensets
+#assertDictEqual(a, b) Dicts
---
+++ 15_Tests/04_Python_Unit_Tests/Unittest_03_Simple_Examples/Sample/test_1.py
@@ -0,0 +1,10 @@
+import unittest
+import sample_file_to_be_checked as test_me
+
+class TestClass09(unittest.TestCase):
+    def test_case01(self):
+        self.assertEqual(test_me.add(2, 3), 5)
+        print("\nIn test_case01()")
+    def test_case02(self):
+        self.assertEqual(test_me.mul(2, 3), 6)
+        print("\nIn test_case02()")
