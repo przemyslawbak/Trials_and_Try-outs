5e0d4eab09d596799b997a348371263138b756ee
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-02-07 09:21:24 +0100

    Working on TE graph data parser;

--- 25_Other/Graphics_01_GraphFinding/ImageToText/Program.cs
+++ 25_Other/Graphics_01_GraphFinding/ImageToText/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
 
 namespace ImageToText
 {
@@ -15,27 +12,34 @@ namespace ImageToText
         {
             string path = Path.Combine("chart.png");
 
-            var pixels = GetPixels(path);
+            var curvePixels = GetPixels(path, 70, 130, 180);
+            var gridPixels = GetPixels(path, 211, 211, 211);
+            var numbersPixels = GetPixels(path, 0, 0, 0);
+            //todo: get number values
+            //todo: numer pixels
+            //todo: parse grid pixels with number values
+            //todo: 
         }
 
-        private static List<PixelData> GetPixels(string path)
+        private static List<PixelData> GetPixels(string path, int r, int g, int b)
         {
-            Bitmap b = new Bitmap(path);
+            Bitmap bmp = new Bitmap(path);
 
-            List<PixelData> colorList = new List<PixelData>
+            List<PixelData> curvePixels = new List<PixelData>();
+
+            for (int y = 0; y < bmp.Height; y++)
             {
-
-            };
-
-            for (int y = 0; y < b.Height; y++)
-            {
-                for (int x = 0; x < b.Width; x++)
+                for (int x = 0; x < bmp.Width; x++)
                 {
-                    colorList.Add(new PixelData() { Color = b.GetPixel(x, y), X = x, Y = y });
+                    var pixel = bmp.GetPixel(x, y);
+                    if (pixel.R == r && pixel.G == g && pixel.B == b)
+                    {
+                        curvePixels.Add(new PixelData() { Color = pixel, X = x, Y = y }); 
+                    }
                 }
             }
 
-            return colorList;
+            return curvePixels;
         }
     }
 }
