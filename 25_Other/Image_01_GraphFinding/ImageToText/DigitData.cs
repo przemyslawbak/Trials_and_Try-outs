5fd65cecc88350b5b0711243278f2ce0ac769cfc
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-02-09 19:38:43 +0100

    Several methods added;

---
+++ 25_Other/Image_01_GraphFinding/ImageToText/DigitData.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageToText
+{
+    public class DigitData
+    {
+        public int Digit { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
--- 25_Other/Image_01_GraphFinding/ImageToText/Program.cs
+++ 25_Other/Image_01_GraphFinding/ImageToText/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using AForge.Imaging;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
@@ -32,19 +35,101 @@ namespace ImageToText
             var minXlabelLine = _gridPixels.Where(y => y.Y == maxYgraph + 2).Select(y => y.X).Min();
             var oYlabelsXmin = maxXgraph + 1;
             var oYlabelsXmax = bmp.Width;
-            var oXlabelsYmin = 0;
             var oXlabelsYmax = minYgraph;
-            var oYlabelHeight = 20;
-            var oXlabelHeight = 20;
-            var oXlabelWidth = 60;
+            var oYlabelHeight = 24;
+            var oXlabelHeight = 24;
+            var oXlabelWidth = 80;
 
-            var oYmaxValue = 0;
-            var oYminValue = 0;
-            var oXmaxValue = 0;
-            var oXminValue = 0;
+            var oYmaxRect = new RectangleF(oYlabelsXmin, maxYlabelLine + oYlabelHeight/2, oYlabelsXmax - oYlabelsXmin, oYlabelHeight);
+            var oYminRect = new RectangleF(oYlabelsXmin, minYlabelLine + oYlabelHeight / 2, oYlabelsXmax - oYlabelsXmin, oYlabelHeight);
+            var oXmaxRect = new RectangleF(maxXlabelLine - oXlabelWidth/2, oXlabelsYmax, oXlabelWidth, oXlabelHeight);
+            var oXminRect = new RectangleF(maxXlabelLine - oXlabelWidth / 2, oXlabelsYmax, oXlabelWidth, oXlabelHeight);
 
-            //todo: detecting imgaes (digits) in images (numbers)
-            //todo: assign values to Y ranges
+            var oYmaxValueImage = CropImage(bmp, oYmaxRect);
+            var oYminValueImage = CropImage(bmp, oYminRect);
+            var oXmaxValueImage = CropImage(bmp, oXmaxRect);
+            var oXminValueImage = CropImage(bmp, oXminRect);
+
+            var templatesDict = new Dictionary<int, Bitmap>
+            {
+                { 0, ConvertToFormat((Bitmap)Bitmap.FromFile("0.png"), PixelFormat.Format24bppRgb) },
+                { 1, ConvertToFormat((Bitmap)Bitmap.FromFile("1.png"), PixelFormat.Format24bppRgb) },
+                { 2, ConvertToFormat((Bitmap)Bitmap.FromFile("2.png"), PixelFormat.Format24bppRgb) },
+                { 3, ConvertToFormat((Bitmap)Bitmap.FromFile("3.png"), PixelFormat.Format24bppRgb) },
+                { 4, ConvertToFormat((Bitmap)Bitmap.FromFile("4.png"), PixelFormat.Format24bppRgb) },
+                { 5, ConvertToFormat((Bitmap)Bitmap.FromFile("5.png"), PixelFormat.Format24bppRgb) },
+                { 6, ConvertToFormat((Bitmap)Bitmap.FromFile("6.png"), PixelFormat.Format24bppRgb) },
+                { 7, ConvertToFormat((Bitmap)Bitmap.FromFile("7.png"), PixelFormat.Format24bppRgb) },
+                { 8, ConvertToFormat((Bitmap)Bitmap.FromFile("8.png"), PixelFormat.Format24bppRgb) },
+                { 9, ConvertToFormat((Bitmap)Bitmap.FromFile("9.png"), PixelFormat.Format24bppRgb) },
+            };
+
+            var oYmaxValue = GetValueFromImage(templatesDict, oYmaxValueImage);
+            var oYminValue = GetValueFromImage(templatesDict, oYminValueImage);
+            var oXmaxValue = GetValueFromImage(templatesDict, oXmaxValueImage);
+            var oXminValue = GetValueFromImage(templatesDict, oXminValueImage);
+        }
+
+        public static Bitmap ConvertToFormat(Bitmap image, PixelFormat format)
+        {
+            Bitmap copy = new Bitmap(image.Width, image.Height, format);
+            using (Graphics gr = Graphics.FromImage(copy))
+            {
+                gr.DrawImage(image, new Rectangle(0, 0, copy.Width, copy.Height));
+            }
+            return copy;
+        }
+
+        private static int GetValueFromImage(Dictionary<int, Bitmap> templatesDict, Bitmap valueImage)
+        {
+            var sourceImage = ConvertToFormat(valueImage, PixelFormat.Format24bppRgb);
+
+            // create template matching algorithm's instance
+            // (set similarity threshold to 98%)
+
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0.95f);
+            // find all matchings with specified above similarity
+
+            List<DigitData> digits = new List<DigitData>();
+
+            foreach (KeyValuePair<int, Bitmap> template in templatesDict)
+            {
+                System.Console.WriteLine("processing: " + template.Key);
+                TemplateMatch[] matchings = tm.ProcessImage(sourceImage, template.Value);
+                // highlight found matchings
+
+                BitmapData data = sourceImage.LockBits(
+                     new Rectangle(0, 0, sourceImage.Width, sourceImage.Height),
+                     ImageLockMode.ReadWrite, sourceImage.PixelFormat);
+
+
+                foreach (TemplateMatch m in matchings)
+                {
+                    Drawing.Rectangle(data, m.Rectangle, Color.White);
+
+                    digits.Add(new DigitData() { X = m.Rectangle.Location.X, Y = m.Rectangle.Location.Y, Digit = template.Key });
+
+                    //MessageBox.Show(m.Rectangle.Location.ToString());
+                    System.Console.WriteLine(m.Rectangle.Location.ToString());
+                    // do something else with matching
+                }
+                sourceImage.UnlockBits(data);
+            }
+
+            digits = digits.OrderBy(x => x.X).ToList();
+            string number = "";
+
+            foreach (var digit in digits)
+            {
+                number = number + digit.Digit;
+            }
+
+            return int.Parse(number);
+        }
+
+        private static Bitmap CropImage(Bitmap bmpImage, RectangleF cropArea)
+        {
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
         }
 
         private static void GetPixels(Bitmap bmp)
