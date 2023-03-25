8ff08a72076863ab4ef05f7a65a021b6e0bb27b6
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-03-25 08:40:56 +0100

    New sample project added;

---
+++ 12_String/String_25_GivenNamesExractor/List_Comparer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        private static List<string> _givenNames = new List<string>();
+
+        static void Main(string[] args)
+        {
+            foreach (string file in Directory.EnumerateFiles(Path.Combine("data"), "*.*"))
+            {
+                Console.WriteLine("Reading " + file);
+                List<string> contents = File.ReadAllLines(file).ToList();
+                List<string> names = contents.Where(c => c.Contains(",")).Select(i => i.Split(',')[0]).ToList();
+                _givenNames.AddRange(names);
+            }
+
+            File.WriteAllLines("output2.txt", _givenNames.Distinct());
+            Console.WriteLine("saved");
+            Console.ReadKey();
+
+        }
+    }
+}
