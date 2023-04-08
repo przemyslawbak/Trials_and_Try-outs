fb11e0e7ab5d17a1eadb92ecf21fde55d6782ac2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-04-08 13:59:21 +0200

    New Volatility sample added;

---
+++ 32_Python_Financial/Trial_28_Volatility/Sample/Sample.py
@@ -0,0 +1,22 @@
+## Computing Volatility
+# Load the required modules and packages
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+#https://www.w3schools.com/python/pandas/pandas_plotting.asp
+
+NIFTY = yf.download('^GDAXI',start='2019-6-1', end='2023-6-1')
+
+# Compute the logarithmic returns using the Closing price
+NIFTY['Log_Ret'] = np.log(NIFTY['Close'] / NIFTY['Close'].shift(1))
+
+# Compute Volatility using the pandas rolling standard deviation function
+NIFTY['Volatility'] = NIFTY['Log_Ret'].rolling(window=15).std() * np.sqrt(252)
+print(NIFTY.tail(15))
+
+# Plot the NIFTY Price series and the Volatility
+NIFTY[['Close', 'Volatility']].plot(subplots=True, color='blue',figsize=(8, 6))
+plt.show()
