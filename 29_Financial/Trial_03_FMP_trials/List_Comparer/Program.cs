45fc0af510711cba7a193c193a6ddcd682a387f9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-04-11 08:30:20 +0200

    Stock Ticker search lookup API sample;

--- 29_Financial/Trial_03_FMP_trials/List_Comparer/Program.cs
+++ 29_Financial/Trial_03_FMP_trials/List_Comparer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace List_Comparer
 {
+    //docs: https://site.financialmodelingprep.com/developer/docs/
     class Program
     {
         private static Hidden _hiddenService;
@@ -15,6 +17,24 @@ namespace List_Comparer
         private static async Task DoSomething()
         {
             string k = _hiddenService.GetK();
+
+            //Stock Ticker search lookup API
+            using (var httpClient = new HttpClient())
+
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/search?query=KGH&limit=100&apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(d);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
