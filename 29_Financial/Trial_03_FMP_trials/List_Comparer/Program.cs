060130740ff44e9ac16345c00640aa53d6f55005
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-04-11 10:54:03 +0200

    Testing various API endpoints;

--- 29_Financial/Trial_03_FMP_trials/List_Comparer/Program.cs
+++ 29_Financial/Trial_03_FMP_trials/List_Comparer/Program.cs
@@ -20,9 +20,89 @@ namespace List_Comparer
 
             //Stock Ticker search lookup API
             using (var httpClient = new HttpClient())
-
             {
-                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/search?query=KGH&limit=100&apikey=" + k))
+                string symbol = "AAPL";
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/search?query=" + symbol + "&limit=100&apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(d);
+                }
+            }
+
+            //Stock Historical Price
+            using (var httpClient = new HttpClient())
+            {
+                string symbol = "AAPL";
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/historical-chart/1min/" + symbol + "?apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(d);
+                }
+            }
+
+            //Index Historical Price
+            using (var httpClient = new HttpClient())
+            {
+                //SP500
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/historical-chart/1min/%5EGSPC?apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(d);
+                }
+            }
+
+            //Market Index
+            using (var httpClient = new HttpClient())
+            {
+                //SP500
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/quote/%5EGSPC?apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    //onsole.WriteLine(d);
+                }
+            }
+
+            //SP500 companies
+            using (var httpClient = new HttpClient())
+            {
+                //SP500, no weight :(
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/sp500_constituent?apikey=" + k))
+
+                {
+                    request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    var d = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(d);
+                }
+            }
+
+            //Historical SP500 components
+            using (var httpClient = new HttpClient())
+            {
+                //SP500
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://financialmodelingprep.com/api/v3/historical/sp500_constituent?apikey=" + k))
 
                 {
                     request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
