c5f9d44d3e6434a6170cb635f163a88f359382b1
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-04-11 08:03:36 +0200

    New sample project added;

---
+++ 29_Financial/Trial_03_FMP_trials/List_Comparer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        private static Hidden _hiddenService;
+        static void Main(string[] args)
+        {
+            _hiddenService = new Hidden();
+            DoSomething().Wait();
+        }
+
+        private static async Task DoSomething()
+        {
+            string k = _hiddenService.GetK();
+        }
+    }
+}
