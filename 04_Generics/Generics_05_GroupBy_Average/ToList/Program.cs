ae223184667ce63c6e9f2af0424e87836e3bc810
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-05-02 22:49:13 +0200

    Extended sample with parsing;

--- 04_Generics/Generics_05_GroupBy_Average/ToList/Program.cs
+++ 04_Generics/Generics_05_GroupBy_Average/ToList/Program.cs
@@ -8,17 +8,20 @@ namespace ToList
     {
         static void Main(string[] args)
         {
+            decimal d;
+
             var sampleList = new List<SampleModel>()
             {
                 new SampleModel() { SampleName = "name1", SampleValue = 0.5M },
                 new SampleModel() { SampleName = "name2", SampleValue = 0.8M },
                 new SampleModel() { SampleName = "name3", SampleValue = 0.3M },
                 new SampleModel() { SampleName = "name1", SampleValue = 1.5M },
+                new SampleModel() { SampleName = "name1", SampleValue = "dupa" },
             };
 
             var res = sampleList.GroupBy(x => x.SampleName).Select(x => new DataModel()
             {
-                ResultValue = x.Average(y => (decimal)y.SampleValue),
+                ResultValue = x.Where(y => decimal.TryParse(y.SampleValue.ToString(), out d)).Average(y => (decimal)y.SampleValue),
                 ResultName = x.Select(y => y.SampleName + "_res").First(),
             }).ToList();
 
