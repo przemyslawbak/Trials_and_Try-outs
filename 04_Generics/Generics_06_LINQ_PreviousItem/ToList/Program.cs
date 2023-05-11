4562c78690f12f16024f08ddfb28a434eeba0156
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-05-11 18:41:10 +0200

    Added sample where LINQ uses previous items from the list;

---
+++ 04_Generics/Generics_06_LINQ_PreviousItem/ToList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToList
+{
+    class Program
+    {
+        //https://stackoverflow.com/a/4460167
+        static void Main(string[] args)
+        {
+            List<int> A = new List<int>() { 1,2,3,4,5,5,6,7,8,8,9,9,0 };
+
+            var m = Enumerable.Range(1, A.Count - 1)
+                  .Select(i => A[i] == A[i - 1])
+                  .Where(x => x == true)
+                  .Count();
+
+            Console.ReadKey();
+        }
+    }
+}
