78ed51717c792887eb67a56ec9fb0d61f63a7f11
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-05-19 00:28:33 +0200

    OhlcvResult model update;

--- 29_Financial/Trial_07_Peaks_Spike/List_Comparer/OhlcvResult.cs
+++ 29_Financial/Trial_07_Peaks_Spike/List_Comparer/OhlcvResult.cs
@@ -7,5 +7,6 @@
         public decimal Hammers { get; set; }
         public decimal Patterns { get; set; }
         public decimal Momentum { get; set; }
+        public decimal Strength { get; set; }
     }
 }
