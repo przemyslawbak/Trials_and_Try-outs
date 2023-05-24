25db74dfa8db64f32998cb7f3393b3d491cd728b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-05-25 00:01:07 +0200

    One model added, one removed;

---
+++ 29_Financial/Trial_07_Peaks_Spike/List_Comparer/VariablesModel.cs
@@ -0,0 +1,23 @@
+﻿namespace List_Comparer
+{
+    public class VariablesModel
+    {
+        public decimal _minCandleSize { get; set; }
+        public decimal _maxShortCandleSize { get; set; }
+        public decimal _minCandleDifference { get; set; }
+        public decimal _maxDojiShadowSizes { get; set; }
+        public decimal _maxDojiBodySize { get; set; }
+        public decimal _minCandleShadowSize { get; set; }
+        public decimal _maxShadowSize { get; set; }
+        public decimal _maxPriceDifference { get; set; }
+        public decimal _maxCloseDifference { get; set; }
+        public decimal _maxCandleBodySize { get; set; }
+        public decimal _maxCandleShadowSize { get; set; }
+        public decimal _maxDifference { get; set; }
+        public decimal _maxShadowChange { get; set; }
+        public decimal _maxCloseHighChange { get; set; }
+        public decimal _maxCandleSize { get; set; }
+        public decimal _maxOpenDifference { get; set; }
+        public decimal _inBarMaxChange { get; set; }
+    }
+}
