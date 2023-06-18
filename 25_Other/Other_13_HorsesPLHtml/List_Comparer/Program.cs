ece441f489053fa77c25a5204be43c66eb81b769
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-06-18 09:53:47 +0200

    Mothers results bug fixed;

--- 25_Other/Other_13_HorsesPLHtml/List_Comparer/Program.cs
+++ 25_Other/Other_13_HorsesPLHtml/List_Comparer/Program.cs
@@ -144,7 +144,7 @@ namespace List_Comparer
             startyFatherArray.RemoveAt(0);
 
 
-            var startyMotherHtml = htmlFather.Split(new string[] { "<th style=\"text-align:right\" scope=\"col\">wygrana</th>" }, StringSplitOptions.None)[1];
+            var startyMotherHtml = htmlMother.Split(new string[] { "<th style=\"text-align:right\" scope=\"col\">wygrana</th>" }, StringSplitOptions.None)[1];
             var startyMotherArray = startyMotherHtml.Split(new string[] { "<td style=\"text-align:center\"> <span>" }, StringSplitOptions.None).ToList();
             startyMotherArray.RemoveAt(0);
 
@@ -339,140 +339,6 @@ namespace List_Comparer
                 }
             }
 
-
-
-
-
-
-
-
-/*
-            foreach (var row in sibling1Roes)
-            {
-                try
-                {
-                    int distance = int.Parse(row.Split(separator)[2].Split(' ')[0].Trim());
-                    decimal distanceScore = 1 + (decimal)Math.Abs(raceDistance - distance) / raceDistance * 10;
-                    int yearRace = DateTime.Parse(row.Split(separator)[0]).Year;
-                    decimal yearRaceScore = (1 + (yearNow - yearRace) * yearStep);
-                    int catValue = classValues[row.Split(separator)[3]];
-                    int place = int.Parse(row.Split(separator)[7].Split('/')[0].Trim());
-                    int qty = int.Parse(row.Split(separator)[7].Split('/')[1].Trim());
-                    decimal raceScore = (decimal)catValue * (decimal)place / (decimal)qty * distanceScore;
-                    if (raceScore > 0)
-                        resultsSibling1.Add(raceScore);
-                }
-                catch (Exception ex)
-                {
-                    //do nothing
-                }
-            }
-
-            foreach (var row in sibling2Roes)
-            {
-                try
-                {
-                    int distance = int.Parse(row.Split(separator)[2].Split(' ')[0].Trim());
-                    decimal distanceScore = 1 + (decimal)Math.Abs(raceDistance - distance) / raceDistance * 10;
-                    int yearRace = DateTime.Parse(row.Split(separator)[0]).Year;
-                    decimal yearRaceScore = (1 + (yearNow - yearRace) * yearStep);
-                    int catValue = classValues[row.Split(separator)[3]];
-                    int place = int.Parse(row.Split(separator)[7].Split('/')[0].Trim());
-                    int qty = int.Parse(row.Split(separator)[7].Split('/')[1].Trim());
-                    decimal raceScore = (decimal)catValue * (decimal)place / (decimal)qty * distanceScore;
-                    if (raceScore > 0)
-                        resultsSibling2.Add(raceScore);
-                }
-                catch (Exception ex)
-                {
-                    //do nothing
-                }
-            }
-
-            foreach (var row in sibling3Roes)
-            {
-                try
-                {
-                    int distance = int.Parse(row.Split(separator)[2].Split(' ')[0].Trim());
-                    decimal distanceScore = 1 + (decimal)Math.Abs(raceDistance - distance) / raceDistance * 10;
-                    int yearRace = DateTime.Parse(row.Split(separator)[0]).Year;
-                    decimal yearRaceScore = (1 + (yearNow - yearRace) * yearStep);
-                    int catValue = classValues[row.Split(separator)[3]];
-                    int place = int.Parse(row.Split(separator)[7].Split('/')[0].Trim());
-                    int qty = int.Parse(row.Split(separator)[7].Split('/')[1].Trim());
-                    decimal raceScore = (decimal)catValue * (decimal)place / (decimal)qty * distanceScore;
-                    if (raceScore > 0)
-                        resultsSibling3.Add(raceScore);
-                }
-                catch (Exception ex)
-                {
-                    //do nothing
-                }
-            }
-
-            foreach (var row in jockeyRows)
-            {
-                try
-                {
-                    string horseName = row.Split(separator)[0].Split(' ')[0];
-                    decimal nameMultiplier = horseName == horsieName ? 0.7M : 1.0M;
-
-                    if (decimal.Parse(row.Split(separator)[10]) > 0 && (int.Parse(row.Split(separator)[4]) > 0 || int.Parse(row.Split(separator)[5]) > 0 || int.Parse(row.Split(separator)[6]) > 0))
-                    {
-
-                        decimal times1 = 0M;
-                        decimal times2 = 0M;
-                        decimal times3 = 0M;
-                        if (int.Parse(row.Split(separator)[4]) > 0) times1 = 1 / decimal.Parse(row.Split(separator)[4]) * decimal.Parse(row.Split(separator)[10]) * 0.1M;
-                        if (int.Parse(row.Split(separator)[5]) > 0) times2 = 1 / decimal.Parse(row.Split(separator)[5]) * decimal.Parse(row.Split(separator)[10]) * 0.2M;
-                        if (int.Parse(row.Split(separator)[6]) > 0) times3 = 1 / decimal.Parse(row.Split(separator)[6]) * decimal.Parse(row.Split(separator)[10]) * 0.3M;
-                        var results = (times1 + times2 + times3) * nameMultiplier;
-
-                        decimal raceScore = nameMultiplier * results;
-                        resultsJockey.Add(raceScore);
-                    }
-                    else if (decimal.Parse(row.Split(separator)[10]) > 0)
-                    {
-                        resultsJockey.Add(1M * decimal.Parse(row.Split(separator)[10]));
-                    }
-                }
-                catch (Exception ex)
-                {
-                    //do nothing
-                }
-            }
-
-            foreach (var row in trenerRows)
-            {
-                try
-                {
-                    string horseName = row.Split(separator)[0].Split(' ')[0];
-                    decimal nameMultiplier = horseName == horsieName ? 0.7M : 1.0M;
-
-                    if (decimal.Parse(row.Split(separator)[10]) > 0 && (int.Parse(row.Split(separator)[4]) > 0 || int.Parse(row.Split(separator)[5]) > 0 || int.Parse(row.Split(separator)[6]) > 0))
-                    {
-
-                        decimal times1 = 0M;
-                        decimal times2 = 0M;
-                        decimal times3 = 0M;
-                        if (int.Parse(row.Split(separator)[4]) > 0) times1 = 1 / decimal.Parse(row.Split(separator)[4]) * decimal.Parse(row.Split(separator)[10]) * 0.1M;
-                        if (int.Parse(row.Split(separator)[5]) > 0) times2 = 1 / decimal.Parse(row.Split(separator)[5]) * decimal.Parse(row.Split(separator)[10]) * 0.2M;
-                        if (int.Parse(row.Split(separator)[6]) > 0) times3 = 1 / decimal.Parse(row.Split(separator)[6]) * decimal.Parse(row.Split(separator)[10]) * 0.3M;
-                        var results = (times1 + times2 + times3) * nameMultiplier;
-
-                        decimal raceScore = nameMultiplier * results;
-                        resultsTrener.Add(raceScore);
-                    }
-                    else if (decimal.Parse(row.Split(separator)[10]) > 0)
-                    {
-                        resultsTrener.Add(1M * decimal.Parse(row.Split(separator)[10]));
-                    }
-                }
-                catch (Exception ex)
-                {
-                    //do nothing
-                }
-            }*/
             var siblingsScore = siblingResults.Count > 0 ? (decimal)siblingResults.Average(x => x) : 1;
             var motherScore = resultsMother.Count > 0 ? (decimal)resultsMother.Average(x => x) : 1;
             var fatherScore = resultsFather.Count > 0 ? (decimal)resultsFather.Average(x => x) : 1;
