94b5207283322c58728e1becd8adb02a2d0335f0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-06-19 14:42:31 +0200

    Updated for clean shutdown;

--- 30_Python/Trials_04_PythonMultiCpuCore/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
+++ 30_Python/Trials_04_PythonMultiCpuCore/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
@@ -19,10 +19,40 @@
     
 #print('That took {} seconds'.format(time.time() - starttime))
 
+#import time
+#import multiprocessing 
+
+##multi CPU core (NOT clean shutdown)
+#def basic_func(x):
+#    if x == 0:
+#        return 'zero'
+#    elif x%2 == 0:
+#        return 'even'
+#    else:
+#        return 'odd'
+
+#def multiprocessing_func(x):
+#    y = x*x
+#    time.sleep(2)
+#    print('{} squared results in a/an {} number'.format(x, basic_func(y)))
+    
+#if __name__ == '__main__':
+#    starttime = time.time()
+#    processes = []
+#    for i in range(0,100000000000):
+#        p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
+#        processes.append(p)
+#        p.start()
+        
+#    for process in processes:
+#        process.join()
+        
+#    print('That took {} seconds'.format(time.time() - starttime))
+
 import time
 import multiprocessing 
 
-#multi CPU core
+##multi CPU core (clean shutdown)
 def basic_func(x):
     if x == 0:
         return 'zero'
@@ -37,14 +67,9 @@ def multiprocessing_func(x):
     print('{} squared results in a/an {} number'.format(x, basic_func(y)))
     
 if __name__ == '__main__':
+    
     starttime = time.time()
-    processes = []
-    for i in range(0,100000000000):
-        p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
-        processes.append(p)
-        p.start()
-        
-    for process in processes:
-        process.join()
-        
+    pool = multiprocessing.Pool()
+    pool.map(multiprocessing_func, range(0,10))
+    pool.close()
     print('That took {} seconds'.format(time.time() - starttime))
