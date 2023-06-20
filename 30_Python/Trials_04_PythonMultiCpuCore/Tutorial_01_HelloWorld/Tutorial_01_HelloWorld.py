62f642da5aea1e5898f528e7d81eb5cc3dfa3ccc
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-06-20 08:01:36 +0200

    Testing Python multiprocessing;

--- 30_Python/Trials_04_PythonMultiCpuCore/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
+++ 30_Python/Trials_04_PythonMultiCpuCore/Tutorial_01_HelloWorld/Tutorial_01_HelloWorld.py
@@ -12,47 +12,17 @@
 #        return 'odd'
     
 #starttime = time.time()
-#for i in range(0,100000000000):
+#for i in range(0,100):
 #    y = i*i
 #    time.sleep(2)
 #    print('{} squared results in a/an {} number'.format(i, basic_func(y)))
     
-#print('That took {} seconds'.format(time.time() - starttime))
-
-#import time
-#import multiprocessing 
-
-##multi CPU core (NOT clean shutdown)
-#def basic_func(x):
-#    if x == 0:
-#        return 'zero'
-#    elif x%2 == 0:
-#        return 'even'
-#    else:
-#        return 'odd'
-
-#def multiprocessing_func(x):
-#    y = x*x
-#    time.sleep(2)
-#    print('{} squared results in a/an {} number'.format(x, basic_func(y)))
-    
-#if __name__ == '__main__':
-#    starttime = time.time()
-#    processes = []
-#    for i in range(0,100000000000):
-#        p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
-#        processes.append(p)
-#        p.start()
-        
-#    for process in processes:
-#        process.join()
-        
-#    print('That took {} seconds'.format(time.time() - starttime))
+#print('That took {} seconds'.format(time.time() - starttime)) #201s
 
 import time
 import multiprocessing 
 
-##multi CPU core (clean shutdown)
+#multi CPU core (NOT clean shutdown)
 def basic_func(x):
     if x == 0:
         return 'zero'
@@ -67,9 +37,39 @@ def multiprocessing_func(x):
     print('{} squared results in a/an {} number'.format(x, basic_func(y)))
     
 if __name__ == '__main__':
-    
     starttime = time.time()
-    pool = multiprocessing.Pool()
-    pool.map(multiprocessing_func, range(0,10))
-    pool.close()
-    print('That took {} seconds'.format(time.time() - starttime))
+    processes = []
+    for i in range(0,100):
+        p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
+        processes.append(p)
+        p.start()
+        
+    for process in processes:
+        process.join()
+        
+    print('That took {} seconds'.format(time.time() - starttime)) #8s
+
+#import time
+#import multiprocessing 
+
+###multi CPU core (clean shutdown)
+#def basic_func(x):
+#    if x == 0:
+#        return 'zero'
+#    elif x%2 == 0:
+#        return 'even'
+#    else:
+#        return 'odd'
+
+#def multiprocessing_func(x):
+#    y = x*x
+#    time.sleep(2)
+#    print('{} squared results in a/an {} number'.format(x, basic_func(y)))
+    
+#if __name__ == '__main__':
+    
+#    starttime = time.time()
+#    pool = multiprocessing.Pool()
+#    pool.map(multiprocessing_func, range(0,100))
+#    pool.close()
+#    print('That took {} seconds'.format(time.time() - starttime)) #57s
