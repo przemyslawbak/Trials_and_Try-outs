7613afcce5eb267ca7c08edb29bf79d65cb92f15
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-07-04 14:19:43 +0200

    #if DEBUG sample added;

---
+++ 25_Other/Other_14_if_DEBUG/Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Activator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+            Console.WriteLine("THE BUG");
+            #else
+            Console.WriteLine("NOT THE BUG");
+            #endif
+        }
+    }
+}
+
+
+
