3bafa96ea6562c6629e969880728e61ce11de378
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-08-31 22:15:05 +0200

    Playing with data values and display;

--- 19_Wpf/Wpf_14_Chart_Custom/Financial_02_WpfTest/MainWindow.xaml.cs
+++ 19_Wpf/Wpf_14_Chart_Custom/Financial_02_WpfTest/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace Financial_02_WpfTest
@@ -14,41 +16,37 @@ namespace Financial_02_WpfTest
 
         private void AddCustomPiontsAndColor()
         {
-            Points.Add(new Point(1, 10));
-            Points.Add(new Point(2, 30));
-            Points.Add(new Point(3, 30));
-            Points.Add(new Point(4, 20));
-            Points.Add(new Point(5, 40));
-            Points.Add(new Point(6, 70));
-            Points.Add(new Point(7, 50));
-            Points.Add(new Point(8, 10));
-            Points.Add(new Point(9, 30));
-            Points.Add(new Point(10, 30));
-            Points.Add(new Point(11, 20));
-            Points.Add(new Point(12, 40));
-            Points.Add(new Point(13, 70));
-            Points.Add(new Point(14, 50));
-            Points.Add(new Point(15, 10));
-            Points.Add(new Point(16, 30));
-            Points.Add(new Point(17, 30));
-            Points.Add(new Point(18, 20));
-            Points.Add(new Point(19, 40));
-            Points.Add(new Point(20, 70));
-            Points.Add(new Point(21, 50));
-            Points.Add(new Point(22, 10));
-            Points.Add(new Point(23, 30));
-            Points.Add(new Point(24, 30));
-            Points.Add(new Point(25, 20));
-            Points.Add(new Point(26, 40));
-            Points.Add(new Point(27, 70));
-            Points.Add(new Point(28, 50));
-            Points.Add(new Point(29, 10));
-            Points.Add(new Point(30, 30));
-            Points.Add(new Point(31, 30));
-            Points.Add(new Point(32, 20));
-            Points.Add(new Point(33, 40));
-            Points.Add(new Point(34, 70));
-            Points.Add(new Point(35, 50));
+            var data = new List<SomeDataModel>()
+            {
+                new SomeDataModel() { Value = 3584, Signal = 0 },
+                new SomeDataModel() { Value = 3586, Signal = 0 },
+                new SomeDataModel() { Value = 3534, Signal = 0 },
+                new SomeDataModel() { Value = 3589, Signal = 0 },
+                new SomeDataModel() { Value = 3612, Signal = 0 },
+                new SomeDataModel() { Value = 3634, Signal = 1 },
+                new SomeDataModel() { Value = 3639, Signal = 0 },
+                new SomeDataModel() { Value = 3636, Signal = 0 },
+                new SomeDataModel() { Value = 3628, Signal = 0 },
+                new SomeDataModel() { Value = 3621, Signal = 0 },
+                new SomeDataModel() { Value = 3611, Signal = 0 },
+                new SomeDataModel() { Value = 3592, Signal = -1 },
+                new SomeDataModel() { Value = 3591, Signal = 0 },
+                new SomeDataModel() { Value = 3582, Signal = 0 },
+                new SomeDataModel() { Value = 3543, Signal = 0 },
+                new SomeDataModel() { Value = 3534, Signal = 0 },
+                new SomeDataModel() { Value = 3512, Signal = 0 },
+                new SomeDataModel() { Value = 3485, Signal = 0 },
+            };
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                //todo: how to get actual window height and width?
+                //todo: fix lines row height
+                //todo: process Value, depending on window height
+                //todo: process X depending on window width
+
+                Points.Add(new Point(i, data[i].Value));
+            }
 
             ColorName = "Red";
         }
