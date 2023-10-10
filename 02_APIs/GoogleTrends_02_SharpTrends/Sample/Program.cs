aaa0102802aac0111dae161d7391ca70f79cec64
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-10-10 11:41:18 +0200

    Google Trends sample added;

---
+++ 02_APIs/GoogleTrends_02_SharpTrends/Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using SharpTrends.DailyTrends;
+
+var client = new SharpTrends.Client();
+
+List<TrendingSearch> data = client.DailyTrends();
+
+foreach (var i in data) //unable to search own :(
+{
+    Console.WriteLine($"{i.Title}:" +
+        $"\n\tDate:\t{i.Date}" +
+        $"\n\tTraffic:\t{i.Traffic:E2}" +
+        $"\n\tRelated:\t{String.Join(", ", i.RelatedQueries)}" +
+        $"\n\tArticles:\t{i.Articles.Count}");
+}
