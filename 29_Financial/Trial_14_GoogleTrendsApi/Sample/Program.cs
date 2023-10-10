03e18b78f3eab76fed90846c9f5c4fdf8fa3b7c0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-10-10 19:01:39 +0200

    New sample added;

---
+++ 29_Financial/Trial_14_GoogleTrendsApi/Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using GoogleTrendsApi;
+using Newtonsoft.Json.Linq;
+using Sample;
+using System.Text.Json.Nodes;
+
+HelperService _service = new HelperService();
+
+var xxx = await Api.GetInterestOverTime(new[] { "aapl" }, "", DateOptions.LastWeek, GroupOptions.All, 0); //cool
+
+if (xxx != null)
+{
+    var arr = (JsonArray)xxx;
+    var lastDayQty = arr.Count / 7;
+    var collection = new List<int>();
+
+    foreach (var x in arr)
+    {
+        var parsed = JObject.Parse(x.ToString());
+        var yyy = (int)parsed.SelectToken("value")[0];
+        collection.Add(yyy);
+    }
+
+    var lastDayCollection = collection.Skip(Math.Max(0, collection.Count() - lastDayQty));
+    var aver = lastDayCollection.Average();
+
+    Console.WriteLine("Aver from last day: " + aver);
+}
+
+Console.WriteLine();
