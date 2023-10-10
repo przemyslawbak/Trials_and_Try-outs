667393671520fed32f2066bb015ed99baf778c28
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-10-10 11:48:05 +0200

    Google Trends sample added;

---
+++ 02_APIs/GoogleTrends_03_trendsdotnet/Sample/Program.cs
@@ -0,0 +1,7 @@
+﻿using Trendsdotnet;
+
+using (TrendsClient client = new TrendsClient())
+{
+    string[] terms = new string[] { "aapl", "kghm" };
+    var res = await client.GetInterestOverTime(terms, DateTime.Now.AddDays(-7), DateTime.Now, Resolution.WEEK); //exception
+}
