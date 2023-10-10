86bd21c3e9de601059e70eed84ba68a7d328d486
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-10-10 11:36:11 +0200

    Google Trends sample added;

---
+++ 02_APIs/GoogleTrends_01_GoogleTrendsDotNet/Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿using GoogleTrends;
+using GoogleTrends.Models.Explore;
+using GoogleTrends.Models.ParameterTypes;
+
+var trendsClient = new GoogleTrendsClient();
+
+ExploreResponse exploreData = await trendsClient.Explore.ExploreQuery("aapl", SearchType.WebSearch,
+            queryTime: QueryTimes.PastWeek, geoSearch: GeoId.WorldWide); //exception
+
+Console.WriteLine("done");
