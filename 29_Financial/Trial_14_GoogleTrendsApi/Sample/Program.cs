96e65635f030d6984de6ad58497fedcbfc5e5d34
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-10-12 08:59:38 +0200

    Google trends sample updated;

--- 29_Financial/Trial_14_GoogleTrendsApi/Sample/Program.cs
+++ 29_Financial/Trial_14_GoogleTrendsApi/Sample/Program.cs
@@ -5,25 +5,38 @@ using System.Text.Json.Nodes;
 
 HelperService _service = new HelperService();
 
-var xxx = await Api.GetInterestOverTime(new[] { "aapl" }, "", DateOptions.LastWeek, GroupOptions.All, 0); //cool
 
-if (xxx != null)
+var components = _service.GetIndexComponents();
+
+foreach (var component in components)
 {
-    var arr = (JsonArray)xxx;
-    var lastDayQty = arr.Count / 7;
-    var collection = new List<int>();
-
-    foreach (var x in arr)
+    try
     {
-        var parsed = JObject.Parse(x.ToString());
-        var yyy = (int)parsed.SelectToken("value")[0];
-        collection.Add(yyy);
+        var trending = await Api.GetInterestOverTime(new[] { component.Key }, "", DateOptions.LastWeek, GroupOptions.All, 1163);
+
+        if (trending != null)
+        {
+            var arr = (JsonArray)trending;
+            var lastDayQty = arr.Count / 7;
+            var collection = new List<int>();
+
+            foreach (var x in arr)
+            {
+                var parsed = JObject.Parse(x.ToString());
+                var yyy = (int)parsed.SelectToken("value")[0];
+                collection.Add(yyy);
+            }
+
+            var lastDayCollection = collection.Skip(Math.Max(0, collection.Count() - lastDayQty));
+            var lastDayAverage = lastDayCollection.Average();
+
+            Console.WriteLine("Aver from last day for " + component.Key + ": " + lastDayAverage);
+        }
     }
-
-    var lastDayCollection = collection.Skip(Math.Max(0, collection.Count() - lastDayQty));
-    var aver = lastDayCollection.Average();
-
-    Console.WriteLine("Aver from last day: " + aver);
+    catch
+    {
+        Console.WriteLine("skipped");
+    }
 }
 
-Console.WriteLine();
+Console.WriteLine("finito");
