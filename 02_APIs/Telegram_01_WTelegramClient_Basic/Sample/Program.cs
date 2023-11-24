5cb9170ce2f2870766fc411bb28a1c8865befe52
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-11-24 11:13:38 +0100

    Telegram interactive logging example;

---
+++ 02_APIs/Telegram_01_WTelegramClient_Basic/Sample/Program.cs
@@ -0,0 +1,5 @@
+﻿using var client = new WTelegram.Client();
+var myself = await client.LoginUserIfNeeded();
+Console.WriteLine($"We are logged-in as {myself} (id {myself.id})");
+Console.WriteLine("Press any key...");
+Console.ReadKey();
