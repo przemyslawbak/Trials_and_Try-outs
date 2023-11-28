9d8423ba2f5ed7d603c258d3422d554e8efed918
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-11-28 20:02:15 +0100

    Show value on hover ScottPlot sample added;

---
+++ 29_Financial/Charting_04_ScottPlot_ShowValueUnderCursor/Sample/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using ScottPlot.Plottable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ScatterPlot MyScatterPlot;
+        private readonly MarkerPlot HighlightedPoint;
+        private int LastHighlightedIndex = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //https://scottplot.net/quickstart/wpf/
+            //https://scottplot.net/cookbook/
+
+            var dataSet1 = GetSomeData().Select(x => x.Value).ToArray();
+            var dataSet2 = GetSomeData().Select(x => x.Time.ToOADate()).ToArray();
+
+            MyScatterPlot = WpfPlot1.Plot.AddScatter(dataSet2, dataSet1);
+            WpfPlot1.Plot.Title("Testing ScottPlot with DateTime");
+            WpfPlot1.Plot.XAxis.DateTimeFormat(true);
+
+            HighlightedPoint = WpfPlot1.Plot.AddPoint(0, 0);
+            HighlightedPoint.Color = System.Drawing.Color.Red;
+            HighlightedPoint.MarkerSize = 10;
+            HighlightedPoint.MarkerShape = ScottPlot.MarkerShape.openCircle;
+            HighlightedPoint.IsVisible = false;
+
+            WpfPlot1.Refresh();
+        }
+
+        private void wpfPlot1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // determine point nearest the cursor
+            (double mouseCoordX, double mouseCoordY) = WpfPlot1.GetMouseCoordinates();
+            double xyRatio = WpfPlot1.Plot.XAxis.Dims.PxPerUnit / WpfPlot1.Plot.YAxis.Dims.PxPerUnit;
+            (double pointX, double pointY, int pointIndex) = MyScatterPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
+
+            // place the highlight over the point of interest
+            HighlightedPoint.X = pointX;
+            HighlightedPoint.Y = pointY;
+            HighlightedPoint.IsVisible = true;
+
+            // render if the highlighted point chnaged
+            if (LastHighlightedIndex != pointIndex)
+            {
+                LastHighlightedIndex = pointIndex;
+                WpfPlot1.Refresh();
+            }
+
+            // update the GUI to describe the highlighted point
+            double mouseX = e.GetPosition(this).X;
+            double mouseY = e.GetPosition(this).Y;
+            label1.Content = $"Closest point to ({mouseX:N2}, {mouseY:N2}) " +
+                $"is index {pointIndex} ({DateTime.FromOADate(pointX).ToShortTimeString()}, {pointY:N2})";
+        }
+
+        private List<SomeDataModel> GetSomeData()
+        {
+            var xxx =  new List<SomeDataModel>()
+            {
+                new SomeDataModel() { Value = 3584, Time = DateTime.Now.AddMinutes(-20) },
+                new SomeDataModel() { Value = 3586, Time = DateTime.Now.AddMinutes(-19) },
+                new SomeDataModel() { Value = 3534, Time = DateTime.Now.AddMinutes(-18) },
+                new SomeDataModel() { Value = 3589, Time = DateTime.Now.AddMinutes(-17) },
+                new SomeDataModel() { Value = 3612, Time = DateTime.Now.AddMinutes(-16) },
+                new SomeDataModel() { Value = 3634, Time = DateTime.Now.AddMinutes(-15) },
+                new SomeDataModel() { Value = 3639, Time = DateTime.Now.AddMinutes(-14) },
+                new SomeDataModel() { Value = 3636, Time = DateTime.Now.AddMinutes(-13) },
+                new SomeDataModel() { Value = 3628, Time = DateTime.Now.AddMinutes(-12) },
+                new SomeDataModel() { Value = 3621, Time = DateTime.Now.AddMinutes(-11) },
+                new SomeDataModel() { Value = 3611, Time = DateTime.Now.AddMinutes(-10) },
+                new SomeDataModel() { Value = 3592, Time = DateTime.Now.AddMinutes(-9) },
+                new SomeDataModel() { Value = 3591, Time = DateTime.Now.AddMinutes(-8) },
+                new SomeDataModel() { Value = 3582, Time = DateTime.Now.AddMinutes(-7) },
+                new SomeDataModel() { Value = 3543, Time = DateTime.Now.AddMinutes(-6) },
+                new SomeDataModel() { Value = 3534, Time = DateTime.Now.AddMinutes(-5) },
+                new SomeDataModel() { Value = 3512, Time = DateTime.Now.AddMinutes(-4) },
+                new SomeDataModel() { Value = 3485, Time = DateTime.Now.AddMinutes(-3) },
+            };
+
+            return xxx;
+        }
+    }
+}
