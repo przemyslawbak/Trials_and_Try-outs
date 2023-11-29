34585d42433db40f75233d1afd71abebf392749d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-11-29 11:04:01 +0100

    Workin on signal plot with DateTme Y axis and crosshair, using ScottPlot;

---
+++ 29_Financial/Charting_07_ScottPlot_MouseTracker_DateTimeXAxis/Sample/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using ScottPlot;
+using ScottPlot.Drawing.Colormaps;
+using ScottPlot.Plottable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Crosshair Crosshair;
+
+        public MainWindow() //https://scottplot.net/faq/datetime/
+        {
+            InitializeComponent();
+            var dataSetY = GetSomeData().Select(x => x.Value).ToArray();
+            var dataSetX = GetSomeData().Select(x => x.Time.ToOADate()).ToArray();
+            double samplesPerDay = (24 * 60); // one sample per minute
+
+            wpfPlot1.Plot.AddSignal(dataSetY, sampleRate: samplesPerDay);
+            wpfPlot1.Plot.XAxis.DateTimeFormat(true);
+            Crosshair = wpfPlot1.Plot.AddCrosshair(0, dataSetY[0]);
+            wpfPlot1.Refresh();
+        }
+        private List<SomeDataModel> GetSomeData()
+        {
+            var xxx = new List<SomeDataModel>()
+            {
+                new SomeDataModel() { Value = 3584, Time = DateTime.Now.AddMinutes(-20) },
+                new SomeDataModel() { Value = 3586, Time = DateTime.Now.AddMinutes(-19) },
+                new SomeDataModel() { Value = 3534, Time = DateTime.Now.AddMinutes(-18) },
+                new SomeDataModel() { Value = 3589, Time = DateTime.Now.AddMinutes(-17) },
+                new SomeDataModel() { Value = 3612, Time = DateTime.Now.AddMinutes(-16) },
+                new SomeDataModel() { Value = 3634, Time = DateTime.Now.AddMinutes(-15) },
+                new SomeDataModel() { Value = 3639, Time = DateTime.Now.AddMinutes(-14) },
+                new SomeDataModel() { Value = 3636, Time = DateTime.Now.AddMinutes(-13) },
+                new SomeDataModel() { Value = 3628, Time = DateTime.Now.AddMinutes(-12) },
+                new SomeDataModel() { Value = 3621, Time = DateTime.Now.AddMinutes(-11) },
+                new SomeDataModel() { Value = 3611, Time = DateTime.Now.AddMinutes(-10) },
+                new SomeDataModel() { Value = 3592, Time = DateTime.Now.AddMinutes(-9) },
+                new SomeDataModel() { Value = 3591, Time = DateTime.Now.AddMinutes(-8) },
+                new SomeDataModel() { Value = 3582, Time = DateTime.Now.AddMinutes(-7) },
+                new SomeDataModel() { Value = 3543, Time = DateTime.Now.AddMinutes(-6) },
+                new SomeDataModel() { Value = 3534, Time = DateTime.Now.AddMinutes(-5) },
+                new SomeDataModel() { Value = 3512, Time = DateTime.Now.AddMinutes(-4) },
+                new SomeDataModel() { Value = 3485, Time = DateTime.Now.AddMinutes(-3) },
+            };
+
+            return xxx;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            int pixelX = (int)e.MouseDevice.GetPosition(wpfPlot1).X;
+            int pixelY = (int)e.MouseDevice.GetPosition(wpfPlot1).Y;
+
+            (double coordinateX, double coordinateY) = wpfPlot1.GetMouseCoordinates();
+
+            XPixelLabel.Content = $"{pixelX:0.000}";
+            YPixelLabel.Content = $"{pixelY:0.000}";
+
+            XCoordinateLabel.Content = $"{wpfPlot1.Plot.GetCoordinateX(pixelX):0.00000000}";
+            YCoordinateLabel.Content = $"{wpfPlot1.Plot.GetCoordinateY(pixelY):0.00000000}";
+
+            Crosshair.X = coordinateX;
+            Crosshair.Y = coordinateY;
+
+            wpfPlot1.Refresh();
+        }
+
+        private void wpfPlot1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MouseTrackLabel.Content = "Mouse ENTERED the plot";
+            Crosshair.IsVisible = true;
+        }
+
+        private void wpfPlot1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MouseTrackLabel.Content = "Mouse LEFT the plot";
+            XPixelLabel.Content = "--";
+            YPixelLabel.Content = "--";
+            XCoordinateLabel.Content = "--";
+            YCoordinateLabel.Content = "--";
+
+            Crosshair.IsVisible = false;
+            wpfPlot1.Refresh();
+        }
+    }
+}
---
+++ 29_Financial/Charting_07_ScottPlot_MouseTracker_DateTimeXAxis/Sample/SomeDataModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sample
+{
+    internal class SomeDataModel
+    {
+        public double Value { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
