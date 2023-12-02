8044c3ed41fcff623cfad6af81acd860a5cc2b94
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-02 22:25:14 +0100

    Started populating data;

---
+++ 29_Financial/Charting_09_ScottPlot_MultipleCharts_AutoProps/Sample/DataResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sample
+{
+    internal class DataResultModel
+    {
+        public Guid DataGuid { get; set; }
+        public string IndexName { get; set; }
+        public string DataType { get; set; }
+        public decimal ResultValue { get; set; }
+        public DateTime UtcTimeStamp { get; set; }
+        public bool Release { get; set; }
+    }
+}
