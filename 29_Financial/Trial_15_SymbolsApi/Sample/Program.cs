ea9bbf5cd82afc377102bf18ce13815dfa240eb0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-03 01:15:33 +0100

    Data gathering sample added;

---
+++ 29_Financial/Trial_15_SymbolsApi/Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Sample;
+
+internal class Program
+{
+    private static HttpClient _client = new HttpClient();
+
+    private static async Task Main(string[] args)
+    {
+        HelperService _service = new HelperService();
+        UrlLocker _locker = new UrlLocker();
+
+        Console.WriteLine("Sending request to get JSON...");
+        HttpResponseMessage response = await _client.GetAsync(_locker.GetApiSymbolsUrl());
+        response.EnsureSuccessStatusCode();
+        var jsonString = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("Writing JSON to file...");
+        File.WriteAllText("_json.txt", jsonString);
+
+        Console.WriteLine("Deserializing JSON string...");
+        var symbols = JsonConvert.DeserializeObject<List<SymbolModel>>(jsonString);
+
+        Console.WriteLine("FINITO");
+    }
+}
+
