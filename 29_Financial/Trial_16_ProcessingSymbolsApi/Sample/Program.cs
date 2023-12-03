97f7d479609b60a76ad45bd6b82e7bd180021c02
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-03 01:43:32 +0100

    Sample TODOs updated;

--- 29_Financial/Trial_16_ProcessingSymbolsApi/Sample/Program.cs
+++ 29_Financial/Trial_16_ProcessingSymbolsApi/Sample/Program.cs
@@ -22,16 +22,27 @@ internal class Program
         var symbols = JsonConvert.DeserializeObject<List<SymbolModel>>(jsonString);
 
         //NOW:
-        //todo: get OHLCV for all symbols
+        //todo: get OHLCV for all symbols https://site.financialmodelingprep.com/developer/docs#chart-intraday
         //todo: detect trends (up, down, horizontal) https://www.investopedia.com/articles/active-trading/041814/four-most-commonlyused-indicators-trend-trading.asp
-        //todo: detect trend strenght (MACD, RSI, OBV)
+        //todo: detect trend strenght (MACD, RSI, OBV), multiple maybe
         //todo: add data about the duration of the trend?
+        //todo: add data about the volume of the trend?
         //todo: compute normal distribution velocity (5%, 20%, 50%)
-        //todo: add distribution data for each trend (average if appears more than one time)
+        //todo: add distribution data for each trend (average if appears more than one time, or save array)
         //todo: save result as a CSV
 
         //LATER:
-        //
+        //todo: possible to random data symbol / name (optional)
+        //todo: possible to fake made up data symbol / name (optional)
+        //todo: when faking include previous historic volacity for trend (optional)
+        //todo: when faking include random volacity data between max/min values (optional)
+        //todo: when faking include volumes (optional)
+        //todo: when faking pick up symbol
+        //todo: when faking pick up trend type
+        //todo: when faking pick up interval
+        //todo: when faking pick up how many entities
+        //todo: when faking pick up if want to average rends for the symbol, or get random one
+        //todo: when faking combine volumes with volacity
 
         Console.WriteLine("FINITO");
     }
