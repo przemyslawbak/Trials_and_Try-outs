63ddebe31465ee88ca928760154cf750b32f0ce3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-07 10:16:08 +0100

    String trial added;

---
+++ 12_String/String_28_Int_Spread/List_Comparer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = File.ReadAllText("1.txt");
+            var numbers = text.Split(',').Select(x => int.Parse(x)).ToList();
+            var diffs = numbers.Select((x, i) => i == 0 ? 0 : x - numbers[i - 1]).ToList();
+            var greaterDiffs = diffs.Where(x => x > 300).ToList();
+            Console.WriteLine("saved");
+            Console.ReadKey();
+
+        }
+    }
+}
