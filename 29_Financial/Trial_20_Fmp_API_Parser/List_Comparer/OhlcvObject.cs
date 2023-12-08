dff98f8d0d2716e42ac1afd8013e032b36345865
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-08 15:31:22 +0100

    Working on API data parsing;

---
+++ 29_Financial/Trial_20_Fmp_API_Parser/List_Comparer/OhlcvObject.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace List_Comparer
+{
+    internal class OhlcvObject
+    {
+        [JsonProperty(PropertyName = "date")]
+        public DateTime EstTimeStamp { get; set; }
+
+        [JsonProperty(PropertyName = "open")]
+        public decimal Open { get; set; }
+
+        [JsonProperty(PropertyName = "high")]
+        public decimal High { get; set; }
+
+        [JsonProperty(PropertyName = "low")]
+        public decimal Low { get; set; }
+
+        [JsonProperty(PropertyName = "close")]
+        public decimal Close { get; set; }
+
+        [JsonProperty(PropertyName = "volume")]
+        public int Volume { get; set; }
+        public decimal Capital { get; set; }
+        public string Symbol { get; set; }
+
+    }
+}
---
+++ 29_Financial/Trial_20_Fmp_API_Parser/List_Comparer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        private static HelperService _helper = new HelperService();
+        private static UrlVault _url = new UrlVault();
+        private static HttpClient _client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        private static async Task RunAsync()
+        {
+            _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/120.0");
+
+            string[] companySymbols = _helper.GetSymbols().Distinct().ToArray();
+
+            foreach (var symbol in companySymbols)
+            {
+                await GetAndSaveCompanyTicks(symbol);
+            }
+
+        }
+
+        private static async Task GetAndSaveCompanyTicks(string symbol)
+        {
+            int[] months = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] days = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
+            for (int i = months.Length - 1; i > 0; i--)
+            {
+                for (int j = days.Length - 1; j > 0; j--)
+                {
+                    var url = _url.GetFirstPart() + symbol + _url.SecondPart() + months[i].ToString("D2") + "-" + days[j].ToString("D2") + _url.ThirdPart() + months[i].ToString("D2") + "-" + days[j].ToString("D2") + _url.FourthPart();
+                    var response = await _client.GetAsync(url);
+                    var json = await response.Content.ReadAsStringAsync();
+                    if (!json.Contains("2023-12-07"))
+                    {
+                        var data = JsonConvert.DeserializeObject<List<OhlcvObject>>(json);
+                    }
+                }
+            }
+        }
+    }
+}
