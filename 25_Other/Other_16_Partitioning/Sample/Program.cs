4b8e490bbd209b643bdf1bf3377b99928d6f85e7
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-19 16:17:04 +0100

    Testing partitioner for parallel linq data;

---
+++ 25_Other/Other_16_Partitioning/Sample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Concurrent;
+
+namespace Activator
+{
+    class Program
+    {
+        //https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/custom-partitioners-for-plinq-and-tpl?redirectedfrom=MSDN
+        static void Main(string[] args)
+        {
+            // Static partitioning requires indexable source. Load balancing
+            // can use any IEnumerable.
+            var nums = Enumerable.Range(0, 5000).ToArray();
+
+            var watchSync = System.Diagnostics.Stopwatch.StartNew();
+
+            foreach (var i in nums)
+            {
+                var l = ProcessData(i);
+            }
+
+            watchSync.Stop();
+            var edMs = watchSync.ElapsedMilliseconds;
+            Console.WriteLine("sync (ms): " + edMs);
+
+            Console.ReadKey();
+
+            var watchParallel = System.Diagnostics.Stopwatch.StartNew();
+
+            // Create a load-balancing partitioner. Or specify false for static partitioning.
+            Partitioner<int> customPartitioner = Partitioner.Create(nums, true);
+
+            // The partitioner is the query's data source.
+            var q = customPartitioner.AsParallel().Select(x => x);
+
+            q.ForAll(x =>
+            {
+                var l = ProcessData(x);
+            });
+
+            watchParallel.Stop();
+            var elapsedMs = watchParallel.ElapsedMilliseconds;
+            Console.WriteLine("parallel (ms): " + elapsedMs);
+        }
+
+        private static long ProcessData(int n)
+        {
+            Console.WriteLine(n);
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+    }
+}
+
+
+
---
+++ 25_Other/Other_16_Partitioning/Sample/UrlLocker.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample
+{
+    internal class UrlLocker
+    {
+        public string GetUrl()
+        {
+            return "https://finapi.testuj-strone.com.pl/api/data/get-test-values/?rows=2000&key=CgOvRXEVTc9nqEONo9zSLimaqgDOSLKV";
+        }
+    }
+}
