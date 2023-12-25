2f29a87345ef435d41e251ce136fccf2424d338c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-12-25 13:21:05 +0100

    Pearson correlation sample added;

---
+++ 29_Financial/Correlation_01_Pearson/List_Comparer/Program.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Numerics.Statistics;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dataA = new double[] { 0.1, 0.2, 0.2, 0.5, 0.9, 0.8 };
+            double[] dataB = new double[] { 0.1, 0.2, 0.2, 0.5, 0.9, 0.8 };
+            double[] dataC = new double[] { 0.1, 0.2, 0.2, 0.4, 0.9, 0.8 };
+            double[] dataD = new double[] { 0.1, 0.0, 0.0, -0.3, -0.7, -0.6 };
+
+            var correlationPaersonB = Correlation.Pearson(dataA, dataB);
+            var correlationPaersonC = Correlation.Pearson(dataA, dataC);
+            var correlationPaersonD = Correlation.Pearson(dataA, dataD);
+        }
+    }
+}
