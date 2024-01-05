c0be67cafb4258a79856c1fb7265b8dcf619ecf6
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-01-05 11:43:13 +0100

    API call sample added;

---
+++ 02_APIs/Jobs_01_ApiCall/Sample/ConfigProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace Sample
+{
+    internal class ConfigProvider
+    {
+        internal string GetApiHash()
+        {
+            return "8456fb5b54bbf4888e780c7f0736b2f9";
+        }
+
+        internal int GetApiId()
+        {
+            return 22684378;
+        }
+
+        internal string GetSenderPhoneNumber()
+        {
+            return "+48793672867";
+        }
+
+        internal string GetTargetPhoneNumber()
+        {
+            return "++48725875135";
+        }
+
+        internal string GetUserName()
+        {
+            return "przemyslawpszemek";
+        }
+    }
+}
---
+++ 02_APIs/Jobs_01_ApiCall/Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Net;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var url = "https://jooble.org/api/";
+        var key = "xxx";
+
+        //create request object
+        WebRequest request = HttpWebRequest.Create(url + key);
+        //set http method
+        request.Method = "POST";
+        //set content type
+        request.ContentType = "application/json";
+        //create request writer
+        var writer = new StreamWriter(request.GetRequestStream());
+        //write request body
+        writer.Write("{ keywords: 'it', location: 'Washington'}");
+        //close writer
+        writer.Close();
+        //get response reader
+        var response = request.GetResponse();
+        var reader = new StreamReader(response.GetResponseStream());
+        //read response
+        while (!reader.EndOfStream)
+        {
+            Console.WriteLine(reader.ReadLine());
+        }
+    }
+}
