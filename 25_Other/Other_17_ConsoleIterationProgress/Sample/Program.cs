e5f12ac77379a1e89e538a2f8d4f75a0cbfe9c5f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-01-11 03:41:20 +0100

    Percentage of foreach loop in console application;

---
+++ 25_Other/Other_17_ConsoleIterationProgress/Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace Activator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var percentage = 0.00;
+            var progress = 0;
+            Console.WriteLine("Progress:");
+            for (int i = 0; i < 654; i++)
+            {
+                progress++;
+                Console.Write("\r{0}%", percentage);
+                Thread.Sleep(100);
+                percentage = Math.Round((double)progress / 654 * 100, 2);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Finito.");
+        }
+    }
+}
+
+
+
