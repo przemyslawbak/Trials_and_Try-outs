a2245ffbe6a2d88b75de6513d48d090ace6d5d1d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-01-19 11:01:06 +0100

    New sample added;

---
+++ 12_String/String_29_Combine2ndAnd3rdLine/List_Comparer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace List_Comparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 1;
+            string toWrite = "";
+            foreach (var line in File.ReadLines("1.txt"))
+            {
+                if (counter == 1) 
+                {
+                    toWrite = line;
+                }
+                if (counter == 2) 
+                {
+                    toWrite = toWrite + "|" + line;
+                }
+                if (counter == 3) 
+                {
+                    toWrite = toWrite + ", " + line + Environment.NewLine;
+                }
+
+                if (counter == 3)
+                {
+                    File.AppendAllText("output.txt", toWrite);
+                    counter = 1;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("DONE");
+            Console.ReadKey();
+        }
+    }
+}
