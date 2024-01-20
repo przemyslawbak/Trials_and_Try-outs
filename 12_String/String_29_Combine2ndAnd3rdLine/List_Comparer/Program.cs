bac1576f0cc656da941daa8d713f934bc67dbdf5
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-01-20 20:03:30 +0100

    Changed for new data set;

--- 12_String/String_29_Combine2ndAnd3rdLine/List_Comparer/Program.cs
+++ 12_String/String_29_Combine2ndAnd3rdLine/List_Comparer/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlTypes;
 using System.IO;
 
 namespace List_Comparer
@@ -10,28 +9,24 @@ namespace List_Comparer
         {
             var counter = 1;
             string toWrite = "";
-            string last = "";
             foreach (var line in File.ReadLines("1.txt"))
             {
-                if (counter == 1 && !string.IsNullOrEmpty(line) && last != line)
+                if (counter == 1)
                 {
-                    toWrite = line;
+                    toWrite = line + "|";
                     counter++;
                 }
-                else if (counter == 2 && line.Contains("views"))
+                else if (counter == 2)
                 {
-                    toWrite = toWrite + "|" + line.Split(',')[1].Trim();
-                    counter = 0;
+                    counter++;
                 }
-                else if (string.IsNullOrEmpty(line))
+                else if (counter == 3)
                 {
-                    toWrite = toWrite + Environment.NewLine;
-                    File.AppendAllText("output.txt", toWrite);
+                    toWrite = toWrite + line + Environment.NewLine;
                     counter = 1;
+                    File.AppendAllText("output.txt", toWrite);
                     toWrite = "";
                 }
-
-                last = line;
             }
 
             Console.WriteLine("DONE");
