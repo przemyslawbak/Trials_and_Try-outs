ee0f89c4a58ebb37672d8efefc13ff6a509eb654
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-03-11 15:20:07 +0100

    Working on recreating problem;

---
+++ 25_Other/Other_20_FuncTest/Sample/InputClosePositionModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Sample
+{
+    public class InputClosePositionModel
+    {
+        public decimal LastClose { get; set; }
+        public bool BuySignal { get; set; }
+        public bool SellSignal { get; set; }
+        public decimal BuyPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public bool IsLong { get; set; }
+        public bool IsShort { get; set; }
+        public decimal Dropdown { get; set; }
+        public string Timestamp { get; set; }
+        public string OpenTimestamp { get; set; }
+        public string CloseTimestamp { get; set; }
+        public List<string> OpenTimestampPut { get; set; }
+        public List<decimal> BuyPricePutList { get; set; }
+        public List<decimal> SellPricePutList { get; set; }
+        public List<decimal> DropdownPut { get; set; }
+        public string CheckSignal { get; set; }
+        public string LastKnownSignal { get; set; }
+        public bool IsLongFol { get; set; }
+        public bool IsShortFol { get; set; }
+        public bool BuySignalFol { get; set; }
+        public bool SellSignalFol { get; set; }
+        public decimal BuyPriceFol { get; set; }
+        public decimal SellPriceFol { get; set; }
+        public decimal DropdownFol { get; set; }
+        public string OpenTimestampFol { get; set; }
+        public string CloseTimestampFol { get; set; }
+        public string CheckSignalFol { get; set; }
+        public string LastKnownSignalFol { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ 25_Other/Other_20_FuncTest/Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputClosePosition = new InputClosePositionModel()
+            {
+                LastClose = input.LastClose,
+                BuySignal = buySignal,
+                SellSignal = sellSignal,
+                BuyPrice = 4994.42500000M,
+                SellPrice = computeTransactionsOutput.SellPrice,
+                IsLong = computeTransactionsOutput.IsLong,
+                IsShort = computeTransactionsOutput.IsShort,
+                Dropdown = computeDropdownOutput.Dropdown,
+                Timestamp = input.Timestamp,
+                OpenTimestamp = computeTransactionsOutput.OpenTimestamp,
+                CloseTimestamp = computeTransactionsOutput.CloseTimestamp,
+                OpenTimestampPut = computeTransactionsOutput.OpenTimestampPut,
+                BuyPricePutList = new List<decimal>() { 4994.42500000M, 4992.95500000M },
+                SellPricePutList = computeTransactionsOutput.SellPricePutList,
+                DropdownPut = computeDropdownOutput.DropdownPut,
+                CheckSignal = computeTransactionsOutput.CheckSignal,
+                LastKnownSignal = computeTransactionsOutput.LastKnownSignal,
+                BuySignalFol = buySignalFol,
+                SellSignalFol = sellSignalFol,
+                BuyPriceFol = 4987.62500000M,
+                SellPriceFol = computeTransactionsOutput.SellPriceFol,
+                IsLongFol = computeTransactionsOutput.IsLongFol, // true i 92
+                IsShortFol = computeTransactionsOutput.IsShortFol,
+                DropdownFol = computeDropdownOutput.DropdownFol,
+                OpenTimestampFol = computeTransactionsOutput.OpenTimestampFol,
+                CloseTimestampFol = computeTransactionsOutput.CloseTimestampFol,
+                CheckSignalFol = computeTransactionsOutput.CheckSignalFol,
+                LastKnownSignalFol = computeTransactionsOutput.LastKnownSignalFol,
+            };
+        }
+    }
+}
