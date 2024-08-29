de25638aba74b949d43bedd27144480dcd50a181
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-08-29 20:23:25 +0200

    Cpp arrays;

---
+++ 43_Cpp/ModernCppBook_05_arrays/ModernCppBook.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    char arr[5]; //arr of type char
+
+    int arr2[5] = { 10, 20, 30, 40, 50 };
+    arr2[0] = 100; // change the value of the first array element
+}
