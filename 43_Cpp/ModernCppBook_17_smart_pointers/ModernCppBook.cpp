15ff5ee9896c53af819dc6125287a94eb3419c0a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-08-31 15:57:26 +0200

    Cpp smart pointers sample;

---
+++ 43_Cpp/ModernCppBook_17_smart_pointers/ModernCppBook.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class MyBaseClass
+{
+public:
+	virtual void printmessage()
+	{
+		std::cout << "Hello from a base class.";
+	}
+};
+
+class MyderivedClass : public MyBaseClass
+{
+public:
+	void printmessage()
+	{
+		std::cout << "Hello from a derived class.";
+	}
+};
+
+int main()
+{
+	std::unique_ptr<MyBaseClass> p = std::make_unique<MyderivedClass>();
+	p->printmessage();
+}
