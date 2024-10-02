aab34300f8d8fc9a45f3a59d9d3d2dfdbc0b1527
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-02 22:23:46 +0200

    Data models added;

---
+++ 28_MachineLearning/Programming_ML.NET_03_RentalsForecast/Taxi.Models/RentalData.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.Models
+{
+    public class RentalData
+    {
+        public DateTime RentalDate { get; set; }
+        public float Year { get; set; }
+        public float TotalRentals { get; set; }
+    }
+}
---
+++ 28_MachineLearning/Programming_ML.NET_03_RentalsForecast/Taxi.Models/RentalPrediction.cs
@@ -0,0 +1,18 @@
+﻿namespace Rental.Models
+{
+    public class RentalPrediction
+    {
+        public float[] ForecastedRentals
+        {
+            get; set;
+        }
+        public float[] LowerBoundRentals
+        {
+            get; set;
+        }
+        public float[] UpperBoundRentals
+        {
+            get; set;
+        }
+    }
+}
