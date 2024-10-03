0c435d1141120f035d83ece760dfaa4a87038af1
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-03 09:55:59 +0200

    Prediction added;

--- 28_MachineLearning/Programming_ML.NET_03_RentalsForecast/Taxi.AppConsumingTrainedModel/Program.cs
+++ 28_MachineLearning/Programming_ML.NET_03_RentalsForecast/Taxi.AppConsumingTrainedModel/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.ML;
-using Microsoft.ML;
-using System.Reflection;
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms.TimeSeries;
+using Rental.Models;
 
 namespace Taxi.AppConsumingTrainedModel
 {
@@ -8,7 +8,22 @@ namespace Taxi.AppConsumingTrainedModel
     {
         static void Main(string[] args)
         {
-            
+            //Create MLContext and loading pre-trained model
+            MLContext mlContext = new MLContext();
+            var trainedModel = mlContext.Model.Load(@"..\..\..\..\Sample\Data\RentalForecast.Model.zip", out var modelInputSchema);
+
+            //creating forecast engine
+            var forecastEngine = trainedModel.CreateTimeSeriesEngine<RentalData, RentalPrediction>(mlContext);
+
+            //make a prediction
+            var horizon = 5;
+            RentalData latest = new RentalData()
+            {
+                RentalDate = DateTime.UtcNow,
+                TotalRentals = 666,
+                Year = 2024
+            };
+            var prediction = forecastEngine.Predict(latest, horizon);
         }
     }
 }
