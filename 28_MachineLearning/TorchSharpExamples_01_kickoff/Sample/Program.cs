82b00f48a428c65c5171c6a120122a9aa444848b
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-06 15:40:49 +0200

    Creating simple tensors;

--- 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
+++ 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using TorchSharp;
+using TorchSharp.Modules;
 
 namespace Sample
 {
@@ -8,13 +9,42 @@ namespace Sample
         static void Main(string[] args)
         {
             //Formatting
+            //https://github.com/dotnet/TorchSharpExamples/blob/main/tutorials/CSharp/tutorial1.ipynb
 
             torch.ones(2, 3, 3);
 
             torch.TensorStringStyle = torch.numpy; //Numpy styles
 
-            torch.TensorStringStyle = torch.csharp; //Julia styles
+            torch.TensorStringStyle = torch.csharp; //csharp styles
             torch.rand(2, 3, 3);
+
+            //Tensors
+            //https://github.com/dotnet/TorchSharpExamples/blob/main/tutorials/CSharp/tutorial2.ipynb
+
+            //3x4 matrix
+            var t = torch.ones(3, 4);
+
+            //printing tensor
+            Console.WriteLine(t.ToString(TensorStringStyle.Julia)); //https://github.com/dotnet/TorchSharp/wiki/Tensor-String-Formatting
+
+            var o = torch.ones(2, 4, 4);
+            Console.WriteLine(o.ToString(TensorStringStyle.Julia));
+
+            //fill up with empty values
+            var e = torch.empty(4, 4);
+
+            //fill up with pre-defined values
+            var p = torch.full(4, 4, 3.14f);
+            p.ToString(TensorStringStyle.Julia);
+
+            //other way to print
+            t.print();
+
+            //other than float32 data type
+            torch.zeros(4, 15, dtype: torch.int32);
+
+            //access matrix item (array)
+            Console.Write(t[0, 0]);
         }
     }
 }
