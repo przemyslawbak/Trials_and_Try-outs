9bc6b47a8123d6354e8a0819e94eca8c82b50dbb
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-06 09:49:05 +0200

    Blank TorchSharp sample added;

---
+++ 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sample
+{
+    //https://github.com/dotnet/TorchSharpExamples/tree/main/tutorials
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
