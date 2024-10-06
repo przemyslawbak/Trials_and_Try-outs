655590f42628a63dece0963d657c14ba69ae714d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-06 14:31:33 +0200

    TorchSharp formatting examples added;

--- 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
+++ 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
@@ -1,11 +1,20 @@
-﻿namespace Sample
+﻿using TorchSharp;
+
+namespace Sample
 {
     //https://github.com/dotnet/TorchSharpExamples/tree/main/tutorials
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Formatting
+
+            torch.ones(2, 3, 3);
+
+            torch.TensorStringStyle = torch.numpy; //Numpy styles
+
+            torch.TensorStringStyle = torch.csharp; //Julia styles
+            torch.rand(2, 3, 3);
         }
     }
 }
