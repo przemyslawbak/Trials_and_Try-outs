09044ad7f1e56ad7cfdca82afb64fd0907a0883e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-06 18:54:07 +0200

    Randm data generating with TorchSharp;

--- 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
+++ 28_MachineLearning/TorchSharpExamples_01_kickoff/Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using TorchSharp;
 using TorchSharp.Modules;
+using static TorchSharp.torch.distributions;
 
 namespace Sample
 {
@@ -84,6 +85,55 @@ namespace Sample
             var arithmetic = a * c + b; //6x6x6
             Console.WriteLine(arithmetic.ToString(TensorStringStyle.Julia));
             Console.WriteLine(arithmetic.shape);
+
+            //Random Numbers and Distributions
+            //https://github.com/dotnet/TorchSharpExamples/blob/main/tutorials/CSharp/tutorial4.ipynb
+
+            torch.rand(10).print();
+            torch.randn(10).print();
+            torch.randint(100, 10).print();
+            torch.randperm(25).print();
+
+            //seed number
+            torch.random.manual_seed(4711);
+            torch.rand(10).print();
+            torch.random.manual_seed(17);
+            torch.rand(10).print();
+            torch.random.manual_seed(4711);
+            torch.rand(10).print();
+
+            //coin toss
+            var bern = new Bernoulli(torch.tensor(0.5f));
+            bern.sample().item<float>();
+
+            //Categories
+            var cat = new Categorical(torch.tensor(new float[] { 0.1f, 0.7f, 0.1f, 0.1f }));
+            cat.sample(4);
+
+            //Real-valued Distributions
+            torch.Tensor foo(Distribution dist) { return dist.sample(4, 4); }
+
+            var norm1 = Normal(torch.tensor(0.5f), torch.tensor(0.125f));
+            var norm2 = Normal(torch.tensor(0.15f), torch.tensor(0.025f));
+
+            foo(norm1).print();
+            foo(norm2).print();
+
+            //Generator
+            torch.Generator gen1 = torch.random.manual_seed(17);
+            torch.rand(2, 3, generator: gen1).print();
+            torch.randn(2, 3, generator: gen1).print();
+
+            var gen2 = new torch.Generator(189);
+            var gen3 = new torch.Generator(189);
+
+            torch.rand(2, 3, generator: gen2).print();
+            torch.rand(2, 3, generator: gen3).print();
+            torch.rand(2, 3, generator: gen2).print();
+            torch.rand(2, 3, generator: gen3).print();
+
+            norm1 = Normal(torch.tensor(0.5f), torch.tensor(0.125f), generator: gen2);
+            norm1.sample(10);
         }
     }
 }
