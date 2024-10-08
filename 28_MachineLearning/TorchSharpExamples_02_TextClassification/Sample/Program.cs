0bfe78705bb3f904d36477e40f41cf815d0014fd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-08 21:32:19 +0200

    Working on text classification sample with TorchSharp;

---
+++ 28_MachineLearning/TorchSharpExamples_02_TextClassification/Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.distributions;
+
+namespace Sample
+{
+    //https://github.com/dotnet/TorchSharp/tree/main/src/Examples
+    //https://github.com/dotnet/TorchSharp/blob/main/src/Examples/TextClassification.cs
+
+    internal class Program
+    {
+        private const long emsize = 200;
+
+        private const long batch_size = 128;
+        private const long eval_batch_size = 128;
+
+        private const int epochs = 15;
+
+        private readonly static string _dataLocation = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "..", "Downloads", "AG_NEWS");
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
