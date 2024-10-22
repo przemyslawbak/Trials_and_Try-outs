928436c87567fad279dbda411e325104299028a3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-22 21:31:19 +0200

    CNTK LSTM blank sample added;

---
+++ 28_MachineLearning/CNTK_03_LSTM_SentimentDetection/Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using CNTK;
+using Sample.CNTKUtil;
+using XPlot.Plotly;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Sample
+{
+    //https://github.com/mdfarragher/DLR/tree/master/BinaryClassification/LstmDemo
+
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    internal class Program
+    {
+        private static readonly string _dataPath = Path.GetFullPath(@"..\..\..\..\Data\california_housing.csv");
+
+        /// <summary>
+        /// The main application entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
