bf6ee9ccda731f6ee61ffa2b5cd168cc27ec87bc
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-22 07:23:07 +0200

    Blank sample added for HousePricePrediction with CNTK DL;

---
+++ 28_MachineLearning/CNTK_02_HousePricesPrediction/Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using CNTK;
+using Sample.CNTKUtil;
+
+namespace Sample
+{
+    //https://github.com/mdfarragher/DLR/tree/master/Regression/HousePricePrediction
+
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main application entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
