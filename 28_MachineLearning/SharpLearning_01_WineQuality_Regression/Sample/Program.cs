9c8df4f5dcb48b84d32a0e92471b12ae01f538cb
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-25 21:12:34 +0200

    Completed SharpLearning Random Forrest ML sample;

--- 28_MachineLearning/SharpLearning_01_WineQuality_Regression/Sample/Program.cs
+++ 28_MachineLearning/SharpLearning_01_WineQuality_Regression/Sample/Program.cs
@@ -1,10 +1,11 @@
-﻿using SharpLearning.InputOutput.Csv;
-using System;
-using System.IO;
-using System.Linq;
+﻿using SharpLearning.CrossValidation.TrainingTestSplitters;
+using SharpLearning.InputOutput.Csv;
+using SharpLearning.Metrics.Regression;
+using SharpLearning.RandomForest.Learners;
 
 namespace Sample
 {
+    //https://github.com/mdabros/SharpLearning/wiki/Introduction-to-SharpLearning
     internal class Program
     {
         private static string dataPath = Path.GetFullPath(@"..\..\..\Data\winequality-white.csv");
@@ -26,7 +27,38 @@ namespace Sample
             var observations = parser.EnumerateRows(c => c != targetName)
                 .ToF64Matrix();
 
+            // 30 % of the data is used for the test set. 
+            var splitter = new RandomTrainingTestIndexSplitter<double>(trainingPercentage: 0.7, seed: 24);
 
+            var trainingTestSplit = splitter.SplitSet(observations, targets);
+            var trainSet = trainingTestSplit.TrainingSet;
+            var testSet = trainingTestSplit.TestSet;
+
+            // Create the learner and learn the model.
+            var learner = new RegressionRandomForestLearner(trees: 100);
+            var model = learner.Learn(trainSet.Observations, trainSet.Targets);
+
+            // predict the training and test set.
+            var trainPredictions = model.Predict(trainSet.Observations);
+            var testPredictions = model.Predict(testSet.Observations);
+
+            // create the metric
+            var metric = new MeanSquaredErrorRegressionMetric();
+
+            // measure the error on training and test set.
+            var trainError = metric.Error(trainSet.Targets, trainPredictions);
+            var testError = metric.Error(testSet.Targets, testPredictions);
+
+            // the variable importance requires the featureNameToIndex
+            // from the data set. This mapping describes the relation
+            // from column name to index in the feature matrix.
+            var featureNameToIndex = parser.EnumerateRows(c => c != targetName)
+                .First().ColumnNameToIndex;
+
+            // Get the variable importance from the model.
+            var importances = model.GetVariableImportance(featureNameToIndex);
+
+            //POSSIBLE TO SAVE AND LOAD THE MODEL
         }
     }
 }
