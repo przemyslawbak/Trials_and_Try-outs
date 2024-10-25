7d4b88dcbf2e6b204778b2e814cb7609d21f110f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-25 20:45:27 +0200

    SharpLearning blank sample with data added and data loading;

---
+++ 28_MachineLearning/SharpLearning_01_WineQuality_Regression/Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using SharpLearning.InputOutput.Csv;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Sample
+{
+    internal class Program
+    {
+        private static string dataPath = Path.GetFullPath(@"..\..\..\Data\winequality-white.csv");
+
+        static void Main(string[] args)
+        {
+            // Setup the CsvParser
+            var parser = new CsvParser(() => new StreamReader(dataPath));
+
+            // the column name in the wine quality data set we want to model.
+            var targetName = "quality";
+
+            // read the "quality" column, this is the targets for our learner. 
+            var targets = parser.EnumerateRows(targetName)
+                .ToF64Vector();
+
+            // read the feature matrix, all columns except "quality",
+            // this is the observations for our learner.
+            var observations = parser.EnumerateRows(c => c != targetName)
+                .ToF64Matrix();
+
+
+        }
+    }
+}
