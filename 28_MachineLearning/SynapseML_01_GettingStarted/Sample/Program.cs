1b054b7c8a20a2bf7c14302afdfdddc6ee9a02a3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-27 22:08:23 +0100

    Installed several packages;

--- 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
+++ 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using Synapse.ML.Lightgbm;
+using Synapse.ML.Featurize;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Types;
 
 //https://devblogs.microsoft.com/dotnet/announcing-synapseml-for-dotnet/#getting-started-in-.net
 
@@ -13,6 +14,7 @@ namespace Sample
         static void Main(string[] args)
         {
             //https://devblogs.microsoft.com/dotnet/announcing-synapseml-for-dotnet/#a-.net-example-using-lightgbmclassifier-in-synapseml
+            //https://microsoft.github.io/SynapseML/docs/Reference/Dotnet%20Setup/#1-install-net
         }
     }
 }
