82c42f72d6cd89e6bc9a0b26f85ce3bbddce48b0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-27 20:35:17 +0100

    Added blank kick off SynapseML sample with packages;

---
+++ 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+//https://devblogs.microsoft.com/dotnet/announcing-synapseml-for-dotnet/#getting-started-in-.net
+
+namespace Sample
+{
+    internal class Program
+    {
+        private static string trainingDataPath = Path.GetFullPath(@"..\..\..\Data\train_data.csv");
+
+        static void Main(string[] args)
+        {
+            //https://microsoft.github.io/SynapseML/docs/Reference/Dotnet%20Setup/#1-install-net
+        }
+    }
+}
