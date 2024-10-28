37f300b2f127f306f87246ba172e3b2612b70222
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-28 07:15:45 +0100

    Trials with SparkSession ended up with exception;

--- 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
+++ 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
@@ -15,6 +15,21 @@ namespace Sample
         {
             //https://devblogs.microsoft.com/dotnet/announcing-synapseml-for-dotnet/#a-.net-example-using-lightgbmclassifier-in-synapseml
             //https://microsoft.github.io/SynapseML/docs/Reference/Dotnet%20Setup/#1-install-net
+            //https://www.c-sharpcorner.com/article/how-to-addedit-path-environment-variable-in-windows-11/
+
+            // Create Spark session
+            SparkSession spark = SparkSession //exception
+                .Builder()
+                .AppName("LightGBMExample")
+                .GetOrCreate();
+
+            // Load Data
+            DataFrame df = spark.Read()
+                .Option("inferSchema", true)
+                .Parquet("wasbs://publicwasb@mmlspark.blob.core.windows.net/AdultCensusIncome.parquet")
+                .Limit(2000);
+
+
         }
     }
 }
