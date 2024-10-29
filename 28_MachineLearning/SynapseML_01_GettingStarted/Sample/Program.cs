aea956977f20ae3b4b820ab65009bffffaed862a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-29 06:52:48 +0100

    Trying to install Spark with another approach;

--- 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
+++ 28_MachineLearning/SynapseML_01_GettingStarted/Sample/Program.cs
@@ -13,11 +13,7 @@ namespace Sample
 
         static void Main(string[] args)
         {
-            //https://devblogs.microsoft.com/dotnet/announcing-synapseml-for-dotnet/#a-.net-example-using-lightgbmclassifier-in-synapseml
-            //https://microsoft.github.io/SynapseML/docs/Reference/Dotnet%20Setup/#1-install-net
-            //https://www.c-sharpcorner.com/article/how-to-addedit-path-environment-variable-in-windows-11/
-            //https://github.com/dotnet/spark/blob/main/docs/developer-guide.md#debugging-spark-net-application
-            //https://stackoverflow.com/a/56187530
+            //https://github.com/dotnet/spark/blob/main/docs/getting-started/windows-instructions.md
 
             // Create Spark session
             SparkSession spark = SparkSession //exception "System.Net.Internals.SocketExceptionFactory.ExtendedSocketException: 'No connection could be made because the target machine actively refused it. 127.0.0.1:5567'"
