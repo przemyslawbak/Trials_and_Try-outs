c1287452d67b3d89770779741899edd1b2806687
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-10-30 20:43:59 +0100

    New blank added;

---
+++ 28_MachineLearning/TensorFlow_09_Tutorial_LinearRegressionKeras/Sample/Utilities/IExample.cs
@@ -0,0 +1,32 @@
+﻿using Tensorflow;
+
+namespace Sample.Utilities
+{
+    public interface IExample
+    {
+        ExampleConfig Config { get; set; }
+        ExampleConfig InitConfig();
+        bool Run();
+
+        void BuildModel();
+
+        /// <summary>
+        /// Build dataflow graph, train and predict
+        /// </summary>
+        /// <returns></returns>
+        void Train();
+        string FreezeModel();
+        void Test();
+
+        void Predict();
+
+        Graph ImportGraph();
+
+        Graph BuildGraph();
+
+        /// <summary>
+        /// Prepare dataset
+        /// </summary>
+        void PrepareData();
+    }
+}
