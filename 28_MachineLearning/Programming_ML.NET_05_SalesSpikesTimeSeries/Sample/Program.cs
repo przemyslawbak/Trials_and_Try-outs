abb30b5dbf5fd1c98c474b03b0f6a48e141ea173
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-11-03 19:51:32 +0100

    New ML.NET time series added blank with data;

---
+++ 28_MachineLearning/Programming_ML.NET_05_SalesSpikesTimeSeries/Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+
+//https://github.com/mdfarragher/DSC/tree/master/TimeSeries/SalesSpikes
+
+namespace Sample
+{
+    
+
+    internal class Program
+    {
+        // filenames for training and test data
+        private static string trainingDataPath = Path.GetFullPath(@"..\..\..\Data\sales.csv");
+
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
