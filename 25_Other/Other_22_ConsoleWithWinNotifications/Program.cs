aa29a5ddc01cf9fc01fe35eb0242f485055335b1
przemyslawbak <przemyslaw.bak@simple-mail.net>
2025-01-03 13:54:01 +0100

    New sample added, testing WinForms with Console app;

---
+++ 25_Other/Other_22_ConsoleWithWinNotifications/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Text = "TestApp";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            ContextMenu trayMenu = new ContextMenu();
+
+            trayMenu.MenuItems.Add("Blah", item1_Click);
+            trayMenu.MenuItems.Add("Blah2", item1_Click);
+            trayMenu.MenuItems.Add("Blah3", item1_Click);
+
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+            Application.Run();
+        }
+
+        private static void item1_Click(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
