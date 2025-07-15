008c72186b27d9035f7990b8712ee5f0ee8d7f4d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2025-07-15 22:47:09 +0200

    Blank sample project added;

---
+++ 12_String/String_30_ReadingFileDataAndSavingToDb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
