670b5470b0ed802cd1b1ac89e0b5e482d1ea7e07
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-05 09:08:49 +0100

    New MAUI blank sample project added;

---
+++ 45_MAUI/06_Maui_CrashCourse/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Sample_MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ 45_MAUI/06_Maui_CrashCourse/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Sample_MAUI
+{
+    //https://www.youtube.com/watch?v=mgW6xviirQk&list=PLfbOp004UaYVt1En4WW3pVuM-vm66OqZe
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
