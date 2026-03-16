49110248f1b3675c7cb3f7a531c882c0740688ce
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-16 20:16:16 +0100

    Chapter 2 completed;

--- 46_Godot/03-learning-gd-script-book/main.gd
+++ 46_Godot/03-learning-gd-script-book/main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 #Called when the node enters the scene
 func _ready(): 
-	print("Hello, World")
+	
 	var number_of_lives = 3
-	print(number_of_lives)
+	var fireball_damage = 2
+	number_of_lives -= number_of_lives
+	prints("The player has", number_of_lives, "out of", 10, "lives left")
