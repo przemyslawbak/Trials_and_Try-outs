744b36b111ce58d206f8c3720f5514ac5963b1b5
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-16 20:34:05 +0100

    Chapter 3 of the book completed;

--- 46_Godot/03-learning-gd-script-book/main.gd
+++ 46_Godot/03-learning-gd-script-book/main.gd
@@ -3,7 +3,18 @@ extends Node2D
 #Called when the node enters the scene
 func _ready(): 
 	
-	var number_of_lives = 3
-	var fireball_damage = 2
-	number_of_lives -= number_of_lives
-	prints("The player has", number_of_lives, "out of", 10, "lives left")
+	var inventory = [
+		"item1",
+		"item2",
+		"item3",
+		"item4",
+	]
+	
+	var dict = {
+		"a": 1,
+		"b": "bbb",
+		"c": 1.5,
+	}
+	
+	for item in inventory:
+		print(item)
