dfffe69fd2b4097fa63982f860b092436807ba6c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-17 14:57:37 +0100

    Cont. with chapter 6;

--- 46_Godot/04-learning-gd-script-book-vs-game/player.gd
+++ 46_Godot/04-learning-gd-script-book-vs-game/player.gd
@@ -1,11 +1,15 @@
 extends Node2D
+const MAX_HEALTH: int = 10
+var health: int = 10
+@onready var _health_label: Label = $HealthLabel
 
+func add_health_points(difference: int):
+	health += difference
+	health = clamp(health, 0, MAX_HEALTH)
+	update_health_label()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func update_health_label():
+	_health_label.text = str(health) + "/" + str(MAX_HEALTH)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	add_health_points(-2)
