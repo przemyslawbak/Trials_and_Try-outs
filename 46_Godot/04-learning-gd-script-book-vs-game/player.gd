9b5853886aae0bd57e3116423940aa55c948e310
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-18 19:55:41 +0100

    Completed chapter 6 of the book;

--- 46_Godot/04-learning-gd-script-book-vs-game/player.gd
+++ 46_Godot/04-learning-gd-script-book-vs-game/player.gd
@@ -1,15 +1,23 @@
 extends Node2D
 const MAX_HEALTH: int = 10
-var health: int = 10
+@export var damage: float = 0.0
+@export var player_name: String = "Erika"
+@export var health: int = 10:
+	get:
+		return health
+	set(new_value):
+		health = clamp(new_value, 0, MAX_HEALTH)
+		update_health_label()
+
 @onready var _health_label: Label = $HealthLabel
 
 func add_health_points(difference: int):
 	health += difference
-	health = clamp(health, 0, MAX_HEALTH)
-	update_health_label()
 
 func update_health_label():
+	if not is_instance_valid(_health_label):
+		return
 	_health_label.text = str(health) + "/" + str(MAX_HEALTH)
 
 func _ready():
-	add_health_points(-2)
+	update_health_label()
