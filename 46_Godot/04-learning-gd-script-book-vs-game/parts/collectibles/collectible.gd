ba8ecfcc1ca6581a667edbb2d36b27b4ece2d02d
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-20 21:41:16 +0100

    Chapter 9 of the book completed;

---
+++ 46_Godot/04-learning-gd-script-book-vs-game/parts/collectibles/collectible.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+class_name Collectible
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
--- 46_Godot/04-learning-gd-script-book-vs-game/parts/player/camera_position.gd
+++ 46_Godot/04-learning-gd-script-book-vs-game/parts/player/camera_position.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+@export var position_interpolation_speed: float = 1.0
 @export var camera_distance: float = 200
 @onready var _player: CharacterBody2D = get_parent()
 func _process(_delta):
 	var move_direction: Vector2 = _player.velocity.normalized()
 	position = move_direction * camera_distance
+
+func _physics_process(delta):
+	var move_direction: Vector2 = _player.velocity.normalized()
+	var target_position: Vector2 = move_direction * camera_distance
+	position = position.lerp(target_position, position_interpolation_speed * delta)
+	
+	
