c0e1faf0f4d7040fa084c51cc6a44b71a99c88c6
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-21 18:30:53 +0100

    Comment added;

--- 46_Godot/04-learning-gd-script-book-vs-game/screens/ui/menu.gd
+++ 46_Godot/04-learning-gd-script-book-vs-game/screens/ui/menu.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	$CenterContainer/MainUIContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
 	$CenterContainer/MainUIContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/ExitButton.pressed.connect(_on_exit_button_pressed)
 
+#optional: how should look Option B — Connect via editor in this case?
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://screens/game/main.tscn")
 
