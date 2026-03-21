5b452378353ce34695a373dcfb43e51f74d7d071
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-21 18:30:23 +0100

    Connecting buttons;

---
+++ 46_Godot/04-learning-gd-script-book-vs-game/screens/ui/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready() -> void:
+	$CenterContainer/MainUIContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
+	$CenterContainer/MainUIContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/ExitButton.pressed.connect(_on_exit_button_pressed)
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://screens/game/main.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
