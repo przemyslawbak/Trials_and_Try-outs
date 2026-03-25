4c36dfb88c03dcda40819365b652fe5f2d0f8fda
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-03-25 19:01:58 +0100

    Chapter 1 completed;

--- 46_Godot/05-godot-intro/Scripts/greetings_label.gd
+++ 46_Godot/05-godot-intro/Scripts/greetings_label.gd
@@ -1,9 +1,29 @@
 extends Label
+var xVelocity = 150 #150 pixels per second to the right
+var yVelocity = 120 #120 pixels per second downwards
+var velocity = Vector2(xVelocity, yVelocity) # Initial speed and direction
+var screen_size_x #screen horizontal dimension
+var screen_size_y #screen vertical dimension
+var label_size_x #label width
+var label_size_y #label height
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
- text = "Go Go Godot!"
+	# Get the screen size (viewport size)
+	screen_size_x = get_viewport_rect().size.x
+	screen_size_y = get_viewport_rect().size.y
+	label_size_x = size.x
+	label_size_y = size.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
- pass
+	position = position + velocity * delta
+	# Bounce off left/right edges
+	if position.x < 0 or position.x + label_size_x > screen_size_x:
+		velocity.x *= -1
+		position.x = clamp(position.x, 0, screen_size_x - label_size_x)
+		
+	# Bounce off top/bottom edges
+	if position.y < 0 or position.y + label_size_y > screen_size_y:
+		velocity.y *= -1
+		position.y = clamp(position.y, 0, screen_size_y - label_size_y)
