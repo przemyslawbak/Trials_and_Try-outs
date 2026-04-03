0582accd7400866fef7dc4c1af445de22ec41432
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-04-03 20:01:20 +0200

    Moved back some changes;

--- 46_Godot/07-tile-map-playground/Scripts/world.gd
+++ 46_Godot/07-tile-map-playground/Scripts/world.gd
@@ -146,7 +146,7 @@ func _input(event: InputEvent) -> void:
 
 		_selected_direction = data["direction"]
 		_selected_tiles     = data["tiles"]
-		_selected_facing    = "NE"
+		_selected_facing    = ""
 		_destination        = picked
 		_awaiting_facing    = true
 
