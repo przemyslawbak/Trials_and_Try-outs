4d113bff7bd16609581f38b8162d47fe188c66c3
przemyslawbak <przemyslaw.bak@simple-mail.net>
2026-04-13 23:06:20 +0200

    Finished trial;

---
+++ 46_Godot/07-tile-map-playground/print_cells3.gd
@@ -0,0 +1,9 @@
+extends SceneTree
+
+func _init():
+	var world = load("res://Scenes/world.tscn").instantiate()
+	var ground = world.get_node("Ground-lvl1")
+	var cells = ground.get_used_cells()
+	for c in cells:
+		print("Cell ", c, " -> ", ground.get_cell_atlas_coords(c))
+	quit()
